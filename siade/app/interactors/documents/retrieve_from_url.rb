8e4edf2ef21d9ea86041974904117dd1fdf1d643
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-02-26 11:35:24 +0100

    Introduce interactor to retrieve documents from source URL
    Locally download a file (before self storage) from the URL obtained by a data
    provider.

---
+++ siade/app/interactors/documents/retrieve_from_url.rb
@@ -0,0 +1,33 @@
+class Documents::RetrieveFromUrl < ApplicationInteractor
+  before do
+    context.errors ||= []
+  end
+
+  def call
+    context.content = URI.open(source_url).binmode.read
+  rescue OpenURI::HTTPError => e
+    response = e.io
+    msg = "Erreur lors de la récupération du document : status #{response.status[0]} with body '#{response.string}'"
+    fail_with_error(msg)
+  rescue Net::OpenTimeout, Net::ReadTimeout
+    fail_with_error('Temps d\'attente de téléchargement du document écoulé')
+  rescue URI::InvalidURIError => e
+    msg = "L\'URL source du document chez le fournisseur de données est invalide : #{e.message}."
+    fail_with_error(msg)
+  end
+
+  private
+
+  def fail_with_error(msg)
+    errors << msg
+    context.fail!
+  end
+
+  def errors
+    context.errors
+  end
+
+  def source_url
+    URI.parse(context.url)
+  end
+end
---
+++ siade/spec/interactors/documents/retrieve_from_url_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+describe Documents::RetrieveFromUrl do
+  subject { described_class.call(retrieve_params) }
+
+  let(:retrieve_params) do
+    { url: source_doc_url }
+  end
+
+  describe '.call' do
+    let(:source_doc_url) { 'https://random.com/get_doc' }
+
+    context 'when the source server responds with an HTTP success' do
+      before do
+        stub_request(:get, source_doc_url)
+          .to_return(
+            status: 200,
+            body: 'much doc content'
+        )
+      end
+
+      it { is_expected.to be_success }
+
+      its(:content) { is_expected.to eq('much doc content') }
+    end
+
+    context 'when the source server responds with an HTTP error' do
+      before do
+        stub_request(:get, source_doc_url)
+          .to_return(
+            status: 404,
+            body: 'Not Found'
+        )
+      end
+
+      it { is_expected.to be_failure }
+
+      its(:errors) { is_expected.to include('Erreur lors de la récupération du document : status 404 with body \'Not Found\'') }
+    end
+
+    context 'when a timeout occurs downloading the document' do
+      before { stub_request(:get, source_doc_url).to_timeout }
+
+      it { is_expected.to be_failure }
+
+      its(:errors) { is_expected.to include('Temps d\'attente de téléchargement du document écoulé') }
+    end
+
+    context 'when the source URL is invalid' do
+      let(:source_doc_url) { 'not an URL' }
+
+      it { is_expected.to be_failure }
+
+      its(:errors) { is_expected.to include('L\'URL source du document chez le fournisseur de données est invalide : bad URI(is not URI?): "not an URL".') }
+    end
+  end
+end
