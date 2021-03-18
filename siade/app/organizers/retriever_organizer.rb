b339df2b76a7006b26414e76da841f783e5ad309
Loïc Delmaire <loic.delmaire@gmail.com>
2021-03-18 12:20:11 +0100

    Raise error if status is not defined at the end of RetrieverOrganizer

--- siade/app/organizers/retriever_organizer.rb
+++ siade/app/organizers/retriever_organizer.rb
@@ -1,11 +1,23 @@
 class RetrieverOrganizer < ApplicationOrganizer
+  class StatusNotDefined < Exception; end
+
   def self.inherited(klass)
     klass.class_eval do
       before do
         context.resource = nil
-        context.status   = 200
+        context.status   = nil
         context.errors   = []
       end
+
+      after do
+        if context.status.nil?
+          status_not_defined!
+        end
+      end
     end
+  end
+
+  def status_not_defined!
+    raise StatusNotDefined
   end
 end
---
+++ siade/spec/organizers/retriever_organizer_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe RetrieverOrganizer, type: :organizer do
+  before(:all) do
+    class DummyRetrieverInteractor < ApplicationInteractor
+      def call
+        if context.set_status
+          context.status = context.set_status
+        end
+      end
+    end
+
+    class DummyRetrieverOrganizer < RetrieverOrganizer
+      organize DummyRetrieverInteractor
+    end
+  end
+
+  describe 'status context' do
+    subject { DummyRetrieverOrganizer.call(params).status }
+
+    context 'when there is no status defined' do
+      let(:params) { {} }
+
+      it 'raises an error' do
+        expect {
+          subject
+        }.to raise_error(RetrieverOrganizer::StatusNotDefined)
+      end
+    end
+
+    context 'when there is a status defined' do
+      let(:params) { { set_status: 418 } }
+
+      it 'does not raise an error' do
+        expect {
+          subject
+        }.not_to raise_error
+      end
+    end
+  end
+end
