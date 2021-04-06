a6ca0b15d61b21486fd13f18cea335d9b074779f
Alexandre De Pablo <alexandre.depablo@gmail.com>
2021-04-06 21:47:32 +0200

    Fix failing spec
    The 'id' attribute is mandatory in JSONAPI specification and therefore
    was added in the BuildResource component.

--- siade/spec/interactors/probtp/attestations_cotisations_retraite/build_resource_spec.rb
+++ siade/spec/interactors/probtp/attestations_cotisations_retraite/build_resource_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 describe PROBTP::AttestationsCotisationsRetraite::BuildResource do
   describe '.call' do
-    subject { described_class.call(url: 'not.a.real/file/upload') }
+    subject { described_class.call(url: 'not.a.real/file/upload', params: params) }
+
+    let(:params) do
+      {
+        siret: eligible_siret(:probtp),
+      }
+    end
 
     it { is_expected.to be_success }
 
     its(:resource) do
       is_expected.to include(
+        id: eligible_siret(:probtp),
         document_url: 'not.a.real/file/upload',
       )
     end
