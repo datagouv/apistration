48adbea77a7172c4a6554dd35b0408080aba4642
Loïc Delmaire <loic.delmaire@gmail.com>
2021-04-15 11:01:06 +0200

    Introduce ACOSS::AttestationsSociales::ValidateParams

---
+++ siade/spec/organizers/acoss/attestations_sociales/validate_params_spec.rb
@@ -0,0 +1,43 @@
+RSpec.describe ACOSS::AttestationsSociales::ValidateParams, type: :validate_params do
+  subject { described_class.call(params: params) }
+
+  let(:params) do
+    {
+      siren: siren,
+      user_id: user_id,
+      recipient: recipient,
+    }
+  end
+
+  let(:siren) { valid_siren }
+  let(:user_id) { '9001' }
+  let(:recipient) { 'DINUM' }
+
+  context 'with valid params' do
+    it { is_expected.to be_a_success }
+  end
+
+  context 'with invalid siren' do
+    let(:siren) { invalid_siren }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to be_present }
+  end
+
+  context 'with invalid user_id' do
+    let(:user_id) { nil }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to be_present }
+  end
+
+  context 'with invalid recipient' do
+    let(:recipient) { nil }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to be_present }
+  end
+end
