75fe402ccf75ef6a6b2a13bd1a42b76bf370308f
Loïc Delmaire <loic.delmaire@gmail.com>
2021-04-21 11:09:49 +0200

    Add missing ValidateSiren to PROBTP::AttestationsCotisationsRetraite

--- siade/app/organizers/probtp/attestations_cotisations_retraite.rb
+++ siade/app/organizers/probtp/attestations_cotisations_retraite.rb
@@ -1,5 +1,6 @@
 class PROBTP::AttestationsCotisationsRetraite < ApplicationOrganizer
-  organize PROBTP::AttestationsCotisationsRetraite::MakeRequest,
+  organize ValidateSiren,
+           PROBTP::AttestationsCotisationsRetraite::MakeRequest,
            PROBTP::AttestationsCotisationsRetraite::ValidateResponse,
            PROBTP::UploadAttestationsCotisationRetraite,
            PROBTP::AttestationsCotisationsRetraite::BuildResource
