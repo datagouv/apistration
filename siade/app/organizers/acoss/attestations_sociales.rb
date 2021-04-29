3472199a0d7fcb5e7770fa605220e7e86c75a207
Loïc Delmaire <loic.delmaire@gmail.com>
2021-04-29 18:57:46 +0200

    Introduce ACOSS::AttestationsSociales retriever

---
+++ siade/app/organizers/acoss/attestations_sociales.rb
@@ -0,0 +1,12 @@
+class ACOSS::AttestationsSociales < RetrieverOrganizer
+  organize ACOSS::AttestationsSociales::ValidateParams,
+           ACOSS::AttestationsSociales::Authenticate,
+           ACOSS::AttestationsSociales::MakeRequest,
+           ACOSS::AttestationsSociales::ValidateResponse,
+           ACOSS::AttestationsSociales::UploadDocument,
+           ACOSS::AttestationsSociales::BuildResource
+
+  def provider_name
+    'ACOSS'
+  end
+end
---
+++ siade/spec/organizers/acoss/attestations_sociales_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe ACOSS::AttestationsSociales, type: :retriever_organizer do
+  describe '.call' do
+    subject { described_class.call(params: params) }
+
+    let(:params) do
+      {
+        siren: siren,
+        user_id: '1234567890',
+        recipient: 'recipient',
+      }
+    end
+
+    context 'with valid siren', vcr: { cassette_name: 'acoss/with_valid_siren' } do
+      let(:siren) { valid_siren(:acoss) }
+
+      it { is_expected.to be_a_success }
+
+      its(:resource) { is_expected.to be_present }
+      its(:errors) { is_expected.to be_blank }
+
+      it 'has a document_url key on resource' do
+        expect(subject.resource.document_url).to be_present
+      end
+    end
+
+    context 'with invalid siren', vcr: { cassette_name: 'acoss/with_non_existent_siren' } do
+      let(:siren) { not_found_siren }
+
+      it { is_expected.to be_a_failure }
+
+      its(:resource) { is_expected.to be_blank }
+      its(:errors) { is_expected.to be_present }
+    end
+  end
+end
