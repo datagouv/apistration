eb1f1f8a2d27848542aba61f64af44b4018a5abb
Loïc Delmaire <loic.delmaire@gmail.com>
2021-04-29 19:56:22 +0200

    Introduce API::V3::BaseController#extract_http_code

--- siade/app/controllers/api/v3/base_controller.rb
+++ siade/app/controllers/api/v3/base_controller.rb
@@ -1,6 +1,32 @@
 class API::V3::BaseController < API::AuthenticateEntityController
   def render_errors(organizer)
     render json:    ::ErrorsSerializer.new(organizer.errors, format: error_format).as_json,
-           status:  organizer.status
+           status:  extract_http_code(organizer)
+  end
+
+  def extract_http_code(retriever)
+    if at_least_one_error_kind_of?(:wrong_parameter, retriever)
+      :unprocessable_entity
+    elsif at_least_one_error_kind_of?(:network_error, retriever)
+      :gateway_timeout
+    elsif at_least_one_error_kind_of?(:unavailable_for_legal_reason, retriever)
+      :unavailable_for_legal_reasons
+    elsif at_least_one_error_kind_of?(:unauthorized, retriever)
+      :unauthorized
+    elsif at_least_one_error_kind_of?(:not_found, retriever)
+      :not_found
+    elsif at_least_one_error_kind_of?(:provider_error, retriever)
+      :bad_gateway
+    elsif retriever.errors.blank?
+      :ok
+    else
+      raise 'No valid HTTP status'
+    end
+  end
+
+  def at_least_one_error_kind_of?(kind, retriever)
+    retriever.errors.any? do |error|
+      error.kind == kind
+    end
   end
 end
