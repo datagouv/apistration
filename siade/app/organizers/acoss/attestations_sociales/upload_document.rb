a4cf019df08ab1e1ab5874ce80afc6db6b866f58
Loïc Delmaire <loic.delmaire@gmail.com>
2021-04-29 11:15:33 +0200

    Introduce ACOSS::AttestationsSociales::UploadDocument

---
+++ siade/app/organizers/acoss/attestations_sociales/upload_document.rb
@@ -0,0 +1,15 @@
+class ACOSS::AttestationsSociales::UploadDocument < UploadDocumentOrganizer
+  organize Documents::StoreFromBase64
+
+  def source_file_content
+    context.response.body
+  end
+
+  def file_type
+    'pdf'
+  end
+
+  def filename
+    'attestation_fiscale_acoss'
+  end
+end
---
+++ siade/spec/organizers/acoss/attestations_sociales/upload_document_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe ACOSS::AttestationsSociales::UploadDocument, :self_hosted_doc do
+  describe '.call' do
+    subject { described_class.call(response: response) }
+
+    let(:response) { instance_double(Net::HTTPOK, code: code, body: body) }
+    let(:code) { 200 }
+    let(:encoded_file_content) { encode64_payload_file('pdf/dummy.pdf') }
+    let(:decoded_file_content) { Base64.strict_decode64(encoded_file_content) }
+    let(:body) { encoded_file_content }
+
+    it { is_expected.to be_success }
+
+    its(:file_type) { is_expected.to eq('pdf') }
+
+    its(:filename) { is_expected.to eq('attestation_fiscale_acoss') }
+
+    its(:content) { is_expected.to eq(decoded_file_content) }
+
+    its(:url) { is_expected.to be_a_valid_self_hosted_pdf_url('attestation_fiscale_acoss') }
+  end
+end
