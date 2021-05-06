a66118a3fc2c345a3e6a15802562302a72d05d64
Cyril Kato <cyril.kato@data.gouv.fr>
2021-05-06 18:12:12 +0200

    test(openapi.yaml): instantiating a context by definition to test

--- siade/spec/acceptances/open_api_validation_spec.rb
+++ siade/spec/acceptances/open_api_validation_spec.rb
@@ -1,15 +1,18 @@
 RSpec.describe "OpenAPI file", type: :acceptance do
-  let(:open_api_file) { Rails.root.join("public/v2/open-api.yml") }
+  DEFINITION_PATHS = [
+    Rails.root.join('public', 'v2', 'open-api.yml').to_s
+  ].freeze
 
-  it "is a valid YAML" do
-    expect {
-      YAML.load_file(open_api_file)
-    }.not_to raise_error
-  end
+  DEFINITION_PATHS.each do |definition_path|
+    context "OpenAPI definition path: #{definition_path.inspect}" do
+      it "is a valid YAML" do
+        expect(YAML.load_file(definition_path)).to be_truthy
+      end
 
-  it "is a valid OpenAPI configuration" do
-    document = Openapi3Parser.load_file(open_api_file)
-
-    expect(document).to be_valid, "Errors: #{document.errors.to_h}"
+      it "is a valid OpenAPI configuration" do
+        document = Openapi3Parser.load_file(definition_path)
+        expect(document).to be_valid, "Errors: #{document.errors.to_h}"
+      end
+    end
   end
 end
