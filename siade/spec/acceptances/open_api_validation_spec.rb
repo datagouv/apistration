b6f33d99eb775fa0a17f2a0639aee2b0fe591fce
Cyril Kato <cyril.kato@data.gouv.fr>
2021-05-11 16:59:38 +0200

    test(OpenAPI file): use static contexts (API v2 & v3+)

--- siade/spec/acceptances/open_api_validation_spec.rb
+++ siade/spec/acceptances/open_api_validation_spec.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 
-RSpec.describe "OpenAPI file", type: :acceptance do
-  DEFINITION_PATHS = [
-    'public/v2/open-api.yml',
-    *Dir.glob('swagger/**/*.yaml')
-  ].freeze
+RSpec.describe 'OpenAPI file', type: :acceptance do
+  context 'API v2 definition' do
+    let(:definition_path) { 'public/v2/open-api.yml' }
 
-  DEFINITION_PATHS.each do |definition_path|
-    context "OpenAPI definition path: #{definition_path.inspect}" do
-      it "is a valid YAML" do
-        expect(YAML.load_file(definition_path)).to be_truthy
-      end
+    it 'is a valid YAML' do
+      expect(YAML.load_file(definition_path)).to be_truthy
+    end
 
-      it "is a valid OpenAPI configuration" do
-        document = Openapi3Parser.load_file(definition_path)
-        expect(document).to be_valid, "Errors: #{document.errors.to_h}"
-      end
+    it 'is a valid configuration' do
+      document = Openapi3Parser.load_file(definition_path)
+      expect(document).to be_valid, "Errors: #{document.errors.to_h}"
+    end
+  end
+
+  context 'API v3 (and more) definition' do
+    let(:definition_path) { 'swagger/v3/openapi.yaml' }
+
+    it 'is a valid YAML' do
+      expect(YAML.load_file(definition_path)).to be_truthy
+    end
+
+    it 'is a valid configuration' do
+      document = Openapi3Parser.load_file(definition_path)
+      expect(document).to be_valid, "Errors: #{document.errors.to_h}"
     end
   end
 end
