1d807c5406e062f6fb11bd5c5d2c54f702636844
Loïc Delmaire <loic.delmaire@gmail.com>
2021-05-21 16:22:34 +0200

    FIX SCUMBAG SKELZ0R

--- siade/bin/test_swagger.sh
+++ siade/bin/test_swagger.sh
@@ -2,5 +2,5 @@
 
 cp swagger/v3/openapi.yaml swagger/v3/openapi-versionned.yaml &&
   ./bin/generate_swagger.sh &&
-  (diff swagger/v3/openapi.yaml swagger/v3/openapi-versionned.yaml || echo "Swagger file is different after generation" && exit 1) &&
+  (diff swagger/v3/openapi.yaml swagger/v3/openapi-versionned.yaml || (echo "Swagger file is different after generation" && exit 1)) &&
   exit 0
