97e91f80a4c7c71eb79f694b26e5e9fabe26ea24
Loïc Delmaire <loic.delmaire@gmail.com>
2021-06-02 11:39:00 +0200

    Track provider errors on fail within RetrieverOrganizer
    Thanks to Organizer rollback functionality (and little hack), we can
    call monitoring service on failure within this organizer and track only
    provider errors thanks to `ApplicationError#kind` method.

--- siade/app/organizers/retriever_organizer.rb
+++ siade/app/organizers/retriever_organizer.rb
@@ -12,8 +12,31 @@ class RetrieverOrganizer < ApplicationOrganizer
     end
   end
 
+  def call
+    mark_organizer_as_called_to_run_rollback_on_fail!
+    super
+  end
+
+  def rollback
+    track_providers_errors
+  end
+
+  protected
+
   def provider_name
     fail 'should be implemented in inherited class'
+  end
+
+  private
+
+  def track_providers_errors
+    if context.errors.any?
+      context.errors.select do |error|
+        error.kind == :provider_error
+      end.each do |provider_error|
+        MonitoringService.instance.track_provider_error(provider_error)
+      end
+    end
   end
 
   def invalid_provider_name!
@@ -22,5 +45,9 @@ class RetrieverOrganizer < ApplicationOrganizer
 
   def provider_name_valid?
     ErrorsBackend.instance.provider_code_from_name(context.provider_name).present?
+  end
+
+  def mark_organizer_as_called_to_run_rollback_on_fail!
+    context.called!(self)
   end
 end
--- siade/spec/organizers/retriever_organizer_spec.rb
+++ siade/spec/organizers/retriever_organizer_spec.rb
@@ -1,16 +1,30 @@
 RSpec.describe RetrieverOrganizer, type: :organizer do
   before(:all) do
+    class DummyRetrieverInteractor < ApplicationInteractor
+      def call
+        case context.error_kind
+        when :not_found
+          context.errors << NotFoundError.new(context.provider_name)
+          context.fail!
+        when :provider_error
+          context.errors << ProviderUnknownError.new(context.provider_name, 'whatever')
+          context.fail!
+        end
+      end
+    end
+
     class DummyRetrieverOrganizer < RetrieverOrganizer
+      organize DummyRetrieverInteractor
+
       def provider_name
         context.provider_name
       end
     end
   end
 
-  subject { DummyRetrieverOrganizer.call(params.merge(provider_name: provider_name)).status }
 
   describe 'provider_name method' do
-    let(:params) { { set_status: 418 } }
+    subject { DummyRetrieverOrganizer.call(provider_name: provider_name) }
 
     context 'when it is not a valid provider name' do
       let(:provider_name) { 'Invalid' }
@@ -25,10 +39,64 @@ RSpec.describe RetrieverOrganizer, type: :organizer do
     context 'when it is a valid provider name' do
       let(:provider_name) { 'INSEE' }
 
+      it { is_expected.to be_a_success }
+
       it 'does not raise an error' do
         expect {
           subject
         }.not_to raise_error
+      end
+    end
+  end
+
+  describe 'errors tracking' do
+    subject { DummyRetrieverOrganizer.call(provider_name: provider_name, error_kind: error_kind) }
+
+    let(:provider_name) { 'INSEE' }
+    let(:monitoring_service) { double('monitoring_service') }
+
+    before do
+      allow(MonitoringService).to receive(:instance).and_return(monitoring_service)
+      allow(monitoring_service).to receive(:track_provider_error)
+    end
+
+    context 'when there is no error' do
+      let(:error_kind) { nil }
+
+      it { is_expected.to be_a_success }
+
+      it 'does not track error' do
+        subject
+
+        expect(monitoring_service).not_to have_received(:track_provider_error)
+      end
+    end
+
+    context 'when there is an error' do
+      context 'when it is a not a provider error (not found)' do
+        let(:error_kind) { :not_found }
+
+        it { is_expected.to be_a_failure }
+
+        it 'does not track error' do
+          subject
+
+          expect(monitoring_service).not_to have_received(:track_provider_error)
+        end
+      end
+
+      context 'when it is a provider error' do
+        let(:error_kind) { :provider_error }
+
+        it { is_expected.to be_a_failure }
+
+        it 'tracks this error' do
+          subject
+
+          expect(monitoring_service).to have_received(:track_provider_error).with(
+            instance_of(ProviderUnknownError)
+          )
+        end
       end
     end
   end
