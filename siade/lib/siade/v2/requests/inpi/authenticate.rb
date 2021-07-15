388d4f15aea1356c63f836e35af814ef7b2d9156
Loïc Delmaire <loic.delmaire@gmail.com>
2021-07-15 17:41:52 +0200

    SIADE::V2::Requests::INPI::Authenticate handles SocketError
    Closes https://gitlab.com/etalab/api-entreprise/siade/-/issues/127
    Closes https://sentry.data.gouv.fr/organizations/sentry/issues/12113

--- siade/lib/siade/v2/requests/inpi/authenticate.rb
+++ siade/lib/siade/v2/requests/inpi/authenticate.rb
@@ -18,7 +18,7 @@ class SIADE::V2::Requests::INPI::Authenticate
     Net::HTTP.start(uri.hostname, uri.port, request_options) do |http|
       http.request request
     end
-  rescue Net::OpenTimeout, Net::ReadTimeout, Errno::ECONNREFUSED, Errno::ECONNRESET
+  rescue Net::OpenTimeout, Net::ReadTimeout, Errno::ECONNREFUSED, Errno::ECONNRESET, SocketError
     nil
   end
 
