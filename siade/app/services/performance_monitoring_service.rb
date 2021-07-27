a4f1c41a009f493686557fc5f2495e7f843676c4
Loïc Delmaire <loic.delmaire@gmail.com>
2021-07-27 07:00:39 +0200

    Introduce basic service to track performance

---
+++ siade/app/services/performance_monitoring_service.rb
@@ -0,0 +1,27 @@
+require 'singleton'
+
+class PerformanceMonitoringService
+  include Singleton
+
+  attr_writer :enable
+
+  def track(op:, description:)
+    transaction = Sentry&.get_current_scope&.get_transaction
+
+    if transaction && enable?
+      result = nil
+
+      transaction.with_child_span(op: op, description: description) do
+        result = yield
+      end
+
+      result
+    else
+      yield
+    end
+  end
+
+  def enable?
+    @enable
+  end
+end
