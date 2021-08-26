eaeaeef86ffa419336dc1e33dd7b1ad98df27c28
Alexis Leclerc <alexis@alxs.fr>
2021-08-26 15:57:10 +0200

    feat: add RetrieverGenerator

---
+++ siade/lib/generators/retriever/retriever_generator.rb
@@ -0,0 +1,36 @@
+class RetrieverGenerator < BaseGenerator
+  source_root File.expand_path('templates', __dir__)
+
+  class_option :document,
+    type:    :boolean,
+    default: false,
+    desc:    'Add support of documents uploader'
+
+  class_option :validation_type,
+    type: :string,
+    default: 'siren',
+    desc: 'Does the API needs to validate: siren, siret or custom ?'
+
+  desc 'See `bin/rails generate scaffold_resource --help` instead'
+  def create_retriever
+    template 'retriever.rb.erb', File.join('app/organizers', provider_namespace.underscore, "#{resource_class.underscore}.rb")
+    template 'retriever_spec.rb.erb', File.join('spec/organizers', provider_namespace.underscore, "#{resource_class.underscore}_spec.rb")
+  end
+
+  private
+
+  def document_resource?
+    options[:document]
+  end
+
+  def validation_class
+    case options[:validation_type].downcase
+    when 'siren'
+      'ValidateSiren'
+    when 'siret'
+      'ValidateSiret'
+    else
+      "#{name}::ValidateParams"
+    end
+  end
+end
---
+++ siade/spec/lib/generators/retriever/retriever_generator_spec.rb
@@ -0,0 +1,76 @@
+RSpec.describe RetrieverGenerator, type: :generator do
+  include_context 'with generator'
+
+  let(:resource_name) { 'MODULE::ResourceName' }
+
+  describe 'RetrieverOrganizer class file' do
+    subject { file('app/organizers/module/resource_name.rb') }
+
+    shared_examples 'a valid RetrieverOrganizer' do
+
+      it { is_expected.to exist }
+      it { is_expected.to have_correct_syntax }
+      it { is_expected.to contain(/class #{resource_name} < RetrieverOrganizer/) }
+      it { is_expected.to have_method 'provider_name' }
+    end
+
+    context 'with option: --validation_type SIREN' do
+      before { run_generator [resource_name, '--validation_type', 'siren'] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.to contain(/organize ValidateSiren/) }
+    end
+
+    context 'with option: --validation_type SIRET' do
+      before { run_generator [resource_name, '--validation_type', 'siret'] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.to contain(/organize ValidateSiret/) }
+    end
+
+    context 'with option: --validation_type CUSTOM' do
+      before { run_generator [resource_name, '--validation_type', 'custom'] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.to contain(/organize #{resource_name}::ValidateParams/) }
+    end
+
+    context 'without option: --validation_type' do
+      before { run_generator [resource_name] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.to contain(/organize ValidateSiren/) }
+    end
+
+    context 'with option: --document false' do
+      before { run_generator [resource_name, '--document', 'false'] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.not_to contain(/UploadDocument/) }
+    end
+
+    context 'with option: --document true' do
+      before { run_generator [resource_name, '--document', 'true'] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.to contain(/#{resource_name}::UploadDocument/) }
+    end
+
+    context 'without option: --document' do
+      before { run_generator [resource_name] }
+
+      it_behaves_like 'a valid RetrieverOrganizer'
+      it { is_expected.not_to contain(/UploadDocument/) }
+    end
+  end
+
+  describe 'RetrieverOrganizer spec file' do
+    subject { file('spec/organizers/module/resource_name_spec.rb') }
+
+    before { run_generator [resource_name] }
+
+    it { is_expected.to exist }
+    it { is_expected.to have_correct_syntax }
+    it { is_expected.to contain(/RSpec.describe #{resource_name}, type: :retriever_organizer do/) }
+  end
+end
