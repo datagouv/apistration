16994f1db686d254f021e7103fe45c2a98d23121
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-14 13:29:10 +0200

    Remove CodeToLinelleNafService

--- siade/spec/services/code_to_libelle_naf_service_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe CodeToLibelleNafService do
-  describe '.libelle' do
-    it 'return associate libelle to code naf given' do
-      expect(CodeToLibelleNafService.libelle('0112Z')).to eq('Culture du riz')
-      expect(CodeToLibelleNafService.libelle('1813Z')).to eq('Activités de pré-presse')
-    end
-  end
-end
