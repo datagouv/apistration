3deb6a2906ad7cfa164d8ac325c05d831e39d231
Samuelfaure <samuel.faure.dev@gmail.com>
2021-09-21 13:48:47 +0200

    Fixing diff whitespace in swagger test

--- siade/bin/test_swagger.sh
+++ siade/bin/test_swagger.sh
@@ -2,5 +2,5 @@
 
 cp swagger/openapi.yaml swagger/openapi-versionned.yaml &&
   ./bin/generate_swagger.sh &&
-  (diff swagger/openapi.yaml swagger/openapi-versionned.yaml || (echo "Swagger file is different after generation" && exit 1)) &&
+  (diff --ignore-all-space swagger/openapi.yaml swagger/openapi-versionned.yaml || (echo "Swagger file is different after generation" && exit 1)) &&
   exit 0
