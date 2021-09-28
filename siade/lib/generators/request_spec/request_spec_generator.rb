17c5611845d38470d34fe6dd009eceb3d906e9fd
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-28 12:43:03 +0200

    Generators: introduce request_spec
    Can specify param path (siren, siret...)
    
    Ref https://gitlab.com/etalab/api-entreprise/siade/-/issues/149

---
+++ siade/lib/generators/request_spec/request_spec_generator.rb
@@ -0,0 +1,19 @@
+class RequestSpecGenerator < BaseGenerator
+  source_root File.expand_path('templates', __dir__)
+
+  class_option :validation_type,
+    type: :string,
+    default: 'siren',
+    desc: 'Does the API needs to validate: siren, siret or custom ?'
+
+  desc 'See `bin/rails generate scaffold_resource --help` instead'
+  def create_request_spec
+    template 'request_spec.rb.erb', File.join('spec/requests/api/v3_and_more/', provider_namespace.underscore, resource_class.underscore, 'v3_spec.rb')
+  end
+
+  private
+
+  def id_path_param
+    options[:validation_type].downcase
+  end
+end
---
+++ siade/spec/lib/generators/request_spec/request_spec_generator_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe RequestSpecGenerator, type: :generator do
+  subject { file('spec/requests/api/v3_and_more/module/resource_name/v3_spec.rb') }
+
+  let(:resource_name) { 'MODULE::ResourceName' }
+
+  before { run_generator [resource_name] }
+
+  it { is_expected.to exist }
+  it { is_expected.to have_correct_syntax }
+  it { is_expected.to contain(%r{/v3/module/resource_names}) }
+end
