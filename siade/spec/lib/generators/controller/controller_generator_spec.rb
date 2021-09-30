0f89e34f1ef7fbcc492ca72d49d06e5e283f3af0
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-30 15:28:43 +0200

    Generator: enhance controller generator

--- siade/spec/lib/generators/controller/controller_generator_spec.rb
+++ siade/spec/lib/generators/controller/controller_generator_spec.rb
@@ -11,12 +11,4 @@ RSpec.describe ControllerGenerator, type: :generator do
     it { is_expected.to contain(/class API::V3AndMore::#{resource_name}Controller < API::V3AndMore::BaseController/) }
     it { is_expected.to have_method 'show' }
   end
-
-  describe 'Controller spec file' do
-    subject { file('spec/controllers/api/v3_and_more/module/resource_name_controller_spec.rb') }
-
-    it { is_expected.to exist }
-    it { is_expected.to have_correct_syntax }
-    it { is_expected.to contain(/RSpec.describe API::V3AndMore::#{resource_name}Controller, type: :controller do/) }
-  end
 end
