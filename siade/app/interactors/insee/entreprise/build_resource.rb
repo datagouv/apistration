cd1ebb70b00540b25558daf8fabdb8cb3ba6cff6
Loïc Delmaire <loic.delmaire@gmail.com>
2021-09-30 13:47:40 +0200

    Introduce INSEE::Entreprise::BuildResource

---
+++ siade/app/interactors/insee/entreprise/build_resource.rb
@@ -0,0 +1,122 @@
+class INSEE::Entreprise::BuildResource < BuildResource
+  protected
+
+  def resource_attributes
+    {
+      id: siren,
+      siret_siege_social: "#{unite_legale_json['siren']}#{latest_info_on_unite_legale['nicSiegeUniteLegale']}",
+
+      type: type_of_person,
+
+      personne_morale_attributs: {
+        raison_sociale: latest_info_on_unite_legale['denominationUniteLegale']
+      },
+
+      personne_physique_attributs: {
+        pseudonyme: unite_legale_json['pseudonymeUniteLegale'],
+        prenom_usuel: unite_legale_json['prenomUsuelUniteLegale'],
+        prenom_1: unite_legale_json['prenom1UniteLegale'],
+        prenom_2: unite_legale_json['prenom2UniteLegale'],
+        prenom_3: unite_legale_json['prenom3UniteLegale'],
+        prenom_4: unite_legale_json['prenom4UniteLegale'],
+        nom_usage: latest_info_on_unite_legale['nomUsageUniteLegale'],
+        nom_naissance: latest_info_on_unite_legale['nomUniteLegale'],
+        sexe: unite_legale_json['sexeUniteLegale']
+      },
+
+      categorie_entreprise: unite_legale_json['categorieEntreprise'],
+      numero_tva_intracommunautaire: compute_numero_tva_intracommunautaire,
+      diffusable_commercialement: yes_no_to_boolean(unite_legale_json['statutDiffusionUniteLegale']),
+
+      forme_juridique: referential(
+        'categorie_juridique',
+        code: categorie_juridique
+      ),
+      activite_principale: referential(
+        'activite_principale',
+        code: latest_info_on_unite_legale['activitePrincipaleUniteLegale'],
+        nomenclature: latest_info_on_unite_legale['nomenclatureActivitePrincipaleUniteLegale']
+      ),
+      tranche_effectif_salarie: referential(
+        'tranche_effectif_salarie',
+        code: unite_legale_json['trancheEffectifsUniteLegale'],
+        date_reference: unite_legale_json['anneeEffectifsUniteLegale']
+      ),
+
+      date_creation: date_to_timestamp(unite_legale_json['dateCreationUniteLegale']),
+
+      etat_administratif: etat_administratif,
+      date_cessation: date_cessation,
+
+      date_derniere_mise_a_jour: date_to_timestamp(unite_legale_json['dateDernierTraitementUniteLegale'])
+    }
+  end
+
+  private
+
+  def unite_legale_json
+    @unite_legale_json ||= json_body['uniteLegale']
+  end
+
+  def latest_info_on_unite_legale
+    unite_legale_json['periodesUniteLegale'].find do |periode_unite_legale|
+      periode_unite_legale['dateFin'].nil?
+    end
+  end
+
+  def compute_numero_tva_intracommunautaire
+    cle_tva = ((12 + 3 * (siren.to_i % 97)) % 97).to_s
+    padded_cle_tva = cle_tva.rjust(2, '0')
+    "FR#{padded_cle_tva}#{siren}"
+  end
+
+  def date_cessation
+    return if entreprise_active?
+
+    date_to_timestamp(periode_with_cessation_status['dateDebut'])
+  end
+
+  def periode_with_cessation_status
+    unite_legale_json['periodesUniteLegale'].find do |periode_unite_legale|
+      periode_unite_legale['changementEtatAdministratifUniteLegale']
+    end
+  end
+
+  def entreprise_active?
+    etat_administratif == 'A'
+  end
+
+  def etat_administratif
+    latest_info_on_unite_legale['etatAdministratifUniteLegale']
+  end
+
+  def type_of_person
+    if categorie_juridique == '1000'
+      :personne_physique
+    else
+      :personne_morale
+    end
+  end
+
+  def categorie_juridique
+    latest_info_on_unite_legale['categorieJuridiqueUniteLegale']
+  end
+
+  def siren
+    unite_legale_json['siren']
+  end
+
+  def referential(name, params)
+    SIADE::V2::Referentials.const_get(name.classify).new(params).as_json
+  end
+
+  def yes_no_to_boolean(value)
+    value && value == 'O'
+  end
+
+  def date_to_timestamp(value)
+    return if value.nil?
+
+    Date.parse(value).to_time.to_i
+  end
+end
---
+++ siade/spec/interactors/insee/entreprise/build_resource_spec.rb
@@ -0,0 +1,138 @@
+RSpec.describe INSEE::Entreprise::BuildResource, type: :build_resource do
+  subject { organizer }
+
+  let(:organizer) { described_class.call(response: response) }
+  let(:response) { instance_double('Net::HTTPOK', body: body) }
+
+  let(:body) do
+    INSEE::Entreprise::MakeRequest.call(params: params, token: 'token').response.body
+  end
+
+  let(:params) do
+    {
+      siren: siren
+    }
+  end
+
+  context 'with an active GE, which is a personne morale', vcr: { cassette_name: 'api_insee_fr/siren/active_GE' } do
+    let(:siren) { sirens_insee_v3[:active_GE] }
+
+    it { is_expected.to be_a_success }
+
+    describe 'resource' do
+      subject { organizer.resource }
+
+      it { is_expected.to be_a(Resource) }
+
+      its(:id) { is_expected.to eq(siren) }
+      its(:siret_siege_social) { is_expected.to eq("#{siren}01294") }
+
+      its(:categorie_entreprise) { is_expected.to eq('GE') }
+      its(:numero_tva_intracommunautaire) { is_expected.to eq('FR51306138900') }
+      its(:diffusable_commercialement) { is_expected.to eq(true) }
+
+      its(:type) { is_expected.to eq(:personne_morale) }
+
+      its(:personne_morale_attributs) do
+        is_expected.to eq({
+          raison_sociale: 'DECATHLON',
+        })
+      end
+
+      its(:personne_physique_attributs) do
+        is_expected.to eq({
+          pseudonyme: nil,
+          prenom_usuel: nil,
+          prenom_1: nil,
+          prenom_2: nil,
+          prenom_3: nil,
+          prenom_4: nil,
+          nom_usage: nil,
+          nom_naissance: nil,
+          sexe: nil
+        })
+      end
+
+      its(:forme_juridique) do
+        is_expected.to eq({
+          code: '5599',
+          libelle: 'SA à conseil d\'administration (s.a.i.)'
+        })
+      end
+
+      its(:activite_principale) do
+        is_expected.to eq({
+          code: '46.49Z',
+          libelle: 'Commerce de gros (commerce interentreprises) d\'autres biens domestiques',
+          nomenclature: 'NAFRev2'
+        })
+      end
+
+      its(:tranche_effectif_salarie) do
+        is_expected.to eq({
+          de: 2000,
+          a: 4999,
+          code: '51',
+          intitule: '2 000 à 4 999 salariés',
+          date_reference: '2016'
+        })
+      end
+
+      its(:date_creation) { is_expected.to eq(Date.parse('1977-01-01').to_time.to_i) }
+
+      its(:etat_administratif) { is_expected.to eq('A') }
+      its(:date_cessation) { is_expected.to eq(nil) }
+
+      its(:date_derniere_mise_a_jour) { is_expected.to eq(Date.parse('2018-02-13').to_time.to_i) }
+    end
+  end
+
+  context 'with an active AE, which is a personne physique', vcr: { cassette_name: 'api_insee_fr/siren/active_AE' } do
+    let(:siren) { sirens_insee_v3[:active_AE] }
+
+    it { is_expected.to be_a_success }
+
+    describe 'resource' do
+      subject { organizer.resource }
+
+      it { is_expected.to be_a(Resource) }
+
+      its(:type) { is_expected.to eq(:personne_physique) }
+
+      its(:personne_morale_attributs) do
+        is_expected.to eq({
+          raison_sociale: nil,
+        })
+      end
+
+      its(:personne_physique_attributs) do
+        is_expected.to eq({
+          pseudonyme: nil,
+          prenom_usuel: 'ODILE',
+          prenom_1: 'ODILE',
+          prenom_2: 'RENEE',
+          prenom_3: 'JANINE',
+          prenom_4: nil,
+          nom_usage: 'BAROIN',
+          nom_naissance: 'BIDAU',
+          sexe: 'F'
+        })
+      end
+    end
+  end
+
+  context 'with a ceased company', vcr: { cassette_name: 'api_insee_fr/siren/ceased' } do
+    let(:siren) { sirens_insee_v3[:ceased] }
+
+    it { is_expected.to be_a_success }
+
+    describe 'resource' do
+      subject { organizer.resource }
+
+      it { is_expected.to be_a(Resource) }
+
+      its(:etat_administratif) { is_expected.to eq('C') }
+      its(:date_cessation) { is_expected.to eq(Date.parse('2011-09-05').to_time.to_i) }
+    end
+  end
+end
