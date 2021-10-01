1db3558d8ff087850688ac00fba35ba11b6aad60
Samuelfaure <samuel.faure.dev@gmail.com>
2021-10-01 12:06:55 +0200

    Endpoint V3/MandatairesSociaux: Controller

---
+++ siade/app/controllers/api/v3_and_more/infogreffe/mandataires_sociaux_controller.rb
@@ -0,0 +1,26 @@
+class API::V3AndMore::Infogreffe::MandatairesSociauxController < API::V3AndMore::BaseController
+  def show
+    authorize :mandataires_sociaux
+
+    organizer = ::Infogreffe::MandatairesSociaux.call(params: organizer_params)
+
+    if organizer.success?
+      render json: serializer_class.new(organizer.resource_collection).serializable_hash,
+        status: extract_http_code(organizer)
+    else
+      render_errors(organizer)
+    end
+  end
+
+  private
+
+  def organizer_params
+    {
+      siren: params.require(:siren)
+    }
+  end
+
+  def serializer_module
+    ::Infogreffe::MandataireSocialSerializer
+  end
+end
