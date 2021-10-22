481fa1101b3c0ab6bdcc52d45e79257c109f310c
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-22 11:30:19 +0200

    Remove controller_spec generator, which is useless for v3

--- siade/lib/generators/controller/controller_generator.rb
+++ siade/lib/generators/controller/controller_generator.rb
@@ -4,6 +4,5 @@ class ControllerGenerator < BaseGenerator
   desc 'See `bin/rails generate scaffold_resource --help` instead'
   def create_controller
     template 'controller.rb.erb', File.join('app/controllers/api/v3_and_more', provider_namespace.underscore, "#{resource_class.underscore}_controller.rb")
-    template 'controller_spec.rb.erb', File.join('spec/controllers/api/v3_and_more', provider_namespace.underscore, "#{resource_class.underscore}_controller_spec.rb")
   end
 end
