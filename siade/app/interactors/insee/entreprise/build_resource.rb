1238bebfd336ee03af39d8a240cda0121d5bc364
Loïc Delmaire <loic.delmaire@gmail.com>
2021-10-22 15:36:09 +0200

    Scout commit: rename method in INSEE::Entreprise::BuildResource

--- siade/app/interactors/insee/entreprise/build_resource.rb
+++ siade/app/interactors/insee/entreprise/build_resource.rb
@@ -4,7 +4,7 @@ class INSEE::Entreprise::BuildResource < BuildResource
   def resource_attributes
     {
       id: siren,
-      siret_siege_social: "#{unite_legale_json['siren']}#{latest_info_on_unite_legale['nicSiegeUniteLegale']}",
+      siret_siege_social: "#{unite_legale['siren']}#{latest_info_on_unite_legale['nicSiegeUniteLegale']}",
 
       type: type_of_person,
 
@@ -13,20 +13,20 @@ class INSEE::Entreprise::BuildResource < BuildResource
       },
 
       personne_physique_attributs: {
-        pseudonyme: unite_legale_json['pseudonymeUniteLegale'],
-        prenom_usuel: unite_legale_json['prenomUsuelUniteLegale'],
-        prenom_1: unite_legale_json['prenom1UniteLegale'],
-        prenom_2: unite_legale_json['prenom2UniteLegale'],
-        prenom_3: unite_legale_json['prenom3UniteLegale'],
-        prenom_4: unite_legale_json['prenom4UniteLegale'],
+        pseudonyme: unite_legale['pseudonymeUniteLegale'],
+        prenom_usuel: unite_legale['prenomUsuelUniteLegale'],
+        prenom_1: unite_legale['prenom1UniteLegale'],
+        prenom_2: unite_legale['prenom2UniteLegale'],
+        prenom_3: unite_legale['prenom3UniteLegale'],
+        prenom_4: unite_legale['prenom4UniteLegale'],
         nom_usage: latest_info_on_unite_legale['nomUsageUniteLegale'],
         nom_naissance: latest_info_on_unite_legale['nomUniteLegale'],
-        sexe: unite_legale_json['sexeUniteLegale']
+        sexe: unite_legale['sexeUniteLegale']
       },
 
-      categorie_entreprise: unite_legale_json['categorieEntreprise'],
+      categorie_entreprise: unite_legale['categorieEntreprise'],
       numero_tva_intracommunautaire: compute_numero_tva_intracommunautaire,
-      diffusable_commercialement: yes_no_to_boolean(unite_legale_json['statutDiffusionUniteLegale']),
+      diffusable_commercialement: yes_no_to_boolean(unite_legale['statutDiffusionUniteLegale']),
 
       forme_juridique: referential(
         'categorie_juridique',
@@ -39,27 +39,27 @@ class INSEE::Entreprise::BuildResource < BuildResource
       ),
       tranche_effectif_salarie: referential(
         'tranche_effectif_salarie',
-        code: unite_legale_json['trancheEffectifsUniteLegale'],
-        date_reference: unite_legale_json['anneeEffectifsUniteLegale']
+        code: unite_legale['trancheEffectifsUniteLegale'],
+        date_reference: unite_legale['anneeEffectifsUniteLegale']
       ),
 
-      date_creation: date_to_timestamp(unite_legale_json['dateCreationUniteLegale']),
+      date_creation: date_to_timestamp(unite_legale['dateCreationUniteLegale']),
 
       etat_administratif: etat_administratif,
       date_cessation: date_cessation,
 
-      date_derniere_mise_a_jour: date_to_timestamp(unite_legale_json['dateDernierTraitementUniteLegale'])
+      date_derniere_mise_a_jour: date_to_timestamp(unite_legale['dateDernierTraitementUniteLegale'])
     }
   end
 
   private
 
-  def unite_legale_json
-    @unite_legale_json ||= json_body['uniteLegale']
+  def unite_legale
+    @unite_legale ||= json_body['uniteLegale']
   end
 
   def latest_info_on_unite_legale
-    unite_legale_json['periodesUniteLegale'].find do |periode_unite_legale|
+    unite_legale['periodesUniteLegale'].find do |periode_unite_legale|
       periode_unite_legale['dateFin'].nil?
     end
   end
@@ -75,7 +75,7 @@ class INSEE::Entreprise::BuildResource < BuildResource
   end
 
   def periode_with_cessation_status
-    unite_legale_json['periodesUniteLegale'].find do |periode_unite_legale|
+    unite_legale['periodesUniteLegale'].find do |periode_unite_legale|
       periode_unite_legale['changementEtatAdministratifUniteLegale']
     end
   end
@@ -101,7 +101,7 @@ class INSEE::Entreprise::BuildResource < BuildResource
   end
 
   def siren
-    unite_legale_json['siren']
+    unite_legale['siren']
   end
 
   def referential(name, params)
