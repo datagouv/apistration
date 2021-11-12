ee840ca28ea996072900fca0d2e319ba0df76861
Samuelfaure <samuel.faure.dev@gmail.com>
2021-11-12 14:17:49 +0100

    V3 douanes/eori: ValidateResponse

---
+++ siade/app/interactors/douanes/eori/validate_response.rb
@@ -0,0 +1,11 @@
+class Douanes::EORI::ValidateResponse < ValidateResponse
+  def call
+    return if http_ok?
+
+    if http_not_found?
+      resource_not_found!
+    else
+      unknown_provider_response!
+    end
+  end
+end
