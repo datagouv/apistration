e8df968151bb337f5652437568867e088d27e004
Samuelfaure <samuel.faure.dev@gmail.com>
2021-11-12 14:51:43 +0100

    V3 douanes/eori: Organizer

---
+++ siade/app/organizers/douanes/eori.rb
@@ -0,0 +1,10 @@
+class Douanes::EORI < RetrieverOrganizer
+  organize ValidateSiretOrEORI,
+           Douanes::EORI::MakeRequest,
+           Douanes::EORI::ValidateResponse,
+           Douanes::EORI::BuildResource
+
+  def provider_name
+    'Douanes'
+  end
+end
---
+++ siade/spec/organizers/douanes/eori_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe Douanes::EORI, type: :retriever_organizer do
+  describe '.call' do
+    subject { described_class.call(params: params) }
+
+    let(:params) do
+      {
+        siret_or_eori: siret_or_eori
+      }
+    end
+
+    context 'with valid eori', vcr: { cassette_name: 'douanes/eori/valid_eori' } do
+      let(:siret_or_eori) { valid_eori }
+
+      it { is_expected.to be_a_success }
+
+      its(:resource) { is_expected.to be_present }
+    end
+
+    context 'with valid spanish eori', vcr: { cassette_name: 'douanes/eori/valid_spanish_eori' } do
+      let(:siret_or_eori) { valid_spanish_eori }
+
+      it { is_expected.to be_a_success }
+
+      its(:resource) { is_expected.to be_present }
+    end
+
+    context 'with invalid eori', vcr: { cassette_name: 'douanes/eori/invalid_eori_format' } do
+      let(:siret_or_eori) { invalid_eori }
+
+      it { is_expected.to be_a_failure }
+    end
+  end
+end
