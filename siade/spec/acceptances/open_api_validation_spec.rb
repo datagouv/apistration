be43d589930dc05be5f6fe592926e591e038cfd6
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-18 09:44:11 +0100

    OpenAPI: test if required attributes are present and kind of valid
    In one test we check if it's not the latest description (it's a bug from
    RSwag which takes the latest definition from the previous response
    description, dunno why)
    
    Closes https://gitlab.com/etalab/api-entreprise/siade/-/issues/179

--- siade/spec/acceptances/open_api_validation_spec.rb
+++ siade/spec/acceptances/open_api_validation_spec.rb
@@ -25,5 +25,25 @@ RSpec.describe 'OpenAPI file', type: :acceptance do
       document = Openapi3Parser.load_file(definition_path)
       expect(document).to be_valid, "Errors: #{document.errors.to_h}"
     end
+
+    it 'has all required keys for each endpoint' do
+      YAML.load_file(definition_path)['paths'].each do |path, data|
+        required_keys = %w[
+          tags
+          description
+        ]
+        actual_keys = data['get'].keys
+
+        expect(actual_keys).to include(*required_keys), "#{path} has missing keys: #{required_keys - actual_keys}"
+      end
+    end
+
+    it 'has a valid description for each endpoint' do
+      description_inherited_by_latest_response_definition = 'Le jeton ne possède pas les droits nécessaires'
+
+      YAML.load_file(definition_path)['paths'].each do |path, data|
+        expect(data['get']['description']).not_to eq(description_inherited_by_latest_response_definition), "#{path} has an invalid description"
+      end
+    end
   end
 end
