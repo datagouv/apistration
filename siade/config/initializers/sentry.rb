76d73237c44fb43fbe4369fe856ba9f9e4791fc8
Alexis Leclerc <alexis@alxs.fr>
2021-11-18 16:02:39 +0100

    Enable Sentry in staging env

--- siade/config/initializers/sentry.rb
+++ siade/config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = Siade.credentials[:sentry_url]
 
   config.breadcrumbs_logger = [:active_support_logger]
-  config.enabled_environments = %w[production]
+  config.enabled_environments = %w[production staging]
 
   config.traces_sample_rate = 1.0
 end
