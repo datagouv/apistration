2b4392145abc89618c3a2933910017bf293da516
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-19 11:40:54 +0100

    INSEE::Etablissement::ValidateResponse: change 403 message
    It's a siret, not a siren.

--- siade/app/interactors/insee/etablissement/validate_response.rb
+++ siade/app/interactors/insee/etablissement/validate_response.rb
@@ -1,2 +1,7 @@
 class INSEE::Etablissement::ValidateResponse < INSEE::UniteLegale::ValidateResponse
+  private
+
+  def unavailable_for_legal_reason_message
+    'Le siret demandé est une entité pour laquelle aucun organisme ne peut avoir accès.'
+  end
 end
