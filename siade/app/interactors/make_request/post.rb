586e9b277d770955115a10f2820dd8a87c2cd031
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-24 11:37:08 +0100

    Enhance MakeRequest::Post
    Some providers do not use request params => defined as empty json
    Some providers do use form data => defined as empty json

--- siade/app/interactors/make_request/post.rb
+++ siade/app/interactors/make_request/post.rb
@@ -9,12 +9,17 @@ class MakeRequest::Post < MakeRequest
     {}
   end
 
+  def form_data
+    {}
+  end
+
   private
 
   def api_call
     context.response = http_wrapper do
       request = Net::HTTP::Post.new(request_uri)
-      request.body = build_request_body
+      request.body = build_request_body if request_params.any?
+      request.set_form_data(form_data) if form_data.any?
       request
     end
   end
