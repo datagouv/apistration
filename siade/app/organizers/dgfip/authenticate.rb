87223dff4b1edd3f53ba08d6f386148aaef7bec0
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-24 13:34:19 +0100

    Introduce DGFIP::Authenticate organizer

---
+++ siade/app/organizers/dgfip/authenticate.rb
@@ -0,0 +1,8 @@
+class DGFIP::Authenticate < ApplicationOrganizer
+  organize DGFIP::Authenticate::MakeRequest,
+    DGFIP::Authenticate::ValidateResponse
+
+  after do
+    context.cookie = context.response['set-cookie']
+  end
+end
