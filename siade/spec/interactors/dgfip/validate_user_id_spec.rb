7e7a11726acaf5f16bd5072889ad3f39ed40bdb6
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-26 09:18:28 +0100

    Introduce DGFIP::ValidateUserId

---
+++ siade/spec/interactors/dgfip/validate_user_id_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe DGFIP::ValidateUserId, type: :validate_param do
+  subject { described_class.call(params: params) }
+
+  let(:params) { { user_id: user_id } }
+
+  context 'with a user_id which is a uuid' do
+    let(:user_id) { SecureRandom.uuid }
+
+    it { is_expected.to be_a_success }
+  end
+
+  context 'with a user_id which is not a uuid' do
+    let(:user_id) { "#{SecureRandom.uuid}whatever" }
+
+    it { is_expected.to be_a_failure }
+    its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+  end
+end
