0d6dd678e19bbeb5d29f4a709f14647709434cfd
Loïc Delmaire <loic.delmaire@gmail.com>
2021-11-26 10:56:35 +0100

    Introduce DGFIP::LiassesFiscales::Declarations::MakeRequest

---
+++ siade/app/interactors/dgfip/liasses_fiscales/declarations/make_request.rb
@@ -0,0 +1,29 @@
+class DGFIP::LiassesFiscales::Declarations::MakeRequest < MakeRequest::Get
+  protected
+
+  def request_uri
+    URI(Siade.credentials[:dgfip_liasse_fiscale_declaration_url])
+  end
+
+  def set_headers(request)
+    request['Cookie'] = context.cookie
+  end
+
+  def request_params
+    {
+      annee: params[:year],
+      siren: params[:siren],
+      userId: user_id_sanitized
+    }
+  end
+
+  def params
+    context.params
+  end
+
+  private
+
+  def user_id_sanitized
+    UserIdDGFIPService.call(params[:user_id])
+  end
+end
---
+++ siade/spec/interactors/dgfip/liasses_fiscales/declarations/make_request_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe DGFIP::LiassesFiscales::Declarations::MakeRequest, type: :make_request do
+  subject { described_class.call(cookie: cookie, params: params) }
+
+  let(:params) do
+    {
+      siren: siren,
+      user_id: user_id,
+      year: year
+    }
+  end
+  let(:cookie) { DGFIP::Authenticate.call.cookie }
+
+  describe 'happy path', vcr: { cassette_name: 'dgfip/liasses_fiscales/valid' } do
+    let(:siren) { valid_siren(:liasse_fiscale) }
+    let(:user_id) { valid_dgfip_user_id }
+    let(:year) { 2017 }
+
+    it { is_expected.to be_a_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPOK) }
+  end
+
+  context 'with a non existent siren', vcr: { cassette_name: 'dgfip/liasses_fiscales/with_non_existent_siren' } do
+    let(:siren) { non_existent_siren }
+
+    let(:user_id) { valid_dgfip_user_id }
+    let(:year) { 2017 }
+
+    it { is_expected.to be_a_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPNotFound) }
+  end
+end
