5e4b959da3085b4d27728acd41f029ab7e158324
Loïc Delmaire <loic.delmaire@gmail.com>
2022-01-05 20:11:43 +0100

    Ruby 3 migration: verify_mode no longer accepts boolean

--- siade/lib/siade/v2/requests/infogreffe.rb
+++ siade/lib/siade/v2/requests/infogreffe.rb
@@ -37,7 +37,7 @@ class SIADE::V2::Requests::Infogreffe < SIADE::V2::Requests::Generic
   end
 
   def net_http_options
-    { use_ssl: true, verify_mode: false }
+    { use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE }
   end
 
   def set_headers(request)
