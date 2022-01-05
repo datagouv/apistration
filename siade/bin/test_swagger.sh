573968625eb16d04e2659028fce6b39714f059d7
Loïc Delmaire <loic.delmaire@gmail.com>
2022-01-05 14:57:00 +0100

    Harden bin/test_swagger.sh
    Closes https://gitlab.com/etalab/api-entreprise/siade/-/issues/187

--- siade/bin/test_swagger.sh
+++ siade/bin/test_swagger.sh
@@ -2,5 +2,5 @@
 
 cp swagger/openapi.yaml swagger/openapi-versionned.yaml &&
   ./bin/generate_swagger.sh &&
-  (diff --ignore-all-space swagger/openapi.yaml swagger/openapi-versionned.yaml || (echo "Swagger file is different after generation" && exit 1)) &&
+  (ruby -e 'require "yaml" ; YAML.load_file("./swagger/openapi.yaml") == YAML.load_file("./swagger/openapi-versionned.yaml") ? exit(0) : exit(1)' || (echo "Swagger file is different after generation" && exit 1)) &&
   exit 0
