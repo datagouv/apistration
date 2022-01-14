64cf7996b376815d1d45f77d6aa6f95aa01d19fa
Samuelfaure <samuel.faure.dev@gmail.com>
2022-01-14 15:18:26 +0100

    V3/INPI Modeles: Routes

--- siade/config/routes.rb
+++ siade/config/routes.rb
@@ -88,6 +88,7 @@ Rails.application.routes.draw do
     namespace :inpi do
       get 'brevets/:siren' => '/api/v3_and_more/inpi/latest_brevets#show'
       get 'marques/:siren' => '/api/v3_and_more/inpi/latest_marques#show'
+      get 'modeles/:siren' => '/api/v3_and_more/inpi/latest_modeles#show'
     end
 
     namespace :insee do
