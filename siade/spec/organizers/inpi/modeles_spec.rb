bdf46cab5ac6e62aa8db8a1fa70918a587af2d11
Samuelfaure <samuel.faure.dev@gmail.com>
2022-01-14 15:19:56 +0100

    V3/INPI Modeles: Organizer

---
+++ siade/spec/organizers/inpi/modeles_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe INPI::Modeles, type: :retriever_organizer do
+  subject { described_class.call(params: params) }
+
+  let(:params) do
+    {
+      siren: siren,
+      limit: 3
+    }
+  end
+
+  context 'with valid siren', vcr: { cassette_name: 'inpi/modeles/with_valid_siren' } do
+    let(:siren) { valid_siren(:inpi) }
+
+    it { is_expected.to be_a_success }
+
+    its(:resource_collection) { is_expected.to be_present }
+    its(:meta) { is_expected.to be_present }
+  end
+
+  context 'with siren not found', vcr: { cassette_name: 'inpi/modeles/not_found_siren' } do
+    let(:siren) { not_found_siren(:inpi) }
+
+    it { is_expected.to be_a_failure }
+  end
+
+  context 'with invalid siren', vcr: { cassette_name: 'inpi/modeles/with_invalid_siren' } do
+    let(:siren) { 'invalid siren' }
+
+    it { is_expected.to be_a_failure }
+  end
+end
