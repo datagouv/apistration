67d392f90f654016d14ace8186a371756e8b12b7
Samuelfaure <samuel.faure.dev@gmail.com>
2022-01-14 15:18:47 +0100

    V3/INPI Modeles: Serializer

---
+++ siade/app/serializers/inpi/modeles_serializer/v3.rb
@@ -0,0 +1,14 @@
+class INPI::ModelesSerializer::V3 < JSONAPI::BaseSerializer
+  set_type :modele
+
+  link :notice, :notice_url
+
+  attributes :numero_depot,
+    :titre,
+    :total_representations,
+    :deposant,
+    :date_depot,
+    :date_publication,
+    :classe,
+    :reference
+end
