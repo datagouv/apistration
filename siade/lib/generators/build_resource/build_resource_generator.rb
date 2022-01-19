3cdb3ca63b4c2faaa4ce7020f8a4ab5ec5d0405d
Loïc Delmaire <loic.delmaire@gmail.com>
2022-01-19 13:41:02 +0100

    Enhance BuildResource generator

--- siade/lib/generators/build_resource/build_resource_generator.rb
+++ siade/lib/generators/build_resource/build_resource_generator.rb
@@ -6,6 +6,11 @@ class BuildResourceGenerator < BaseGenerator
     default: false,
     desc: 'Does the API returns some documents ?'
 
+  class_option :validation_type,
+    type: :string,
+    default: 'siren',
+    desc: 'Does the API needs to validate: siren, siret or custom ?'
+
   desc 'See `bin/rails generate scaffold_resource --help` instead'
   def create_build_resource
     template 'build_resource.rb.erb', File.join('app/interactors', provider_namespace.underscore, resource_class.underscore, 'build_resource.rb')
@@ -13,6 +18,17 @@ class BuildResourceGenerator < BaseGenerator
   end
 
   private
+
+  def id_name
+    return if options[:validation_type].nil?
+
+    case options[:validation_type].downcase
+    when 'siren'
+      'siren'
+    when 'siret'
+      'siret'
+    end
+  end
 
   def document_resource?
     options[:document]
