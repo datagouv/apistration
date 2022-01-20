b2231b1f3a012f91d495606b75c1716ddb7162a9
Samuelfaure <samuel.faure.dev@gmail.com>
2022-01-20 16:19:03 +0100

    V3/INPI Actes: Controller

---
+++ siade/app/controllers/api/v3_and_more/inpi/actes_controller.rb
@@ -0,0 +1,32 @@
+class API::V3AndMore::INPI::ActesController < API::V3AndMore::BaseController
+  def show
+    authorize :actes_inpi
+
+    organizer = ::INPI::Actes.call(params: organizer_params)
+
+    if organizer.success?
+      render json: serializer_class.new(organizer.resource_collection, options).serializable_hash,
+        status: extract_http_code(organizer)
+    else
+      render_errors(organizer)
+    end
+  end
+
+  private
+
+  def organizer_params
+    {
+      siren: params.require(:siren)
+    }
+  end
+
+  def serializer_module
+    ::INPI::ActesSerializer
+  end
+
+  def options
+    {
+      is_collection: true
+    }
+  end
+end
---
+++ siade/spec/requests/api/v3_and_more/inpi/actes/v3_spec.rb
@@ -0,0 +1,44 @@
+require 'swagger_helper'
+
+RSpec.describe 'INPI: Actes', type: %i[request swagger] do
+  path '/v3/inpi/actes/{siren}' do
+    get SwaggerInformation.get('inpi.actes.title') do
+      tags(*SwaggerInformation.get('inpi.actes.tags'))
+
+      common_action_attributes
+
+      parameter name: :siren, in: :path, type: :string
+
+      unauthorized_request do
+        let(:siren) { valid_siren(:inpi) }
+      end
+
+      forbidden_request do
+        let(:siren) { valid_siren(:inpi) }
+      end
+
+      describe 'with valid mandatory params', valid: true do
+        response '200', 'Actes trouvés', vcr: { cassette_name: 'inpi/actes/with_valid_siren' } do
+          description SwaggerInformation.get('inpi.actes.description')
+
+          rate_limit_headers
+
+          schema build_rswag_response_collection(
+            type: 'object',
+            properties: SwaggerInformation.get('inpi.actes.items.properties')
+          )
+
+          run_test!
+        end
+
+        response '404', 'Actes non trouvés', vcr: { cassette_name: 'inpi/actes/with_siren_not_found' } do
+          let(:siren) { not_found_siren(:inpi) }
+
+          schema '$ref' => '#/components/schemas/NotFound'
+
+          run_test!
+        end
+      end
+    end
+  end
+end
