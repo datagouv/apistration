4066bf87e4c0a24dc3ddb59e9f0eadf7480fadb0
Loïc Delmaire <loic.delmaire@gmail.com>
2022-01-20 18:13:46 +0100

    Introduce DGFIP::AttestationFiscale::MakeRequest

---
+++ siade/app/interactors/dgfip/attestation_fiscale/make_request.rb
@@ -0,0 +1,28 @@
+class DGFIP::AttestationFiscale::MakeRequest < MakeRequest::Get
+  protected
+
+  def set_headers(request)
+    request['Accept'] = 'application/pdf'
+    request['Cookie'] = context.cookie
+  end
+
+  def request_uri
+    URI(Siade.credentials[:dgfip_attestations_fiscales_url])
+  end
+
+  def request_params
+    {
+      userId: user_id_sanitized,
+      siren: context.params[:siren],
+      groupeIS: 'NON',
+      groupeTVA: 'NON'
+    }
+  end
+
+  private
+
+  # TODO: refactor with attestations fiscales
+  def user_id_sanitized
+    UserIdDGFIPService.call(context.params[:user_id])
+  end
+end
