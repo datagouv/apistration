6de958eb2c456e51f905d04b3ce4f34380456267
Loïc Delmaire <loic.delmaire@gmail.com>
2022-01-21 11:46:18 +0100

    Introduce DGFIP::AttestationFiscale

---
+++ siade/app/organizers/dgfip/attestation_fiscale.rb
@@ -0,0 +1,12 @@
+class DGFIP::AttestationFiscale < RetrieverOrganizer
+  organize DGFIP::AttestationFiscale::ValidateParams,
+    DGFIP::Authenticate,
+    DGFIP::AttestationFiscale::MakeRequest,
+    DGFIP::AttestationFiscale::ValidateResponse,
+    DGFIP::AttestationFiscale::UploadDocument,
+    DGFIP::AttestationFiscale::BuildResource
+
+  def provider_name
+    'DGFIP'
+  end
+end
---
+++ siade/spec/organizers/dgfip/attestation_fiscale_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe DGFIP::AttestationFiscale, :self_hosted_doc, type: :retriever_organizer do
+  subject { described_class.call(params: params) }
+
+  let(:params) do
+    {
+      siren: siren,
+      user_id: user_id
+    }
+  end
+
+  let(:user_id) { yes_jwt_user.id }
+  let(:siren) { valid_siren }
+
+  context 'with valid attributes' do
+    context 'when DGFIP works' do
+      before do
+        mock_dgfip_authenticate
+        mock_valid_dgfip_attestation_fiscale(siren, valid_dgfip_user_id)
+      end
+
+      it { is_expected.to be_a_success }
+
+      it 'sets the resource id' do
+        id = subject.resource.id
+
+        expect(id).to eq(siren)
+      end
+
+      it 'uploads the attestation on the self hosted storage' do
+        document_url = subject.resource.document_url
+
+        expect(document_url).to be_a_valid_self_hosted_pdf_url('attestation_fiscale_dgfip')
+      end
+    end
+  end
+
+  context 'with invalid attributes' do
+    let(:siren) { 'invalid' }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+  end
+end
