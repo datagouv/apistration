029bb113c49f9b7488ef61ecc9a1084847381231
Alexandre De Pablo <alexandre.depablo@gmail.com>
2022-02-23 16:18:44 +0100

    Introduce QUALIBAT::CertificationsBatiment::MakeRequest

---
+++ siade/app/interactors/qualibat/certifications_batiment/make_request.rb
@@ -0,0 +1,22 @@
+class QUALIBAT::CertificationsBatiment::MakeRequest < MakeRequest::Get
+  protected
+
+  def request_uri
+    URI(Siade.credentials[:qualibat_url])
+  end
+
+  def request_params
+    {
+      token: token,
+      SIRET: siret
+    }
+  end
+
+  def token
+    Siade.credentials[:qualibat_token]
+  end
+
+  def siret
+    context.params[:siret]
+  end
+end
---
+++ siade/spec/interactors/qualibat/certifications_batiment/make_request_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe QUALIBAT::CertificationsBatiment::MakeRequest, type: :make_request do
+  subject { described_class.call(params: params) }
+
+  let(:params) do
+    {
+      siret: siret
+    }
+  end
+
+  context 'when siret is valid', vcr: { cassette_name: 'qualibat/certifications_batiment/valid_siret' } do
+    let(:siret) { valid_siret(:qualibat) }
+
+    it { is_expected.to be_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPOK) }
+  end
+
+  context 'when siret is not found', vcr: { cassette_name: 'qualibat/certifications_batiment/not_found_siret' } do
+    let(:siret) { not_found_siret(:qualibat) }
+
+    it { is_expected.to be_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPNotFound) }
+  end
+end
