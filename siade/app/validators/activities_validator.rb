4779f189f2f45adad3a8d6b357d2da531e467c87
Loïc Delmaire <loic.delmaire@gmail.com>
2022-03-21 09:33:33 +0100

    Remove ActivitiesValidator
    No longer used, makes Rails/DeprecatedActiveModelErrorsMethods
    not happy.

--- siade/app/validators/activities_validator.rb
+++
@@ -1,8 +0,0 @@
-class ActivitiesValidator < ActiveModel::Validator
-  def validate(record)
-    activities = record.activities
-    activities.each do |activity|
-      record.errors[:activities] << 'Need to be in the form controller:action:(siret or siren or *)' if (/\w+:\w+:(\d+|\*)/ =~ activity).nil?
-    end
-  end
-end
