819878d745e697b996ca6eae43679eab02cbe8db
Samuelfaure <samuel.faure.dev@gmail.com>
2022-04-13 14:09:18 +0200

    V3/OPQIBI: BuildResource

---
+++ siade/app/interactors/opqibi/certifications_ingenierie/build_resource.rb
@@ -0,0 +1,42 @@
+class OPQIBI::CertificationsIngenierie::BuildResource < BuildResource
+  protected
+
+  def resource_attributes
+    {
+      id: json_body['numero_certificat'],
+      siren: context.params[:siren],
+      url: json_body['url'],
+      date_delivrance_certificat: normalized_date(json_body['date_de_delivrance_du_certificat']),
+      duree_validite_certificat: json_body['duree_de_validite_du_certificat'],
+      assurances: json_body['assurance'],
+      qualifications: build_qualifications(json_body['qualifications']),
+      date_validite_qualifications: normalized_date(json_body['date_de_validite_des_qualification']),
+      qualifications_probatoires: build_qualifications(json_body['qualifications_probatoires']),
+      date_validite_qualifications_probatoires: normalized_date(json_body['date_de_validite_des_qualifications_probatoires'])
+    }
+  end
+
+  private
+
+  def build_qualifications(qualifications)
+    qualifications.map { |q| qualifications_attributes(q) }
+  end
+
+  def qualifications_attributes(qualification)
+    {
+      nom: qualification['Nom'],
+      code_qualification: qualification['CodeQualification'],
+      definition: qualification['Definition'],
+      rge: cast_rge_to_boolean(qualification['rge'])
+    }
+  end
+
+  def cast_rge_to_boolean(value)
+    case value
+    when '0'
+      false
+    when '1'
+      true
+    end
+  end
+end
---
+++ siade/spec/interactors/opqibi/certifications_ingenierie/build_resource_spec.rb
@@ -0,0 +1,64 @@
+RSpec.describe OPQIBI::CertificationsIngenierie::BuildResource, type: :build_resource do
+  subject { described_class.call(response:) }
+
+  let(:response) { instance_double('Net::HTTPOK', body:) }
+
+  let(:body) do
+    OPQIBI::CertificationsIngenierie::MakeRequest.call(params:).response.body
+  end
+
+  let(:params) do
+    {
+      siren:
+    }
+  end
+
+  let(:siren) { valid_siren(:opqibi_with_probatoire) }
+
+  let(:valid_payload) do
+    {
+      id: '05 02 1690',
+      siren: '434717997',
+      url: 'https://www.opqibi.com/fiche/1777',
+      date_delivrance_certificat: '2022-02-01',
+      duree_validite_certificat: 'valable un an',
+      assurances: 'MAF',
+      qualifications: [
+        {
+          nom: 'Étude en acoustique',
+          code_qualification: '1601',
+          definition: 'Correction acoustique des salles',
+          rge: false
+        },
+        {
+          nom: 'Ingénierie en acoustique des infrastructures de transport',
+          code_qualification: '1602',
+          definition: 'Maîtrise des bruits',
+          rge: false
+        }
+      ],
+      date_validite_qualifications: '2025-02-01',
+      qualifications_probatoires: [
+        {
+          code_qualification: '1603',
+          nom: 'Ingénierie en acoustique industrielle',
+          definition: 'Maîtrise des bruits industriels',
+          rge: false
+        }
+      ],
+      date_validite_qualifications_probatoires: '2023-02-01'
+    }
+  end
+
+  describe '.call', vcr: { cassette_name: 'opqibi/certifications_ingenierie/valid_siren' } do
+    subject { described_class.call(response:, params:) }
+
+    it { is_expected.to be_a_success }
+
+    it 'builds valid resource' do
+      expect(subject.resource).to be_a(Resource)
+
+      expect(subject.resource.to_h).to eq(valid_payload)
+    end
+  end
+end
