20586c98d3b96d1bd36dce3dc9dfa442aed10308
Loïc Delmaire <loic.delmaire@gmail.com>
2022-04-22 14:49:43 +0200

    Scout commit

--- siade/config/initializers/logstasher.rb
+++ siade/config/initializers/logstasher.rb
@@ -10,9 +10,7 @@ if LogStasher.enabled?
   LogStasher.add_custom_fields_to_request_context do |fields|
     controller_name = request.params['controller']
 
-    is_api_request =
-      controller_name =~ /\Aapi\//
-
+    is_api_request = (controller_name =~ /\Aapi\//)
 
     if is_api_request
       fields[:api_version] = controller_name.split('/')[1]
