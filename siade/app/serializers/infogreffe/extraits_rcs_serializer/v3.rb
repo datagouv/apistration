2e1700ce66cdd0ce4fe89ce1e8946d8e05d861e3
Samuelfaure <samuel.faure.dev@gmail.com>
2022-04-28 15:47:31 +0200

    V3/ExtraitRCS: Serializer for MVP

--- siade/app/serializers/infogreffe/extraits_rcs_serializer/v3.rb
+++ siade/app/serializers/infogreffe/extraits_rcs_serializer/v3.rb
@@ -2,5 +2,16 @@ class Infogreffe::ExtraitsRCSSerializer::V3 < V3AndMore::BaseSerializer
   attributes :date_extrait,
     :date_immatriculation,
     :mandataires_sociaux,
-    :observations
+    :observations,
+    :denomination,
+    :nom_commercial,
+    :forme_juridique,
+    :code_forme_juridique,
+    :adresse_siege,
+    :etablissement_principal,
+    :date_cloture_exercice_comptable,
+    :date_fin_de_vie,
+    :capital,
+    :greffe,
+    :code_greffe
 end
