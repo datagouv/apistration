b91d8a488d9ba690b67fdf4f3fab9cffb06800b1
Samuelfaure <samuel.faure.dev@gmail.com>
2022-05-12 12:34:24 +0200

    Serializer for new payload ExtraitsRCS

--- siade/app/serializers/infogreffe/extraits_rcs_serializer/v3.rb
+++ siade/app/serializers/infogreffe/extraits_rcs_serializer/v3.rb
@@ -1,17 +1,14 @@
 class Infogreffe::ExtraitsRCSSerializer::V3 < V3AndMore::BaseSerializer
-  attributes :date_extrait,
+  attributes :siren,
+    :date_extrait,
     :date_immatriculation,
     :mandataires_sociaux,
     :observations,
-    :denomination,
     :nom_commercial,
-    :forme_juridique,
-    :code_forme_juridique,
     :adresse_siege,
     :etablissement_principal,
-    :date_cloture_exercice_comptable,
-    :date_fin_de_vie,
     :capital,
     :greffe,
-    :code_greffe
+    :personne_morale,
+    :personne_physique
 end
