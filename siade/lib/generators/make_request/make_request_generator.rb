8e1fd0f4b27c3c65544c57ed1dc8125218a4ce78
Samuelfaure <samuel.faure.dev@gmail.com>
2022-05-16 14:49:07 +0200

    Better MakeRequest generators

--- siade/lib/generators/make_request/make_request_generator.rb
+++ siade/lib/generators/make_request/make_request_generator.rb
@@ -6,6 +6,11 @@ class MakeRequestGenerator < BaseGenerator
     default: 'GET',
     desc: 'HTTP request verb (GET or POST)'
 
+  class_option :validation_type,
+    type: :string,
+    default: 'siren',
+    desc: 'Does the API needs to validate: siren, siret or custom ?'
+
   desc 'See `bin/rails generate scaffold_resource --help` instead'
   def create_make_request
     template 'make_request.rb.erb', File.join('app/interactors', provider_namespace.underscore, resource_class.underscore, 'make_request.rb')
@@ -16,5 +21,16 @@ class MakeRequestGenerator < BaseGenerator
 
   def get_request?
     options[:verb].upcase == 'GET'
+  end
+
+  def id_name
+    return if options[:validation_type].nil?
+
+    case options[:validation_type].downcase
+    when 'siren'
+      'siren'
+    when 'siret'
+      'siret'
+    end
   end
 end
