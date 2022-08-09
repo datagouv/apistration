7575343496fb71e61f8e6cabcfdef2e0989a63eb
Loïc Delmaire <loic.delmaire@gmail.com>
2022-08-09 18:47:45 +0200

    API Particulier: /api/v2/etudiants works with civility params

--- siade/app/controllers/api_particulier/v2/mesri/student_status_controller.rb
+++ siade/app/controllers/api_particulier/v2/mesri/student_status_controller.rb
@@ -2,7 +2,7 @@ class APIParticulier::V2::MESRI::StudentStatusController < APIParticulierControl
   def show
     authorize :mesri_identifiant, :mesri_identite, :mesri_inscription_etudiant, :mesri_inscription_autre, :mesri_admission, :mesri_etablissements
 
-    organizer = ::MESRI::StudentStatusWithINE.call(params: organizer_params)
+    organizer = extract_organizer_thanks_to_params.call(params: organizer_params)
 
     if organizer.success?
       render json: serialize_data(organizer),
@@ -14,11 +14,38 @@ class APIParticulier::V2::MESRI::StudentStatusController < APIParticulierControl
 
   private
 
+  def extract_organizer_thanks_to_params
+    if call_with_ine?
+      MESRI::StudentStatusWithINE
+    else
+      MESRI::StudentStatusWithCivility
+    end
+  end
+
   def organizer_params
-    {
-      ine: params[:ine],
+    mesri_student_status_params.merge(
       user_id: current_user.id
-    }
+    )
+  end
+
+  def mesri_student_status_params
+    if call_with_ine?
+      {
+        ine: params[:ine]
+      }
+    else
+      {
+        family_name: params[:nom],
+        first_name: params[:prenom],
+        birthday_date: params[:dateDeNaissance],
+        birthday_place: params[:lieuDeNaissance],
+        gender: params[:sexe]
+      }
+    end
+  end
+
+  def call_with_ine?
+    params[:ine].present?
   end
 
   def format_not_found_error(error)
--- siade/spec/controllers/api_particulier/v2/mesri/student_status_controller_spec.rb
+++ siade/spec/controllers/api_particulier/v2/mesri/student_status_controller_spec.rb
@@ -1,67 +1,104 @@
 RSpec.describe APIParticulier::V2::MESRI::StudentStatusController, type: :controller do
-  subject { get :show, params: { ine:, token: } }
-
   let(:all_scopes) { %w[mesri_identifiant mesri_identite mesri_inscription_etudiant mesri_inscription_autre mesri_admission mesri_etablissements] }
   let(:one_field_of_each_scope) { %w[ine nom inscriptions] }
 
   let(:token) { TokenFactory.new(scopes).valid }
   let(:ine) { '1234567890G' }
 
-  describe 'with valid params' do
-    before do
-      stub_request(:get, /#{Siade.credentials[:mesri_student_status_url]}/).to_return(
-        status: 200,
-        body: File.read(Rails.root.join('spec/fixtures/payloads/mesri_student_status_with_ine_valid_response.json'))
-      )
-    end
+  describe 'with ine param' do
+    subject { get :show, params: { ine:, token: } }
 
-    context 'when token has full access' do
-      let(:scopes) { all_scopes }
+    describe 'with valid params' do
+      before do
+        stub_request(:get, /#{Siade.credentials[:mesri_student_status_url]}/).to_return(
+          status: 200,
+          body: File.read(Rails.root.join('spec/fixtures/payloads/mesri_student_status_with_ine_valid_response.json'))
+        )
+      end
 
-      its(:status) { is_expected.to eq(200) }
+      context 'when token has full access' do
+        let(:scopes) { all_scopes }
 
-      it 'returns all fields' do
-        json = JSON.parse(subject.body)
+        its(:status) { is_expected.to eq(200) }
 
-        one_field_of_each_scope.each do |key|
-          expect(json).to have_key(key), "#{key} is missing"
+        it 'returns all fields' do
+          json = JSON.parse(subject.body)
+
+          one_field_of_each_scope.each do |key|
+            expect(json).to have_key(key), "#{key} is missing"
+          end
+        end
+      end
+
+      context 'when mesri_identite is missing' do
+        let(:scopes) { all_scopes - %w[mesri_identite] }
+
+        it 'does not return fields associated to this scope' do
+          json = JSON.parse(subject.body)
+
+          (one_field_of_each_scope - %w[nom]).each do |key|
+            expect(json).to have_key(key), "#{key} is missing"
+          end
+
+          expect(json).not_to have_key('nom')
         end
       end
     end
 
-    context 'when mesri_identite is missing' do
-      let(:scopes) { all_scopes - %w[mesri_identite] }
+    describe 'when it is a 404' do
+      let(:scopes) { all_scopes }
 
-      it 'does not return fields associated to this scope' do
-        json = JSON.parse(subject.body)
+      before do
+        stub_request(:get, /#{Siade.credentials[:mesri_student_status_url]}/).to_return(
+          status: 404,
+          body: File.read(Rails.root.join('spec/fixtures/payloads/mesri_student_status_with_ine_not_found_response.json'))
+        )
+      end
 
-        (one_field_of_each_scope - %w[nom]).each do |key|
-          expect(json).to have_key(key), "#{key} is missing"
-        end
+      its(:status) { is_expected.to eq(404) }
 
-        expect(json).not_to have_key('nom')
+      its(:body) do
+        is_expected.to eq({
+          error: 'not_found',
+          reason: 'Student not found',
+          message: 'Aucun étudiant n\'a pu être trouvé avec les critères de recherche fournis'
+        }.to_json)
       end
     end
   end
 
-  describe 'when it is a 404' do
-    let(:scopes) { all_scopes }
+  describe 'with civility params' do
+    subject { get :show, params: { nom:, prenom:, dateDeNaissance:, lieuDeNaissance:, sexe:, token: } }
 
-    before do
-      stub_request(:get, /#{Siade.credentials[:mesri_student_status_url]}/).to_return(
-        status: 404,
-        body: File.read(Rails.root.join('spec/fixtures/payloads/mesri_student_status_with_ine_not_found_response.json'))
-      )
-    end
+    # rubocop:disable RSpec/VariableName
+    let(:nom) { 'Dupont' }
+    let(:prenom) { 'Jean' }
+    let(:dateDeNaissance) { '2000-01-01' }
+    let(:lieuDeNaissance) { 'Paris' }
+    let(:sexe) { 'm' }
+    # rubocop:enable RSpec/VariableName
 
-    its(:status) { is_expected.to eq(404) }
+    describe 'with valid params' do
+      before do
+        stub_request(:post, /#{Siade.credentials[:mesri_student_status_url]}/).to_return(
+          status: 200,
+          body: File.read(Rails.root.join('spec/fixtures/payloads/mesri_student_status_with_civility_valid_response.json'))
+        )
+      end
 
-    its(:body) do
-      is_expected.to eq({
-        error: 'not_found',
-        reason: 'Student not found',
-        message: 'Aucun étudiant n\'a pu être trouvé avec les critères de recherche fournis'
-      }.to_json)
+      context 'when token has full access' do
+        let(:scopes) { all_scopes }
+
+        its(:status) { is_expected.to eq(200) }
+
+        it 'returns all fields' do
+          json = JSON.parse(subject.body)
+
+          one_field_of_each_scope.each do |key|
+            expect(json).to have_key(key), "#{key} is missing"
+          end
+        end
+      end
     end
   end
 end
