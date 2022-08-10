5397a44a9848f494cf6f5e7d9590b7b130b8d299
Loïc Delmaire <loic.delmaire@gmail.com>
2022-08-10 14:15:15 +0200

    Remove dummy controller for API Particulier

--- siade/app/controllers/api_particulier/v2/dummy_controller.rb
+++
@@ -1,7 +0,0 @@
-class APIParticulier::V2::DummyController < APIParticulierController
-  def show
-    render json: {
-      hello: 'world'
-    }, status: :ok
-  end
-end
--- siade/config/routes/api_particulier.rb
+++ siade/config/routes/api_particulier.rb
@@ -1,6 +1,4 @@
 namespace '/api/v2', constraints: APIParticulierDomainConstraint.new do
-  get 'dummy' => '/api_particulier/v2/dummy#show'
-
   get 'composition-familiale' => '/api_particulier/v2/cnaf/quotient_familial#show'
   get 'situations-pole-emploi' => '/api_particulier/v2/pole_emploi/statut#show'
   get 'etudiants' => '/api_particulier/v2/mesri/student_status#show'
