1a29ab4e38ac126e3708c298ab74c91aa7459c6f
Loïc Delmaire <loic.delmaire@gmail.com>
2022-08-10 18:12:12 +0200

    Introduce FranceConnectDataFetcherThroughAccessToken::BuildUser

---
+++ siade/app/interactors/france_connect_data_fetcher_through_access_token/build_user.rb
@@ -0,0 +1,21 @@
+class FranceConnectDataFetcherThroughAccessToken::BuildUser < ApplicationInteractor
+  def call
+    context.user = build_user
+  end
+
+  private
+
+  def build_user
+    JwtUser.new(
+      uid: '99999999-9999-9999-9999-999999999999',
+      scopes: json_body['scope'],
+      jti: '99999999-9999-9999-9999-999999999999',
+      iat: Time.new(2022, 1, 1).to_i,
+      exp: Time.new(2042, 1, 1).to_i
+    )
+  end
+
+  def json_body
+    @json_body ||= JSON.parse(context.response.body)
+  end
+end
---
+++ siade/spec/interactors/france_connect_data_fetcher_through_access_token/build_user_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe FranceConnectDataFetcherThroughAccessToken::BuildUser, type: :interactor do
+  describe '.call' do
+    subject(:call) { described_class.call(response:) }
+
+    let(:response) do
+      instance_double(Net::HTTPOK, body:)
+    end
+
+    let(:body) { france_connect_checktoken_payload(scopes:).to_json }
+    let(:scopes) { %w[mesri_identifiant openid identite_pivot] }
+
+    its(:user) { is_expected.to be_a(JwtUser) }
+
+    it 'gets scopes from access token payload' do
+      expect(call.user.scopes).to eq(scopes)
+    end
+  end
+end
