4a840ab199356f1030bab19b3513e4fcbca41007
Loïc Delmaire <loic.delmaire@gmail.com>
2022-08-12 15:33:15 +0200

    Introduce DGFIPSVAIRAccessDeniedError

---
+++ siade/app/errors/dgfip_svair_access_denied_error.rb
@@ -0,0 +1,5 @@
+class DGFIPSVAIRAccessDeniedError < ForbiddenError
+  def code
+    '03510'
+  end
+end
---
+++ siade/spec/errors/dgfip_svair_access_denied_error_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DGFIPSVAIRAccessDeniedError, type: :error do
+  it_behaves_like 'a valid error'
+end
