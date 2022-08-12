3f9c6a0465d740a5bbbb0a95997b0ed8794f6710
Alexis Leclerc <alexis@alxs.fr>
2022-08-12 12:50:01 +0200

    Add cache for ACOSS (v2)

--- siade/app/controllers/api_entreprise/v2/attestations_sociales_acoss_controller.rb
+++ siade/app/controllers/api_entreprise/v2/attestations_sociales_acoss_controller.rb
@@ -2,17 +2,25 @@ class APIEntreprise::V2::AttestationsSocialesACOSSController < APIEntreprise::V2
   def show
     authorize :attestations_sociales
 
-    retrieve_attestation = SIADE::V2::Retrievers::AttestationsSocialesACOSS.new(params_requests)
-    retrieve_attestation.retrieve
+    retriever = cached_retriever || retrieve_attestation
 
-    if retrieve_attestation.success?
-      render json: { url: retrieve_attestation.document_url }, status: retrieve_attestation.http_code
+    if retriever.success?
+      render json: { url: retriever.document_url }, status: retriever.http_code
     else
-      render_errors(retrieve_attestation)
+      render_errors(retriever)
     end
   end
 
   private
+
+  def retrieve_attestation
+    retriever = SIADE::V2::Retrievers::AttestationsSocialesACOSS.new(params_requests)
+    retriever.retrieve
+
+    write_retriever_cache(retriever) unless at_least_one_error_kind_of?(:network_error, retriever)
+
+    retriever
+  end
 
   def params_requests
     {
@@ -21,5 +29,17 @@ class APIEntreprise::V2::AttestationsSocialesACOSSController < APIEntreprise::V2
       user_id: current_user.logstash_id,
       recipient: params[:recipient]
     }
+  end
+
+  def write_retriever_cache(retriever)
+    EncryptedCache.write(cache_key, retriever, expires_in: 24.hours.to_i)
+  end
+
+  def cache_key
+    request.path
+  end
+
+  def cached_retriever
+    @cached_retriever ||= EncryptedCache.read(cache_key)
   end
 end
