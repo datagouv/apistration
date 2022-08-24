5d9cc968b569cb0d906f73d7787c8c5e798a0fb9
Samuelfaure <samuel.faure.dev@gmail.com>
2022-08-24 06:55:52 +0200

    Introducing Cnous StudentScholarship BuildResource

---
+++ siade/app/interactors/cnous/build_resource.rb
@@ -0,0 +1,31 @@
+class CNOUS::BuildResource < BuildResource
+  def resource_attributes
+    {
+      nom: json_body['lastName'],
+      prenom: json_body['firstName'],
+      prenom2: json_body['firstName2'], # rubocop:disable Naming/VariableNumber
+      dateNaissance: format_date(json_body['birthDate']),
+      lieuNaissance: json_body['birthPlace']['libelle'],
+      sexe: json_body['civility'],
+      boursier: json_body['boursier'],
+      echelonBourse: json_body['grantEchelon'],
+      email: json_body['email'],
+      dateDeRentree: format_date(json_body['backToSchoolDate']),
+      dureeVersement: json_body['paymentDuration'],
+      statut: json_body['grantStatus'],
+      statutLibelle: statut_libelle,
+      villeEtudes: json_body['studyTown'],
+      etablissement: json_body['schoolName']
+    }
+  end
+
+  private
+
+  def format_date(date)
+    Date.parse(date).strftime('%Y-%m-%d')
+  end
+
+  def statut_libelle
+    json_body['grantStatus'].zero? ? 'définitif' : 'provisoire (conditionnel)'
+  end
+end
---
+++ siade/spec/interactors/cnous/build_resource_spec.rb
@@ -0,0 +1,73 @@
+RSpec.describe CNOUS::BuildResource, type: :build_resource do
+  subject { instance }
+
+  let(:instance) { described_class.call(response:) }
+
+  let(:response) do
+    instance_double(Net::HTTPOK, body:)
+  end
+
+  context 'when it is from a call with ine param' do
+    let(:body) { File.read(Rails.root.join('spec/fixtures/payloads/cnous_student_scholarship_valid_response.json')) }
+
+    it { is_expected.to be_a_success }
+
+    describe 'resource' do
+      subject { instance.bundled_data.data.to_h }
+
+      it do
+        expect(subject).to eq(
+          {
+            nom: 'Martin',
+            prenom: 'Jerome',
+            prenom2: 'Francis', # rubocop:disable Naming/VariableNumber
+            dateNaissance: '1980-11-14',
+            lieuNaissance: 'La Crèche',
+            sexe: 'M',
+            boursier: true,
+            echelonBourse: '5',
+            email: 'francislalanne@gmail.com',
+            dateDeRentree: '2020-09-01',
+            dureeVersement: 12,
+            statut: 0,
+            statutLibelle: 'définitif',
+            villeEtudes: 'MONTPELLIER',
+            etablissement: 'UFR SCIENCES TECHNOLOG SANTE'
+          }
+        )
+      end
+    end
+  end
+
+  context 'when it is from a call with civility param' do
+    let(:body) { File.read(Rails.root.join('spec/fixtures/payloads/cnous_student_scholarship_valid_response.json')) }
+
+    it { is_expected.to be_a_success }
+
+    describe 'resource' do
+      subject { instance.bundled_data.data.to_h }
+
+      it do
+        expect(subject).to eq(
+          {
+            nom: 'Martin',
+            prenom: 'Jerome',
+            prenom2: 'Francis', # rubocop:disable Naming/VariableNumber
+            dateNaissance: '1980-11-14',
+            lieuNaissance: 'La Crèche',
+            sexe: 'M',
+            boursier: true,
+            echelonBourse: '5',
+            email: 'francislalanne@gmail.com',
+            dateDeRentree: '2020-09-01',
+            dureeVersement: 12,
+            statut: 0,
+            statutLibelle: 'définitif',
+            villeEtudes: 'MONTPELLIER',
+            etablissement: 'UFR SCIENCES TECHNOLOG SANTE'
+          }
+        )
+      end
+    end
+  end
+end
