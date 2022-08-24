f2feea577b066b6fde32942b040bfd32a52f184b
Samuelfaure <samuel.faure.dev@gmail.com>
2022-08-24 11:12:40 +0200

    Route for ScholarshipStatus

--- siade/config/routes/api_particulier.rb
+++ siade/config/routes/api_particulier.rb
@@ -2,5 +2,6 @@ namespace '/api/v2', constraints: APIParticulierDomainConstraint.new do
   get 'composition-familiale' => '/api_particulier/v2/cnaf/quotient_familial#show'
   get 'situations-pole-emploi' => '/api_particulier/v2/pole_emploi/statut#show'
   get 'etudiants' => '/api_particulier/v2/mesri/student_status#show'
+  get 'etudiants-boursiers' => '/api_particulier/v2/cnous/student_scholarship#show'
   get 'avis-imposition' => '/api_particulier/v2/dgfip/svair#show'
 end
