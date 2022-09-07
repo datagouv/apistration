6574d4699833e04a6bedcea19cea154c4602efdc
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-07 20:23:49 +0200

    Add missing scopes to bin/generate_jwt_token.rb

--- siade/bin/generate_jwt_token.rb
+++ siade/bin/generate_jwt_token.rb
@@ -1,10 +1,40 @@
 require 'date'
 
-extra_scopes = [
-  'exercices',
-  'attestations_fiscales',
-  'liasse_fiscale',
-  'entreprises_artisanales',
+extra_scopes = %w[
+  exercices
+  attestations_fiscales
+  liasse_fiscale
+  entreprises_artisanales
+  cnous_echelon_bourse
+  cnous_email
+  cnous_periode_versement
+  cnous_statut_bourse
+  cnous_ville_etudes
+  cnous_identite
+  dgfip_declarant1_nom
+  dgfip_declarant1_nom_naissance
+  dgfip_declarant1_prenoms
+  dgfip_declarant1_date_naissance
+  dgfip_declarant2_nom
+  dgfip_declarant2_nom_naissance
+  dgfip_declarant2_prenoms
+  dgfip_declarant2_date_naissance
+  dgfip_date_recouvrement
+  dgfip_date_etablissement
+  dgfip_adresse_fiscale_taxation
+  dgfip_adresse_fiscale_annee
+  dgfip_nombre_parts
+  dgfip_nombre_personnes_a_charge
+  dgfip_situation_familiale
+  dgfip_revenu_brut_global
+  dgfip_revenu_imposable
+  dgfip_impot_revenu_net_avant_corrections
+  dgfip_montant_impot
+  dgfip_revenu_fiscal_reference
+  dgfip_annee_impot
+  dgfip_annee_revenus
+  dgfip_erreur_correctif
+  dgfip_situation_partielle
 ]
 
 if ARGV.count.zero?
