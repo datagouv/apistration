22d6b85e92c31e27250f47786e4431447544fe0f
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-15 15:05:11 +0200

    Harden redis config
    Closes https://github.com/etalab/siade/issues/280
    Related https://github.com/etalab/very_ansible/pull/199

--- siade/app/services/redis_service.rb
+++ siade/app/services/redis_service.rb
@@ -5,7 +5,11 @@ class RedisService
   attr_reader :redis
 
   def initialize
-    @redis = Redis.new(url: ENV.fetch('REDIS_DATABASE_URL', 'redis://localhost:6379/0'))
+    @redis = Redis.new(url:)
+  end
+
+  def url
+    ENV.fetch('REDIS_DATABASE_URL', 'redis://localhost:6379/0')
   end
 
   def_delegators :redis,
