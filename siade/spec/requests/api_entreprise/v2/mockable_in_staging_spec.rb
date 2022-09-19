41420950840faf3a63acbca09a571585f345eb26
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-19 09:24:45 +0200

    Scout commit

--- siade/spec/requests/api_entreprise/v2/mockable_in_staging_spec.rb
+++ siade/spec/requests/api_entreprise/v2/mockable_in_staging_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'OpenAPI inegrity check for v2 APIs', api: :entreprise do
+RSpec.describe 'OpenAPI integrity check for v2 APIs', api: :entreprise do
   before do
     allow(Rails.env).to receive(:staging?).and_return(true)
     Rack::Attack.reset!
