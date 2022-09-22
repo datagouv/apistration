699122decf978487f185cec6190127524f4c0a3e
Samuelfaure <samuel.faure.dev@gmail.com>
2022-09-22 12:13:07 +0200

    Dictionaries for Liasses Fiscales: CacheResponse

---
+++ siade/app/interactors/dgfip/dictionaries/cache_response.rb
@@ -0,0 +1,43 @@
+class DGFIP::Dictionaries::CacheResponse < ApplicationInteractor
+  def call
+    dictionary.each { |imprime| save_in_redis(imprime) }
+  end
+
+  private
+
+  def dictionary
+    JSON.parse(context.response.body)['dictionnaire']
+  end
+
+  def save_in_redis(imprime)
+    redis.set(redis_key(imprime), declarations(imprime), ex: dictionary_expires_in)
+  end
+
+  def redis_key(imprime)
+    "dgfip:dictionnaires:#{year}:#{numero(imprime)}:#{millesime(imprime)}"
+  end
+
+  def year
+    "year_#{context.params[:year]}"
+  end
+
+  def numero(imprime)
+    "imprime_#{imprime['numero_imprime']}"
+  end
+
+  def millesime(imprime)
+    "millesime_#{imprime['millesimes']['millesime']}"
+  end
+
+  def declarations(imprime)
+    imprime['millesimes']['declaration']
+  end
+
+  def dictionary_expires_in
+    6.months.from_now.to_i
+  end
+
+  def redis
+    @redis ||= RedisService.instance
+  end
+end
---
+++ siade/spec/interactors/dgfip/dictionaries/cache_response_spec.rb
@@ -0,0 +1,61 @@
+RSpec.describe DGFIP::Dictionaries::CacheResponse do
+  subject { described_class.call(response:, params:) }
+
+  let(:response) { instance_double(Net::HTTPOK, code: '200', body:) }
+  let(:body) { Rails.root.join('spec/fixtures/payloads/dgfip-dictionary.json').read }
+  let(:params) do
+    {
+      year: 2018
+    }
+  end
+
+  describe 'happy path' do
+    let(:declarations_1) do
+      [
+        {
+          'code_absolu' => '1234567',
+          'code_EDI' => 'XX:X123:4567:0:XXX',
+          'code' => 'XX',
+          'intitule' => 'Intitulé 1',
+          'code_type_donnee' => 'XXX',
+          'code_nref' => '123456'
+        },
+        {
+          'code_absolu' => '1234568',
+          'code_EDI' => 'XX:X123:4568:2',
+          'code' => 'YY',
+          'intitule' => 'Intitulé 2',
+          'code_type_donnee' => 'YYY',
+          'code_nref' => '123457'
+        }
+      ]
+    end
+
+    let(:declarations_2) do
+      [
+        {
+          'code_absolu' => '2345678',
+          'code_EDI' => 'XX:X123:2222:2:XXX',
+          'code' => 'ZZ',
+          'intitule' => 'Déposé néant',
+          'code_type_donnee' => 'XXY',
+          'code_nref' => '123458'
+        }
+      ]
+    end
+
+    let(:redis_key_1) { 'dgfip:dictionnaires:year_2018:imprime_2050:millesime_201501' }
+    let(:redis_key_2) { 'dgfip:dictionnaires:year_2018:imprime_2051:millesime_201501' }
+
+    let(:expires_in) { 6.months.from_now.to_i }
+
+    it { is_expected.to be_a_success }
+
+    it 'caches all results in redis' do
+      expect(RedisService.instance).to receive(:set).with(redis_key_1, declarations_1, { ex: expires_in })
+      expect(RedisService.instance).to receive(:set).with(redis_key_2, declarations_2, { ex: expires_in })
+
+      subject
+    end
+  end
+end
