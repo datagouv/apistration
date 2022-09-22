c1f8210e7e501f017bab260b1f42bd43d6c0769f
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-22 09:50:50 +0200

    API Particulier: add swagger files
    Closes https://github.com/etalab/siade/issues/320

--- siade/config/routes/api_particulier.rb
+++ siade/config/routes/api_particulier.rb
@@ -1,6 +1,8 @@
 namespace '/api', constraints: APIParticulierDomainConstraint.new do
   get 'caf/famille' => '/api_particulier/v2/cnaf/quotient_familial#show'
   get 'impots/svair' => '/api_particulier/v2/dgfip/svair#show'
+  get 'open-api.yml', to: ->(env) { [200, {}, [File.read(Rails.root.join('swagger/api-particulier-open-api.yml'))]] }
+  get 'france-connect/open-api.yml', to: ->(env) { [200, {}, [File.read(Rails.root.join('swagger/api-particulier-france-connect-open-api.yml'))]] }
 
   namespace '/v2' do
     get 'composition-familiale' => '/api_particulier/v2/cnaf/quotient_familial#show'
---
+++ siade/spec/requests/api_particulier_swagger_files_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe 'API Particulier: swagger files', type: :request do
+  before do
+    host! 'particulier.api.localtest.me'
+  end
+
+  describe 'main swagger' do
+    subject(:get_swagger) do
+      get '/api/open-api.yml'
+    end
+
+    it 'renders 200' do
+      get_swagger
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders valid yaml' do
+      get_swagger
+
+      expect {
+        YAML.unsafe_load(response.body)
+      }.not_to raise_error
+    end
+  end
+
+  describe 'france connect swagger' do
+    subject(:get_swagger) do
+      get '/api/france-connect/open-api.yml'
+    end
+
+    it 'renders 200' do
+      get_swagger
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders valid yaml' do
+      get_swagger
+
+      expect {
+        YAML.unsafe_load(response.body)
+      }.not_to raise_error
+    end
+  end
+end
