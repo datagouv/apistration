e842e87496487297917984c47069e7e833da55ad
Loïc Delmaire <loic.delmaire@gmail.com>
2022-09-22 08:56:01 +0200

    API Particulier: add deprecated routes
    Thanks to [this commit](https://github.com/etalab/compare_legacy_and_siade_api_particulier/commit/d8ff730336aca6a833761fbfaea9d9bcfcc48914) we know that
    legacy endpoints are almost the same as v2 routes.
    
    CNAF routes are the same
    
    For DGFIP, the difference is here:
    
    * https://github.com/betagouv/api-particulier/blob/master/src/presentation/routers/api/data-providers/dgfip/index.router.ts#L17
    * https://github.com/betagouv/api-particulier/blob/master/src/presentation/controllers/fetch-data.controller.ts#L17
    
    => null keys used to be present, no longer on the v2 legacy, but
    reintroduced in our stack (because.. who wants dynamic schemas when a
    key is null ? no one)
    
    Related https://github.com/etalab/very_ansible/pull/202

--- siade/config/routes/api_particulier.rb
+++ siade/config/routes/api_particulier.rb
@@ -1,7 +1,12 @@
-namespace '/api/v2', constraints: APIParticulierDomainConstraint.new do
-  get 'composition-familiale' => '/api_particulier/v2/cnaf/quotient_familial#show'
-  get 'situations-pole-emploi' => '/api_particulier/v2/pole_emploi/statut#show'
-  get 'etudiants' => '/api_particulier/v2/mesri/student_status#show'
-  get 'etudiants-boursiers' => '/api_particulier/v2/cnous/student_scholarship#show'
-  get 'avis-imposition' => '/api_particulier/v2/dgfip/svair#show'
+namespace '/api', constraints: APIParticulierDomainConstraint.new do
+  get 'caf/famille' => '/api_particulier/v2/cnaf/quotient_familial#show'
+  get 'impots/svair' => '/api_particulier/v2/dgfip/svair#show'
+
+  namespace '/v2' do
+    get 'composition-familiale' => '/api_particulier/v2/cnaf/quotient_familial#show'
+    get 'situations-pole-emploi' => '/api_particulier/v2/pole_emploi/statut#show'
+    get 'etudiants' => '/api_particulier/v2/mesri/student_status#show'
+    get 'etudiants-boursiers' => '/api_particulier/v2/cnous/student_scholarship#show'
+    get 'avis-imposition' => '/api_particulier/v2/dgfip/svair#show'
+  end
 end
