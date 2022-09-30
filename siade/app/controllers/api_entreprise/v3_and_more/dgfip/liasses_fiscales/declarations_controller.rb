1eeb8e3f8bb38a79b52e804d6c3834d9f2fee8e8
Samuelfaure <samuel.faure.dev@gmail.com>
2022-09-30 14:02:17 +0200

    Revert "V3 DGFIP Liasses fiscales is still in development"
    This reverts commit 63ab613bcd7d181ef32e4ffce5a226d04c640042.

--- siade/app/controllers/api_entreprise/v3_and_more/dgfip/liasses_fiscales/declarations_controller.rb
+++ siade/app/controllers/api_entreprise/v3_and_more/dgfip/liasses_fiscales/declarations_controller.rb
@@ -1,11 +1,32 @@
 class APIEntreprise::V3AndMore::DGFIP::LiassesFiscales::DeclarationsController < APIEntreprise::V3AndMore::BaseController
-  skip_before_action :verify_api_version!
-
   def show
     authorize :liasse_fiscale
 
-    render content_type: content_type_header,
-      json: ::ErrorsSerializer.new([NotImplementedYetError.new], format: :json_api).as_json,
-      status: :not_implemented
+    organizer = retrieve_payload_data(::DGFIP::LiassesFiscales::Declarations, cache: true, cache_key:)
+
+    if organizer.success?
+      render json: serializer_class.new(organizer.bundled_data).serializable_hash,
+        status: extract_http_code(organizer)
+    else
+      render_errors(organizer)
+    end
+  end
+
+  private
+
+  def organizer_params
+    {
+      siren: params[:siren],
+      year: params[:year],
+      user_id: current_user.id
+    }
+  end
+
+  def cache_key
+    "dgfip/attestations_fiscales:siren=#{params[:siren]}&year=#{params[:year]}"
+  end
+
+  def serializer_module
+    ::APIEntreprise::DGFIP::LiassesFiscales::DeclarationsSerializer
   end
 end
