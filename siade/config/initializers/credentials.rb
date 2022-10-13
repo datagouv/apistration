034262670137200c34756038e344e76e763af155
Loïc Delmaire <loic.delmaire@gmail.com>
2022-10-13 14:44:36 +0200

    Harden Siade.credentials
    Closes https://github.com/etalab/siade/issues/325

--- siade/config/initializers/credentials.rb
+++ siade/config/initializers/credentials.rb
@@ -1,5 +1,13 @@
 module Siade
   def self.credentials
-    Rails.application.credentials.config[Rails.env.to_sym] || {}
+    @credentials ||= begin
+      credentials = Rails.application.credentials.config[Rails.env.to_sym] || {}
+
+      credentials.default_proc = proc do |_,key|
+        raise KeyError, "key '#{key}' not found"
+      end
+
+      credentials
+    end
   end
 end
---
+++ siade/spec/acceptances/credentials_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Siade, '.credentials' do
+  subject(:credential) { described_class.credentials[key] }
+
+  context 'when key exists' do
+    let(:key) { :jwt_hash_secret }
+
+    it 'renders value' do
+      expect(credential).to eq('this_is_a_valid_jwt_hash_for_spec')
+    end
+  end
+
+  context 'when key does not exist' do
+    let(:key) { :lolilol }
+
+    it 'raises KeyError error' do
+      expect {
+        credential
+      }.to raise_error(KeyError)
+    end
+  end
+end
