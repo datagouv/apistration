c7778789431aba8cce902ad79787fe31b208208b
Loïc Delmaire <loic.delmaire@gmail.com>
2022-11-23 10:17:22 +0100

    Banque de France v3: do not verify SSL handshake
    iso v2, fail refactor introduced by 9d8c4f74d504f12edb235c18e29dbbc1dba7613b
    
    Closes https://github.com/etalab/siade/issues/438
    Closes https://sentry.data.gouv.fr/organizations/sentry/issues/1967/

--- siade/app/interactors/banque_de_france/bilans_entreprise/make_request.rb
+++ siade/app/interactors/banque_de_france/bilans_entreprise/make_request.rb
@@ -14,22 +14,14 @@ class BanqueDeFrance::BilansEntreprise::MakeRequest < MakeRequest::Get
   end
 
   def http_options
-    http_wildcard_ssl_options
+    http_wildcard_ssl_options.merge(
+      verify_mode: OpenSSL::SSL::VERIFY_NONE
+    )
   end
 
   private
 
   def siren
     context.params[:siren]
-  end
-
-  def key
-    raw_key = File.read(Siade.credentials[:ssl_wildcard_certif_key_path])
-    OpenSSL::PKey::RSA.new(raw_key)
-  end
-
-  def cert
-    raw_cert = File.read(Siade.credentials[:ssl_wildcard_certif_crt_path])
-    OpenSSL::X509::Certificate.new(raw_cert)
   end
 end
