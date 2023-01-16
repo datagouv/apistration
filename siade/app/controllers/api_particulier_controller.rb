cbe56f0f7c72826222e9c721b8dc03cd015180a1
Loïc Delmaire <loic.delmaire@gmail.com>
2023-01-16 15:52:45 +0100

    API Particulier: remove track_invalid_but_potential_legit_legacy_token method
    According to sentry is no longer relevant

--- siade/app/controllers/api_particulier_controller.rb
+++ siade/app/controllers/api_particulier_controller.rb
@@ -10,8 +10,6 @@ class APIParticulierController < APIController
     if @token && legacy_token_exists?(@token)
       @current_user = build_user_from_legacy_token(@token)
     else
-      track_invalid_but_potential_legit_legacy_token(@token) if legit_format_for_legacy_token?(@token)
-
       super
     end
   end
@@ -126,15 +124,5 @@ class APIParticulierController < APIController
 
   def legacy_token_valid_length_range
     (36..128)
-  end
-
-  def track_invalid_but_potential_legit_legacy_token(token)
-    MonitoringService.instance.track(
-      'error',
-      'Invalid token but legit format for legacy token',
-      {
-        legacy_token_value: token
-      }
-    )
   end
 end
