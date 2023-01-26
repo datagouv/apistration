534031284717a458afd8f612d8e5923565d754a2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-01-26 15:57:16 +0100

    DGFIP::LiassesFiscales::BuildResource: skip if staging

--- siade/app/organizers/dgfip/liasses_fiscales/build_resource.rb
+++ siade/app/organizers/dgfip/liasses_fiscales/build_resource.rb
@@ -1,4 +1,8 @@
 class DGFIP::LiassesFiscales::BuildResource < ApplicationOrganizer
+  around do |interactor|
+    interactor.call unless staging?
+  end
+
   organize DGFIP::LiassesFiscales::BuildResourceWithoutDictionary,
     DGFIP::LiassesFiscales::RetrieveDictionaryFromCacheOrRemote,
     DGFIP::LiassesFiscales::EnrichResourceWithDictionary
