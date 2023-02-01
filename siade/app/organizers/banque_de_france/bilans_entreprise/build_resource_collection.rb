c8f2f7ce9ab7e983c0754135c1b686728b2d11fe
Loïc Delmaire <loic.delmaire@gmail.com>
2023-02-01 17:43:38 +0100

    BanqueDeFrance::BilansEntreprise::BuildResourceCollection: skip if staging

--- siade/app/organizers/banque_de_france/bilans_entreprise/build_resource_collection.rb
+++ siade/app/organizers/banque_de_france/bilans_entreprise/build_resource_collection.rb
@@ -1,4 +1,8 @@
 class BanqueDeFrance::BilansEntreprise::BuildResourceCollection < ApplicationOrganizer
+  around do |interactor|
+    interactor.call unless staging?
+  end
+
   organize BanqueDeFrance::BilansEntreprise::BuildResourceCollectionWithoutDictionaries,
     BanqueDeFrance::BilansEntreprise::RetrieveDictionariesFromCacheOrRemote,
     BanqueDeFrance::BilansEntreprise::EnrichResourceCollectionWithDictionaries
