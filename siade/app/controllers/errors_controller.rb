a9c3007fb7e632b45ec35e028ba8b6f5970d2ed2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-02-10 12:20:23 +0100

    Refactor api_kind duplicate method
    Related https://github.com/etalab/siade/pull/560#pullrequestreview-1292865329

--- siade/app/controllers/errors_controller.rb
+++ siade/app/controllers/errors_controller.rb
@@ -1,16 +1,12 @@
-class ErrorsController < ActionController::API
+class ErrorsController < ApplicationController
   def not_found
-    render plain: "Cette URL n'existe pas sur API #{api_kind}, veuillez vérifier votre URL.",
+    render plain: "Cette URL n'existe pas sur #{api_kind_humanized}, veuillez vérifier votre URL.",
       status: :not_found
   end
 
   private
 
-  def api_kind
-    if request.host =~ /entreprise\.api/
-      'Entreprise'
-    else
-      'Particulier'
-    end
+  def api_kind_humanized
+    api_kind.humanize
   end
 end
