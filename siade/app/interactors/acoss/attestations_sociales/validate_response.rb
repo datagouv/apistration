1ac1b74742ea24e6957f69d8711613cf49351f94
Loïc Delmaire <loic.delmaire@gmail.com>
2023-02-22 17:25:22 +0100

    Intfoduce URSSAF::AttestationsSociales::ValidateResponse::RawBody
    FUNC502 is no longer an error, this new validator makes a disjonction
    between v3 and future v4.
    
    Ref https://github.com/etalab/siade/issues/283
    Ref https://github.com/etalab/siade/issues/585

--- siade/app/interactors/acoss/attestations_sociales/validate_response.rb
+++ siade/app/interactors/acoss/attestations_sociales/validate_response.rb
@@ -1,96 +1,13 @@
-class ACOSS::AttestationsSociales::ValidateResponse < ValidateResponse
-  def call
-    if body.blank?
-      internal_server_error!('Le corps de la réponse est vide, il s\'agit d\'une erreur interne du fournisseur')
-    elsif json_body_has_errors?
-      handle_errors
-    end
-  rescue TypeError
-    handle_json_error_body_not_an_array
-  end
-
-  private
-
+class ACOSS::AttestationsSociales::ValidateResponse < URSSAF::AttestationsSociales::ValidateResponse::RawBody
   def handle_errors
-    if manual_verification_asked?
-      fail_with_error!(ACOSSError.new(:manual_verification_asked))
-    elsif ongoing_manual_verification?
-      fail_with_error!(ACOSSError.new(:ongoing_manual_verification))
-    elsif cannot_deliver_document?
+    if cannot_deliver_document?
       fail_with_error!(ACOSSError.new(:cannot_deliver_document))
-    elsif internal_error?
-      build_and_fail!(ProviderInternalServerError)
-    elsif not_found?
-      build_and_fail!(NotFoundError)
     else
-      build_and_fail!(ProviderUnknownError)
+      super
     end
   end
 
   def json_body_has_errors?
     json_errors.any?
-  end
-
-  def json_errors
-    @json_errors ||= JSON.parse(
-      body,
-      symbolize_names: true
-    )
-  rescue JSON::ParserError
-    []
-  end
-
-  def handle_json_error_body_not_an_array
-    error = build_error(ProviderUnknownError, 'Invalid json error payload')
-    error.add_to_monitoring_private_context(
-      body:
-    )
-    fail_with_error!(error)
-  end
-
-  def build_and_fail!(error_klass)
-    fail_with_error!(
-      build_error_with_provider_errors(
-        error_klass
-      )
-    )
-  end
-
-  def build_error_with_provider_errors(error_klass)
-    build_error(error_klass).add_meta(
-      provider_errors: json_errors
-    )
-  end
-
-  def not_found?
-    error_code_acoss_for_404.intersect?(errors_codes)
-  end
-
-  def internal_error?
-    error_code_acoss_for_503.intersect?(errors_codes)
-  end
-
-  def manual_verification_asked?
-    %w[FUNC503].intersect?(errors_codes)
-  end
-
-  def ongoing_manual_verification?
-    %w[FUNC504].intersect?(errors_codes)
-  end
-
-  def cannot_deliver_document?
-    %w[FUNC502].intersect?(errors_codes)
-  end
-
-  def errors_codes
-    @errors_codes ||= json_errors.pluck(:code)
-  end
-
-  def error_code_acoss_for_503
-    %w[FUNC510 FUNC511 FUNC512 FUNC513 FUNC514 FUNC515 FUNC516 FUNC429]
-  end
-
-  def error_code_acoss_for_404
-    %w[FUNC501 FUNC517]
   end
 end
---
+++ siade/spec/interactors/urssaf/attestations_sociales/validate_response/raw_body_spec.rb
@@ -0,0 +1,132 @@
+RSpec.describe URSSAF::AttestationsSociales::ValidateResponse::RawBody, type: :validate_response do
+  describe '.call' do
+    subject { described_class.call(response:, provider_name: 'ACOSS') }
+
+    let(:response) do
+      instance_double(Net::HTTPOK, code:, body:)
+    end
+    let(:code) { 200 }
+
+    context 'with a valid body, which is an encode 64 string' do
+      let(:body) { Base64.strict_encode64('whatever') }
+
+      it { is_expected.to be_a_success }
+
+      its(:errors) { is_expected.to be_empty }
+
+      its(:cacheable) { is_expected.to be(true) }
+    end
+
+    context 'with an empty body' do
+      let(:body) { nil }
+
+      it { is_expected.to be_a_failure }
+
+      its(:errors) { is_expected.to include(instance_of(ProviderInternalServerError)) }
+
+      its(:cacheable) { is_expected.to be(false) }
+    end
+
+    context 'when body has an error payload' do
+      let(:body) { json_errors.to_json }
+
+      context 'when it has at least one internal error code' do
+        let(:json_errors) do
+          [
+            { code: 'FUNC511', message: 'Message 511', description: 'description 11' },
+            { code: 'FUNC517', message: 'Message 517', description: 'description 17' }
+          ]
+        end
+
+        it { is_expected.to be_a_failure }
+
+        its(:errors) { is_expected.to include(instance_of(ProviderInternalServerError)) }
+
+        its(:cacheable) { is_expected.to be(false) }
+      end
+
+      context 'when it is only not found errors codes' do
+        let(:json_errors) do
+          [
+            { code: 'FUNC501', message: 'Message 501', description: 'description 1' },
+            { code: 'FUNC517', message: 'Message 517', description: 'description 17' }
+          ]
+        end
+
+        it { is_expected.to be_a_failure }
+
+        its(:errors) { is_expected.to include(instance_of(NotFoundError)) }
+
+        its(:cacheable) { is_expected.to be(false) }
+
+        it 'adds meta with json errors' do
+          subject
+
+          error = subject.errors.first
+
+          expect(error.meta[:provider_errors]).to eq(
+            json_errors
+          )
+        end
+      end
+
+      context 'when company situation requires manual operation by the provider' do
+        let(:json_errors) do
+          [
+            { code: 'FUNC503', message: 'manual operation needed', description: 'do something' }
+          ]
+        end
+
+        it { is_expected.to be_a_failure }
+
+        its(:errors) { is_expected.to include(instance_of(ACOSSError)) }
+        its(:errors) { is_expected.to include(have_attributes(code: '04501')) }
+      end
+
+      context 'when company situation is already ongoing manual operation by the provider' do
+        let(:json_errors) do
+          [
+            { code: 'FUNC504', message: 'manual operation ongoing', description: 'we are doing something' }
+          ]
+        end
+
+        it { is_expected.to be_a_failure }
+
+        its(:errors) { is_expected.to include(instance_of(ACOSSError)) }
+        its(:errors) { is_expected.to include(have_attributes(code: '04502')) }
+      end
+
+      context 'when company situation do not permit to deliver the document' do
+        let(:json_errors) do
+          [
+            { code: 'FUNC502', message: 'cannot deliver document', description: 'rend l argent !' }
+          ]
+        end
+
+        it { is_expected.to be_a_success }
+      end
+
+      context 'when it is a hash instead of an array' do
+        let(:json_errors) do
+          {
+            oki: 'wtf??'
+          }
+        end
+
+        it { is_expected.to be_a_failure }
+
+        its(:errors) { is_expected.to include(instance_of(ProviderUnknownError)) }
+
+        it 'adds monitoring private context with json body to error' do
+          subject
+
+          error = subject.errors.first
+
+          expect(error.monitoring_private_context).to eq(
+            body: json_errors.to_json
+          )
+        end
+      end
+    end
+  end
+end
