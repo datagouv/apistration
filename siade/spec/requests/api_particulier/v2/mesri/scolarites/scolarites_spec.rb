22de4418b008ae19510a292ba1e5f3cbeac2f908
Samuelfaure <samuel.faure.dev@gmail.com>
2023-03-10 09:52:54 +0100

    TOREVERT: Removing API Scolarite swagger/tests until API is finished

--- siade/spec/requests/api_particulier/v2/mesri/scolarites/scolarites_spec.rb
+++ siade/spec/requests/api_particulier/v2/mesri/scolarites/scolarites_spec.rb
@@ -1,66 +1,66 @@
-require 'swagger_helper'
+# require 'swagger_helper'
 
-RSpec.describe 'MESRI: Scolarites', api: :particulier, type: %i[request swagger] do
-  path '/api/v2/scolarites' do
-    get SwaggerData.get('mesri.scolarite.title') do
-      tags(*SwaggerData.get('mesri.scolarite.tags'))
+# RSpec.describe 'MESRI: Scolarites', api: :particulier, type: %i[request swagger] do
+#   path '/api/v2/scolarites' do
+#     get SwaggerData.get('mesri.scolarite.title') do
+#       tags(*SwaggerData.get('mesri.scolarite.tags'))
 
-      produces 'application/json'
+#       produces 'application/json'
 
-      parameter name: 'X-Api-Key', in: :header, type: :string
+#       parameter name: 'X-Api-Key', in: :header, type: :string
 
-      parameter name: :nom,                in: :query, type: :string
-      parameter name: :prenom,             in: :query, type: :string
-      parameter name: :sexe,               in: :query, type: :string
-      parameter name: :dateNaissance,     in: :query, type: :string
-      parameter name: :codeEtablissement, in: :query, type: :string
-      parameter name: :anneeScolaire,     in: :query, type: :string
+#       parameter name: :nom,                in: :query, type: :string
+#       parameter name: :prenom,             in: :query, type: :string
+#       parameter name: :sexe,               in: :query, type: :string
+#       parameter name: :dateNaissance,     in: :query, type: :string
+#       parameter name: :codeEtablissement, in: :query, type: :string
+#       parameter name: :anneeScolaire,     in: :query, type: :string
 
-      # rubocop:disable RSpec/VariableName
-      let(:'X-Api-Key') { x_api_key }
+#
+#       let(:'X-Api-Key') { x_api_key }
 
-      let(:dateNaissance) { '2000-06-10' }
-      let(:codeEtablissement) { '0511474A' }
-      let(:anneeScolaire) { '2021' }
-      # rubocop:enable RSpec/VariableName
-      let(:nom) { 'NOMFAMILLE' }
-      let(:prenom) { 'prenom' }
-      let(:sexe) { 'f' }
+#       let(:dateNaissance) { '2000-06-10' }
+#       let(:codeEtablissement) { '0511474A' }
+#       let(:anneeScolaire) { '2021' }
+#       # rubocop:enable RSpec/VariableName
+#       let(:nom) { 'NOMFAMILLE' }
+#       let(:prenom) { 'prenom' }
+#       let(:sexe) { 'f' }
 
-      let(:x_api_key) { nil }
+#       let(:x_api_key) { nil }
 
-      describe 'with valid token and mandatory params' do
-        let(:x_api_key) { TokenFactory.new(['mesri_scolarites']).valid }
+#       describe 'with valid token and mandatory params' do
+#         let(:x_api_key) { TokenFactory.new(['mesri_scolarites']).valid }
 
-        response '200', 'Scolarité trouvée', vcr: { cassette_name: 'mesri/scolarites/valid' } do
-          description SwaggerData.get('mesri.scolarite.description')
+#         response '200', 'Scolarité trouvée', vcr: { cassette_name: 'mesri/scolarites/valid' } do
+#           description SwaggerData.get('mesri.scolarite.description')
 
-          schema build_rswag_response_api_particulier(
-            attributes: SwaggerData.get('mesri.scolarite.attributes')
-          )
+#           schema build_rswag_response_api_particulier(
+#             attributes: SwaggerData.get('mesri.scolarite.attributes')
+#           )
 
-          run_test!
-        end
+#           run_test!
+#         end
 
-        describe 'server errors' do
-          pending 'en attente de fin développement coté MESRI'
-          # unprocessable_entity_error_request do
-          #   let(:sexe) { 'whatever' }
-          # end
+#         describe 'server errors' do
+#           pending 'en attente de fin développement coté MESRI'
+#           # unprocessable_entity_error_request do
+#           #   let(:sexe) { 'whatever' }
+#           # end
 
-          # response '404', 'Non trouvée', vcr: { cassette_name: 'mesri/scolarites/not_found' } do
-          #   let(:sexe) { 'f' }
+#           # response '404', 'Non trouvée', vcr: { cassette_name: 'mesri/scolarites/not_found' } do
+#           #   let(:sexe) { 'f' }
 
-          #   schema '$ref' => '#/components/schemas/NotFound'
+#           #   schema '$ref' => '#/components/schemas/NotFound'
 
-          #   run_test!
-          # end
+#           #   run_test!
+#           # end
 
-          # common_provider_errors_request('MESRI', MESRI::Scolarites)
+#           # common_provider_errors_request('MESRI', MESRI::Scolarites)
 
-          # common_network_error_request('MESRI', MESRI::Scolarites)
-        end
-      end
-    end
-  end
-end
+#           # common_network_error_request('MESRI', MESRI::Scolarites)
+#         end
+#       end
+#     end
+#   end
+# end
