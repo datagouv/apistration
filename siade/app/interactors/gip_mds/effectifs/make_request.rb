b57baa9aac4156a7877e42dbc3da19ea9d1ce36f
Loïc Delmaire <loic.delmaire@gmail.com>
2023-03-10 09:17:16 +0100

    Introduce GIPMDS::Effectifs::MakeRequest

---
+++ siade/app/interactors/gip_mds/effectifs/make_request.rb
@@ -0,0 +1,79 @@
+class GIPMDS::Effectifs::MakeRequest < MakeRequest::Get
+  protected
+
+  def request_uri
+    URI("#{mds_domain}#{mds_path}")
+  end
+
+  def request_params
+    {
+      codeOPSDemandeur: code_demandeur_dinum,
+      dateHeure: current_time_as_iso_8601,
+      source: regime_agricole_and_regime_general_acronym,
+      nature:
+    }.merge(build_time_params)
+  end
+
+  def extra_headers(request)
+    request['Content-Type'] = 'application/json'
+    request['Authorization'] = "Bearer #{token}"
+  end
+
+  private
+
+  def build_time_params
+    case context.params[:nature]
+    when :yearly
+      annual_params
+    when :monthly
+      monthly_params
+    end
+  end
+
+  def nature
+    case context.params[:nature]
+    when :yearly
+      'A01'
+    when :monthly
+      'M01'
+    end
+  end
+
+  def annual_params
+    {
+      siren: context.params[:siren],
+      periode: "#{context.params[:year]}1231"
+    }
+  end
+
+  def monthly_params
+    {
+      siret: context.params[:siret],
+      periode: "#{context.params[:year]}#{context.params[:month]}01"
+    }
+  end
+
+  def token
+    context.token
+  end
+
+  def mds_domain
+    Siade.credentials[:gip_mds_domain]
+  end
+
+  def mds_path
+    '/rcd-api/1.0.0/effectifs'
+  end
+
+  def current_time_as_iso_8601
+    Time.zone.now.iso8601
+  end
+
+  def code_demandeur_dinum
+    '00000DINUM'.freeze
+  end
+
+  def regime_agricole_and_regime_general_acronym
+    'RA;RG'.freeze
+  end
+end
---
+++ siade/spec/interactors/gip_mds/effectifs/make_request_spec.rb
@@ -0,0 +1,100 @@
+RSpec.describe GIPMDS::Effectifs::MakeRequest, type: :make_request do
+  subject { described_class.call(token:, params:) }
+
+  let(:token) { GIPMDS::Authenticate.call.token }
+
+  before do
+    Timecop.freeze(Time.zone.local(2019, 1, 1, 12, 0, 0))
+
+    mock_gip_mds_authenticate
+  end
+
+  after do
+    Timecop.return
+  end
+
+  describe 'for annual data' do
+    let!(:stubbed_request) do
+      stub_request(:get, "#{Siade.credentials[:gip_mds_domain]}/rcd-api/1.0.0/effectifs").with(
+        query: {
+          codeOPSDemandeur: '00000DINUM',
+          dateHeure: '2019-01-01T12:00:00+01:00',
+          source: 'RA;RG',
+          nature: 'A01',
+          siren:,
+          periode: "#{year}1231"
+        },
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{token}"
+        }
+      ).and_return(
+        status: 200
+      )
+    end
+
+    let(:year) { '2020' }
+    let(:siren) { valid_siren }
+    let(:params) do
+      {
+        nature: :yearly,
+        siren:,
+        year:
+      }
+    end
+
+    it { is_expected.to be_a_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPOK) }
+
+    it 'calls endpoint with valid params' do
+      subject
+
+      expect(stubbed_request).to have_been_requested
+    end
+  end
+
+  describe 'for monthly data' do
+    let!(:stubbed_request) do
+      stub_request(:get, "#{Siade.credentials[:gip_mds_domain]}/rcd-api/1.0.0/effectifs").with(
+        query: {
+          codeOPSDemandeur: '00000DINUM',
+          dateHeure: '2019-01-01T12:00:00+01:00',
+          source: 'RA;RG',
+          nature: 'M01',
+          siret:,
+          periode: "#{year}#{month}01"
+        },
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{token}"
+        }
+      ).and_return(
+        status: 200
+      )
+    end
+
+    let(:params) do
+      {
+        nature: :monthly,
+        siret:,
+        year:,
+        month:
+      }
+    end
+
+    let(:year) { '2020' }
+    let(:month) { '01' }
+    let(:siret) { valid_siret }
+
+    it { is_expected.to be_a_success }
+
+    its(:response) { is_expected.to be_a(Net::HTTPOK) }
+
+    it 'calls endpoint with valid params' do
+      subject
+
+      expect(stubbed_request).to have_been_requested
+    end
+  end
+end
