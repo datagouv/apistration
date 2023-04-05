65e46e97aa23a14f5bd9d2d1594267d9d73a2d1b
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-05 08:33:19 +0200

    APIEntreprise::V2::AssociationsController: cache calls for 1h

--- siade/app/controllers/api_entreprise/v2/associations_controller.rb
+++ siade/app/controllers/api_entreprise/v2/associations_controller.rb
@@ -1,16 +1,41 @@
 class APIEntreprise::V2::AssociationsController < APIEntreprise::V2::BaseController
   def show
-    retrieve_association = SIADE::V2::Retrievers::Associations.new(association_id_from_params)
-    retrieve_association.retrieve
+    retriever = cached_retriever || retrieve_association
 
-    if retrieve_association.success?
-      render json: { association: APIEntreprise::AssociationRNASerializer::V2.new(retrieve_association).as_json }, status: retrieve_association.http_code
+    if retriever.success?
+      render json: { association: APIEntreprise::AssociationRNASerializer::V2.new(retriever).as_json }, status: retriever.http_code
     else
       render_errors(retrieve_association)
     end
   end
 
+  private
+
   def association_id_from_params
     params.require(:id)
+  end
+
+  def retrieve_association
+    @retrieve_association ||= begin
+      retriever = SIADE::V2::Retrievers::Associations.new(association_id_from_params)
+
+      retriever.retrieve
+
+      write_retriever_cache(retriever) unless at_least_one_error_kind_of?(:network_error, retriever)
+
+      retriever
+    end
+  end
+
+  def write_retriever_cache(retriever)
+    EncryptedCache.write(cache_key, retriever, expires_in: 1.hour)
+  end
+
+  def cache_key
+    request.path
+  end
+
+  def cached_retriever
+    @cached_retriever ||= EncryptedCache.read(cache_key)
   end
 end
