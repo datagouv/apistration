b661a620c7c64e22b6eda10a800bc5bd464c5821
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-05 10:41:00 +0200

    APIEntreprise::V2::DocumentsAssociationsController: cache calls for 1h

--- siade/app/controllers/api_entreprise/v2/documents_associations_controller.rb
+++ siade/app/controllers/api_entreprise/v2/documents_associations_controller.rb
@@ -1,37 +1,57 @@
 class APIEntreprise::V2::DocumentsAssociationsController < APIEntreprise::V2::BaseController
-  attr_reader :organizer
-
   def show
-    @organizer = ::MI::Associations::Documents.call(params: organizer_params)
+    retriever = cached_retriever || retrieve_documents_associations
 
-    if organizer.success?
-      render json: serialized_data(organizer),
-        status: extract_http_code(organizer)
+    if retriever.success?
+      render json: serialized_data(retriever),
+        status: extract_http_code(retriever)
     else
-      render json: ErrorsSerializer.new(organizer.errors, format: error_format).as_json,
-        status: extract_http_code(organizer)
+      render json: ErrorsSerializer.new(retriever.errors, format: error_format).as_json,
+        status: extract_http_code(retriever)
     end
   end
 
   private
 
-  def serialized_data(organizer)
+  def retrieve_documents_associations
+    @retrieve_documents_association ||= begin
+      retriever = ::MI::Associations::Documents.call(params: retriever_params)
+
+      write_retriever_cache(retriever) unless at_least_one_error_kind_of?(:network_error, retriever)
+
+      retriever
+    end
+  end
+
+  def serialized_data(retriever)
     {
-      documents: organizer.bundled_data.data.map do |document_resource|
+      documents: retriever.bundled_data.data.map do |document_resource|
         {
           type: document_resource.type,
           url: document_resource.url,
           timestamp: document_resource.timestamp
         }
       end,
-      nombre_documents: organizer.total_documents,
-      nombre_documents_deficients: organizer.upload_errors
+      nombre_documents: retriever.total_documents,
+      nombre_documents_deficients: retriever.upload_errors
     }
   end
 
-  def organizer_params
+  def retriever_params
     {
       id: params.require(:id)
     }
+  end
+
+  def write_retriever_cache(retriever)
+    EncryptedCache.write(cache_key, retriever, expires_in: 1.hour)
+  end
+
+  def cache_key
+    request.path
+  end
+
+  def cached_retriever
+    @cached_retriever ||= EncryptedCache.read(cache_key)
   end
 end
