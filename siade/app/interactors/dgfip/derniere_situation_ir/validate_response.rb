1f97abc910660be8f48154e56361119bb174520a
Loïc Delmaire <loic.delmaire@gmail.com>
2023-04-13 11:23:03 +0200

    Introduce DGFIP::DerniereSituationIR::ValidateResponse

---
+++ siade/app/interactors/dgfip/derniere_situation_ir/validate_response.rb
@@ -0,0 +1,9 @@
+class DGFIP::DerniereSituationIR::ValidateResponse < ValidateResponse
+  def call
+    return if http_ok?
+
+    resource_not_found! if [404, 410].include?(http_code)
+
+    unknown_provider_response!
+  end
+end
