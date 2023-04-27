1d25c568507072dcbf856d58e3a789b15e48a55f
Samuelfaure <samuel.faure.dev@gmail.com>
2023-04-27 14:52:55 +0200

    Boyscout

--- siade/app/interactors/mesri/scolarites/validate_response.rb
+++ siade/app/interactors/mesri/scolarites/validate_response.rb
@@ -2,11 +2,11 @@ class MESRI::Scolarites::ValidateResponse < ValidateResponse
   def call
     provider_timeout_error! if http_provider_timeout_error?
 
-    resource_not_found! if http_not_found?
-
     provider_bad_gateway_error! if http_provider_bad_gateway_error?
 
     provider_internal_error! if http_internal_error?
+
+    resource_not_found! if http_not_found?
 
     return if http_ok?
 
