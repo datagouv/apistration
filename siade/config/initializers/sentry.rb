8941aea2364bef81b0f185a046684be0e8547f9f
Samuelfaure <samuel.faure.dev@gmail.com>
2023-05-02 13:44:55 +0200

    Not sending X-Api-Key to sentry anymore

--- siade/config/initializers/sentry.rb
+++ siade/config/initializers/sentry.rb
@@ -5,4 +5,9 @@ Sentry.init do |config|
   config.enabled_environments = %w[production staging]
 
   config.traces_sample_rate = 1.0
+
+  config.before_send = lambda do |event, _hint|
+    event.request.headers.delete('X-Api-Key')
+    event
+  end
 end
