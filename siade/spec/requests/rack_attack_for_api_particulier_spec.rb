3b2d352e0d3345b319d15ad9adb90e50f6452a0d
Loïc Delmaire <loic.delmaire@gmail.com>
2023-05-29 18:08:12 +0200

    Revert "Revert "Init Api Rate limit for Api-part""
    This reverts commit 15400d552c01fb1ab578d8c154a804102a5645f7.

--- siade/spec/requests/rack_attack_for_api_particulier_spec.rb
+++ siade/spec/requests/rack_attack_for_api_particulier_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe 'Rack::Attack config for API Entreprise', api: :particulier do
+  after { Rack::Attack.reset! }
+
   context 'with a blacklisted token' do
     subject(:make_call) do
       get '/api/v2/etudiants', headers: headers_params
@@ -17,6 +19,49 @@ RSpec.describe 'Rack::Attack config for API Entreprise', api: :particulier do
       make_call
 
       expect(response_json).to have_json_error(detail: 'Votre jeton est sur liste noire, celui-ci a certainement été divulgué sur un canal non-sécurisé. Vous pouvez trouver un jeton valide sur votre espace personnel: https://particulier.api.gouv.fr/compte')
+    end
+  end
+
+  context 'with valid api calls' do
+    def call!
+      get('/api/v2/etudiants-boursiers', headers: headers_params)
+    end
+    let(:headers_params) { { 'X-Api-key' => 'tumtumdulu' } }
+
+    let(:rate_limit_subkeys) do
+      %w[
+        Limit
+        Remaining
+        Reset
+      ]
+    end
+
+    it 'has rate limit headers defined' do
+      call!
+
+      rate_limit_subkeys.each do |subkey|
+        expect(response.headers).to have_key("RateLimit-#{subkey}")
+      end
+    end
+
+    context 'when the limit has not been reached' do
+      before { 19.times { call! } }
+
+      it 'accepts incoming requests' do
+        call!
+        expect(response).not_to have_http_status(:too_many_requests)
+      end
+    end
+
+    context 'when the limit has been reached' do
+      it 'rejects incoming requests after the limit' do
+        Timecop.freeze do
+          20.times { call! }
+          call!
+
+          expect(response).to have_http_status(:too_many_requests)
+        end
+      end
     end
   end
 end
