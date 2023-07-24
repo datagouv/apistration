8b2909c0009d54ff7a85cc7db0cd64d7277fdf6b
Loïc Delmaire <loic.delmaire@gmail.com>
2023-07-24 15:33:40 +0200

    Introduce config/credentials/staging.yml.enc
    Closes https://github.com/etalab/siade/issues/572

--- siade/bin/retrieve_master_key.sh
+++ siade/bin/retrieve_master_key.sh
@@ -9,8 +9,10 @@ path=$1
 
 cd $path
 
-key=`ansible-vault view secrets/siade_master_key.yml | grep MASTER_KEY | awk '{print $2}'`
+key=`ansible-vault view secrets/siade_master_key.yml | grep "^MASTER_KEY" | awk '{print $2}'`
+staging_key=`ansible-vault view secrets/siade_master_key.yml | grep "STAGING_MASTER_KEY" | awk '{print $2}'`
 
 cd -
 
 echo $key > config/master.key
+echo $staging_key > config/credentials/staging.key
