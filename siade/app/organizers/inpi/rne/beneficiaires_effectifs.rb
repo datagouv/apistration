4e82a85f16d22225cba04a77cbef05a007475389
Loïc Delmaire <loic.delmaire@gmail.com>
2023-07-25 12:19:54 +0200

    Introduce INPI::RNE::BeneficiairesEffectifs

---
+++ siade/app/organizers/inpi/rne/beneficiaires_effectifs.rb
@@ -0,0 +1,11 @@
+class INPI::RNE::BeneficiairesEffectifs < RetrieverOrganizer
+  organize ValidateSiren,
+    INPI::RNE::Authenticate,
+    INPI::RNE::MakeRequest,
+    INPI::RNE::BeneficiairesEffectifs::ValidateResponse,
+    INPI::RNE::BeneficiairesEffectifs::BuildResourceCollection
+
+  def provider_name
+    'INPI - RNE'
+  end
+end
