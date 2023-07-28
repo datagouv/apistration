80d4d48beb9e0758e9c02946fbe378c20dec46cb
Loïc Delmaire <loic.delmaire@gmail.com>
2023-07-28 10:29:05 +0200

    api_particulier/v2/pole_emploi/indemnites no longer have pole_emploi_identite scope

--- siade/app/serializers/api_particulier/pole_emploi/indemnites/v2.rb
+++ siade/app/serializers/api_particulier/pole_emploi/indemnites/v2.rb
@@ -1,4 +1,4 @@
 class APIParticulier::PoleEmploi::Indemnites::V2 < APIParticulier::V2BaseSerializer
-  attribute :identifiant, if: -> { scope?(:pole_emploi_identite) }
+  attribute :identifiant
   attribute :paiements, if: -> { scope?(:pole_emploi_paiements) }
 end
