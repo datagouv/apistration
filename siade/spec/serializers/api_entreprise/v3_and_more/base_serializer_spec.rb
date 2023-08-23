6c9d590740dcd5fae4990ca4b2065e5077d95e5c
Loïc Delmaire <loic.delmaire@gmail.com>
2023-08-23 13:35:47 +0200

    Test APIEntreprise::V3AndMore::BaseSerializer#url_for_proxied_file

--- siade/spec/serializers/api_entreprise/v3_and_more/base_serializer_spec.rb
+++ siade/spec/serializers/api_entreprise/v3_and_more/base_serializer_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe APIEntreprise::V3AndMore::BaseSerializer, type: :serializer do
         object.attr_1 + object.attr_2
       end
 
+      attribute :inside_url do |object|
+        url_for_proxied_file(object.inside_url)
+      end
+
       meta do |object|
         {
           example_meta: object.meta_1
@@ -28,10 +32,15 @@ RSpec.describe APIEntreprise::V3AndMore::BaseSerializer, type: :serializer do
       attr_1: 'attr_1',
       attr_2: 'attr_2',
       meta_1: 'meta_1',
+      inside_url: 'inside_url',
       url_1: 'url_1'
     })
 
     BundledData.new(data:)
+  end
+
+  before do
+    allow(ProxiedFileService).to receive(:set).and_return('uuid')
   end
 
   it do
@@ -39,7 +48,8 @@ RSpec.describe APIEntreprise::V3AndMore::BaseSerializer, type: :serializer do
       data: {
         attr_1: 'attr_1',
         attr_2: 'attr_2',
-        attr_3: 'attr_1attr_2'
+        attr_3: 'attr_1attr_2',
+        inside_url: 'http://test.entreprise.api.gouv.fr/proxy/files/uuid',
       },
       links: {
         example_url: 'url_1'
