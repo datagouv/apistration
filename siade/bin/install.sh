50d9337666040a1c69ab8e2c78627845d2d6ab52
Loïc Delmaire <loic.delmaire@gmail.com>
2023-09-04 15:44:07 +0200

    CI: Prepare to use gpg
    Enhance install.sh to generate missing gpg keys

--- siade/bin/install.sh
+++ siade/bin/install.sh
@@ -4,6 +4,32 @@ which brew &> /dev/null && brew ls --versions gpgme &> /dev/null || brew install
 which apt-get &> /dev/null && apt-get install libgpgme11-dev
 find config/gpg_public_keys_for_data_encryption -type f -name "*.asc" -print0 | xargs -0 gpg2 --import
 
+gpg_key_email=`ruby -e 'require "yaml" ; print YAML.load_file("config/encrypt_data_signer_data.yml")["shared"]["signer_email"]'`
+gpg_key_passphrase=`ruby -e 'require "yaml" ; print YAML.load_file("config/encrypt_data_signer_data.yml")["shared"]["signer_passphrase"]'`
+
+gpg2 --list-keys | grep "$gpg_key_email" &> /dev/null
+
+if [ $? -ne 0 ] ; then
+  echo "Key not found for $gpg_key_email, generate one"
+
+  echo "Key-Type: DSA
+  Key-Length: 1024
+  Subkey-Type: ELG-E
+  Subkey-Length: 1024
+  Name-Real: Test User
+  Name-Email: $gpg_key_email
+  Expire-Date: 0
+  Passphrase: $gpg_key_passphrase
+  %commit" | gpg2 --batch --gen-key
+  mkdir -p ~/.gnupg
+  touch ~/.gnupg/gpg.conf
+  grep "use-agent" ~/.gnupg/gpg.conf &> /dev/null || echo "use-agent" >> ~/.gnupg/gpg.conf
+  grep "pinentry-mode loopback" ~/.gnupg/gpg.conf &> /dev/null || echo "pinentry-mode loopback" >> ~/.gnupg/gpg.conf
+  touch ~/.gnupg/gpg-agent.conf
+  grep "allow-loopback-pinentry" ~/.gnupg/gpg-agent.conf &> /dev/null || echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
+  echo RELOADAGENT | gpg-connect-agent
+fi
+
 psql -f ./db/postgresql_setup.txt
 bundle install
 bin/rails db:schema:load RAILS_ENV=test
