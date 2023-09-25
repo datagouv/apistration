f52668c14da5259cae858e92323f575e56b687f8
Thomas COMES <thomas.comes.tech@gmail.com>
2023-09-25 12:06:48 +0200

    Added legacy token accessor to Token

--- siade/app/models/token.rb
+++ siade/app/models/token.rb
@@ -4,4 +4,12 @@ class Token < ApplicationRecord
   def siret
     authorization_request.try(:siret)
   end
+
+  def legacy_token?
+    extra_info? && extra_info['legacy_token_id'].present?
+  end
+
+  def legacy_token_migrated?
+    legacy_token? && extra_info['legacy_token_migrated'].present?
+  end
 end
