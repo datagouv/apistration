c9fa735e19f53f7c0c6dd815403cb012f2a1ae06
Loïc Delmaire <loic.delmaire@gmail.com>
2023-09-26 08:49:22 +0200

    Harden tracking error for Sentry
    Can't find the root cause of the non-tracking behaviour of this, but
    `event` can be another thing than a request, so this block can raise a
    NoMethodError (and it's rescued within sentry gem, can be anywhere
    there's a lot of rescue with some logging).
    
    Tested directly in production, it works.

--- siade/config/initializers/sentry.rb
+++ siade/config/initializers/sentry.rb
@@ -7,7 +7,11 @@ Sentry.init do |config|
   config.traces_sample_rate = 1.0
 
   config.before_send = lambda do |event, _hint|
-    event.request.headers.delete('X-Api-Key')
+    begin
+      event.request.headers.delete('X-Api-Key')
+    rescue NoMethodError
+    end
+
     event
   end
 end
