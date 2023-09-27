be13496ac2dab4d91944a2a6e1d7528638a2ac6e
Loïc Delmaire <loic.delmaire@gmail.com>
2023-09-27 22:10:49 +0200

    Introduce AccessLogPingDriver

---
+++ siade/app/ping_drivers/access_log_ping_driver.rb
@@ -0,0 +1,26 @@
+class AccessLogPingDriver < ApplicationPingDriver
+  attr_reader :period, :routes
+
+  def perform
+    valid_access_logs.any? ? :ok : :bad_gateway
+  end
+
+  private
+
+  def valid_access_logs
+    AccessLog.where(
+      route: routes,
+      status: '200',
+      timestamp: (beggining_of_period..)
+    ).limit(1)
+  end
+
+  def beggining_of_period
+    Time.zone.now - period
+  end
+
+  def build_context(driver_params)
+    @period = driver_params.fetch(:period)
+    @routes = driver_params.fetch(:routes)
+  end
+end
---
+++ siade/spec/ping_drivers/access_log_ping_driver_spec.rb
@@ -0,0 +1,59 @@
+RSpec.describe AccessLogPingDriver, type: :ping_driver do
+  describe '#perform' do
+    subject(:make_ping) { described_class.new(params).perform }
+
+    let(:params) do
+      Rails.application.config_for(:pings)[:api_kind][:with_access_logs][:driver_params]
+    end
+    let(:routes) { params.fetch(:routes) }
+    let(:valid_access_log_data) do
+      {
+        route: routes.sample,
+        status: '200',
+        timestamp: 1.minute.ago
+      }
+    end
+
+    before do
+      AccessLog.delete_all
+    end
+
+    context 'when there is no data' do
+      it { is_expected.to eq(:bad_gateway) }
+    end
+
+    context 'when there is data' do
+      context 'when there is no data for routes in the period' do
+        before do
+          AccessLog.create!(route: routes.sample, timestamp: 6.minutes.ago)
+        end
+
+        it { is_expected.to eq(:bad_gateway) }
+      end
+
+      context 'when there is data for routes in the period' do
+        before do
+          AccessLog.create!(valid_access_log_data.merge(extra_data))
+        end
+
+        context 'when it is not a valid route' do
+          let(:extra_data) { { route: 'invalid' } }
+
+          it { is_expected.to eq(:bad_gateway) }
+        end
+
+        context 'when it is not a 200 status' do
+          let(:extra_data) { { status: '404' } }
+
+          it { is_expected.to eq(:bad_gateway) }
+        end
+
+        context 'when everything is valid' do
+          let(:extra_data) { {} }
+
+          it { is_expected.to eq(:ok) }
+        end
+      end
+    end
+  end
+end
