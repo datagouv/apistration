2f1356d86d61cd91c9374e37d3235bf9cba1a7de
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-04 10:33:36 +0200

    Introduce CarifOref::CertificationsQualiopiFranceCompetences::BuildResource

---
+++ siade/app/interactors/carif_oref/certifications_qualiopi_france_competences/build_resource.rb
@@ -0,0 +1,91 @@
+class CarifOref::CertificationsQualiopiFranceCompetences::BuildResource < BuildResource
+  protected
+
+  def resource_attributes
+    {
+      siret: json_body['siret_actif'],
+      code_uai: etablissement['uai'],
+      unite_legale_avec_plusieurs_nda: string_to_bool(unite_legale['multinda']),
+      declarations_activites_etablissement:,
+      habilitations_france_competence:
+    }
+  end
+
+  private
+
+  # rubocop:disable Metrics/AbcSize
+  def declarations_activites_etablissement
+    json_body['nda'].map do |nda_payload|
+      {
+        numero_de_declaration: nda_payload['nda'],
+        actif: string_to_bool(nda_payload['actif']),
+        date_derniere_declaration: nda_payload['date_derniere_declaration'],
+        date_fin_exercice: nda_payload['fin_exercice'],
+        certification_qualiopi: {
+          action_formation: string_to_bool(nda_payload['qualiopi_action_formation']),
+          bilan_competences: string_to_bool(nda_payload['qualiopi_bilan_comptence']),
+          validation_acquis_experience: string_to_bool(nda_payload['qualiopi_vae']),
+          apprentissage: string_to_bool(nda_payload['qualiopi_apprentissage']),
+          origine_obtention_qualiopi: nda_payload['obtention_qualiopi']
+        },
+        specialites: {
+          specialite_1: build_speciality(nda_payload, 1),
+          specialite_2: build_speciality(nda_payload, 2),
+          specialite_3: build_speciality(nda_payload, 3)
+        }
+      }
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  # rubocop:disable Metrics/AbcSize
+  def habilitations_france_competence
+    valid_habilitations.map do |habilitation_payload|
+      {
+        code: habilitation_payload['code_fc'],
+        actif: string_to_bool(habilitation_payload['actif']),
+        date_actif: format_date(habilitation_payload['date_habilitation']),
+        date_fin_enregistrement: format_date(habilitation_payload['date_fin_enregistrement']),
+        date_decision: format_date(habilitation_payload['date_decision']),
+        habilitation_pour_former: string_to_bool(habilitation_payload['a_former']),
+        habilitation_pour_organiser_l_evaluation: string_to_bool(habilitation_payload['a_organiser']),
+        sirets_organismes_certificateurs: habilitation_payload['certificateur'].pluck('siret')
+      }
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def valid_habilitations
+    json_body['habilitations'].reject do |habilitation_payload|
+      habilitation_payload['etat_habilitation'] == 'Supprimé'
+    end
+  end
+
+  def build_speciality(nda_payload, index)
+    {
+      code: nda_payload["code_specialite_#{index}"],
+      libelle: nda_payload["libelle_specialite_#{index}"]
+    }
+  end
+
+  def etablissement
+    json_body['etablissement']
+  end
+
+  def unite_legale
+    json_body['unite_legale']
+  end
+
+  def string_to_bool(string)
+    case string
+    when 'True' then true
+    when 'False' then false
+    end
+  end
+
+  def format_date(date)
+    return unless date
+
+    Date.parse(date).strftime('%Y-%m-%d')
+  end
+end
---
+++ siade/spec/interactors/carif_oref/certifications_qualiopi_france_competences/build_resource_spec.rb
@@ -0,0 +1,64 @@
+RSpec.describe CarifOref::CertificationsQualiopiFranceCompetences::BuildResource, type: :build_resource do
+  subject { instance }
+
+  let(:instance) { described_class.call(response:) }
+  let(:response) { instance_double(Net::HTTPOK, body:) }
+  let(:body) { read_payload_file('carif_oref/qualiopi/valid.json') }
+
+  let(:valid_payload) do
+    {
+      siret: '12000101100010',
+      code_uai: nil,
+      unite_legale_avec_plusieurs_nda: false,
+      declarations_activites_etablissement: [
+        {
+          numero_de_declaration: '12345671234',
+          actif: true,
+          date_derniere_declaration: '2021-01-31',
+          date_fin_exercice: '2022-12-31',
+          certification_qualiopi: {
+            action_formation: true,
+            bilan_competences: false,
+            validation_acquis_experience: true,
+            apprentissage: true,
+            origine_obtention_qualiopi: 'Qualiopi direct sur le NDA'
+          },
+          specialites: {
+            specialite_1: {
+              code: '114',
+              libelle: 'Mathématiques, statistiques'
+            },
+            specialite_2: {
+              code: '128',
+              libelle: 'Droit, sciences politiques'
+            },
+            specialite_3: {
+              code: '314',
+              libelle: 'Comptabilité, gestion'
+            }
+          }
+        }
+      ],
+      habilitations_france_competence: [
+        code: 'RNCP09001',
+        actif: true,
+        date_actif: nil,
+        date_fin_enregistrement: '2024-01-01',
+        date_decision: nil,
+        habilitation_pour_former: true,
+        habilitation_pour_organiser_l_evaluation: true,
+        sirets_organismes_certificateurs: [
+          '13002526500013'
+        ]
+      ]
+    }
+  end
+
+  it { is_expected.to be_a_success }
+
+  describe 'resource' do
+    subject { instance.bundled_data.data.to_h }
+
+    it { is_expected.to eq(valid_payload) }
+  end
+end
