2dab2e12ed7ddfd8fd774b3da19fc828ab1e6fd2
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-18 17:10:33 +0200

    Introduce SyncPingsWithMonitorsRemoteService

---
+++ siade/app/services/sync_pings_with_monitors_remote_service.rb
@@ -0,0 +1,73 @@
+class SyncPingsWithMonitorsRemoteService
+  def perform
+    pings.each do |api_kind, config|
+      config.each do |ping_identifier, ping_config|
+        next if ping_config[:status_page].blank?
+
+        create_or_update_monitor(api_kind, ping_identifier, ping_config)
+      end
+    end
+  end
+
+  private
+
+  def create_or_update_monitor(api_kind, ping_identifier, ping_config)
+    if (remote_monitor = find_remote_monitor(api_kind, ping_identifier))
+      update_monitor(api_kind, ping_identifier, ping_config, remote_monitor)
+    else
+      create_monitor(api_kind, ping_identifier, ping_config)
+    end
+  rescue StandardError => e
+    MonitoringService.instance.track(
+      'error',
+      'Fail to update ping properties on status page',
+      {
+        identifier: ping_identifier,
+        error: e.message,
+        backtrace: e.backtrace
+      }
+    )
+  end
+
+  def create_monitor(api_kind, ping_identifier, ping_config)
+    hyperping_api_service.create_monitor(
+      name: ping_config[:status_page][:name],
+      url: url_for(api_kind, ping_identifier)
+    )
+  end
+
+  def update_monitor(api_kind, ping_identifier, ping_config, remote_monitor)
+    hyperping_api_service.update_monitor(
+      remote_monitor['uuid'],
+      name: ping_config[:status_page][:name],
+      url: url_for(api_kind, ping_identifier)
+    )
+  end
+
+  def url_for(api_kind, identifier)
+    case api_kind
+    when :api_entreprise
+      "https://entreprise.api.gouv.fr/ping/#{identifier}"
+    else
+      "https://particulier.api.gouv.fr/api/#{identifier}/ping"
+    end
+  end
+
+  def find_remote_monitor(api_kind, ping_identifier)
+    remote_monitors.find do |remote_monitor|
+      remote_monitor['url'] == url_for(api_kind, ping_identifier)
+    end
+  end
+
+  def hyperping_api_service
+    @hyperping_api_service ||= HyperpingAPI.new
+  end
+
+  def remote_monitors
+    @remote_monitors ||= hyperping_api_service.get_monitors
+  end
+
+  def pings
+    Rails.application.config_for(:pings)
+  end
+end
---
+++ siade/spec/services/sync_pings_with_monitors_remote_service_spec.rb
@@ -0,0 +1,71 @@
+RSpec.describe SyncPingsWithMonitorsRemoteService, type: :service do
+  describe '#perform' do
+    subject(:sync_pings_with_monitors) { described_class.new.perform }
+
+    let(:get_monitors) do
+      [
+        {
+          'uuid' => '1',
+          'name' => 'With Retriever and Maintenance',
+          'url' => 'https://particulier.api.gouv.fr/api/with_retriever_and_maintenance/ping'
+        }
+      ]
+    end
+
+    before do
+      allow(HyperpingAPI).to receive(:new).and_return(hyperping_api_service)
+    end
+
+    context 'when everything is fine' do
+      let(:hyperping_api_service) { instance_double(HyperpingAPI, get_monitors:, create_monitor: {}, update_monitor: {}) }
+
+      it 'creates missing API with valid attributes' do
+        sync_pings_with_monitors
+
+        expect(hyperping_api_service).to have_received(:create_monitor).with(
+          name: 'With Retriever',
+          url: 'https://particulier.api.gouv.fr/api/with_retriever/ping'
+        )
+      end
+
+      it 'updates existing API with valid attributes' do
+        sync_pings_with_monitors
+
+        expect(hyperping_api_service).to have_received(:update_monitor).with(
+          '1',
+          name: 'With Retriever and Maintenance',
+          url: 'https://particulier.api.gouv.fr/api/with_retriever_and_maintenance/ping'
+        )
+      end
+    end
+
+    context 'when there is an error on one' do
+      let(:hyperping_api_service) { instance_double(HyperpingAPI, get_monitors:, create_monitor: {}) }
+
+      before do
+        allow(hyperping_api_service).to receive(:update_monitor).and_raise(JSON::ParserError)
+      end
+
+      it 'works for the other' do
+        sync_pings_with_monitors
+
+        expect(hyperping_api_service).to have_received(:create_monitor).with(
+          name: 'With Retriever',
+          url: 'https://particulier.api.gouv.fr/api/with_retriever/ping'
+        )
+      end
+
+      it 'tracks error' do
+        expect(MonitoringService.instance).to receive(:track).with(
+          'error',
+          'Fail to update ping properties on status page',
+          hash_including(
+            identifier: :with_retriever_and_maintenance
+          )
+        )
+
+        sync_pings_with_monitors
+      end
+    end
+  end
+end
