9298c76cebecba6e0ba00a51ff8510c9ecf707a0
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-19 11:40:31 +0200

    Scout commit

--- siade/app/controllers/api_particulier/ping_providers_controller.rb
+++ siade/app/controllers/api_particulier/ping_providers_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class APIParticulier::PingProvidersController < ApplicationController
   def show
     render PingService.new('api_particulier', params[:provider]).perform
