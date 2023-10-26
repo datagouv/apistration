201d1119303c627fa53f0b3f1da24e020ecbf9c5
Loïc Delmaire <loic.delmaire@gmail.com>
2023-10-26 15:30:24 +0200

    Add caching for the day for Qualibat

--- siade/app/controllers/api_entreprise/v3_and_more/qualibat/certifications_batiment_controller.rb
+++ siade/app/controllers/api_entreprise/v3_and_more/qualibat/certifications_batiment_controller.rb
@@ -1,6 +1,6 @@
 class APIEntreprise::V3AndMore::QUALIBAT::CertificationsBatimentController < APIEntreprise::V3AndMore::BaseController
   def show
-    organizer = retrieve_payload_data(::QUALIBAT::CertificationsBatiment)
+    organizer = retrieve_payload_data(::QUALIBAT::CertificationsBatiment, cache: true, expires_in:)
 
     if organizer.success?
       render json:   serialize_data(organizer),
@@ -20,5 +20,9 @@ class APIEntreprise::V3AndMore::QUALIBAT::CertificationsBatimentController < API
 
   def serializer_module
     ::APIEntreprise::QUALIBAT::CertificationBatimentSerializer
+  end
+
+  def expires_in
+    (Time.zone.now.end_of_day - Time.zone.now).to_i
   end
 end
