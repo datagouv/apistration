e5775756d64dda021500038ce0a96db7d262efb9
Samuelfaure <samuel.faure.dev@gmail.com>
2024-02-23 13:17:00 +0100

    Scout clean
    Started merging FranceConnect here before realizing I didnt have to...
    Well I factorized a little bit so let's keep it

--- siade/bin/merge_swaggers_particulier.rb
+++ siade/bin/merge_swaggers_particulier.rb
@@ -2,10 +2,18 @@ require 'yaml'
 require 'date'
 require 'active_support/core_ext/hash'
 
-def merge_openapi_particulier(partial_path)
-  openapi_particulier_path = File.expand_path('../../swagger/openapi-particulier.yaml', __FILE__)
-  openapi_particulier = YAML.load_file(openapi_particulier_path, permitted_classes: [Date], aliases: true)
+def merge_all_openapi_particulier
+  partial_paths = %w[
+    ../../swagger/api_particulier_open_api_static/v2.yaml
+  ]
 
+  partial_paths.each do |partial_path|
+    partial = File.expand_path(partial_path, __FILE__)
+    merge_openapi_particulier(partial)
+  end
+end
+
+def merge_openapi_particulier(partial_path)
   partial = YAML.load_file(partial_path, permitted_classes: [Date], aliases: true)
 
   merged_openapi = merge_openapi_paths(openapi_particulier, partial)
@@ -19,11 +27,12 @@ def merge_openapi_paths(base_openapi, partial_openapi)
   base_openapi
 end
 
-partial_paths = %w[
-  ../../swagger/api_particulier_open_api_static/v2.yaml
-]
-
-partial_paths.each do |partial_path|
-  partial = File.expand_path(partial_path, __FILE__)
-  merge_openapi_particulier(partial)
+def openapi_particulier_path
+  File.expand_path('../swagger/openapi-particulier.yaml', __dir__)
 end
+
+def openapi_particulier
+  YAML.load_file(openapi_particulier_path, permitted_classes: [Date], aliases: true)
+end
+
+merge_all_openapi_particulier
