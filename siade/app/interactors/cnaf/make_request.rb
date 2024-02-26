ebebf284267fb0e08b1ab0974b0e087377853af1
Samuelfaure <samuel.faure.dev@gmail.com>
2024-02-26 13:18:25 +0100

    FranceConnect mocking params for CNAF Qfv2/CSS

--- siade/app/interactors/cnaf/make_request.rb
+++ siade/app/interactors/cnaf/make_request.rb
@@ -1,22 +1,15 @@
 class CNAF::MakeRequest < MakeRequest::Get
   protected
 
-  # rubocop:disable Metrics/AbcSize
   def mocking_params
-    {
-      nomUsage: context.params[:nom_usage],
-      nomNaissance: context.params[:nom_naissance],
-      prenoms: context.params[:prenoms],
-      anneeDateDeNaissance: context.params[:annee_date_de_naissance],
-      moisDateDeNaissance: context.params[:mois_date_de_naissance],
-      jourDateDeNaissance: context.params[:jour_date_de_naissance],
-      codeInseeLieuDeNaissance: context.params[:code_insee_lieu_de_naissance],
-      codePaysLieuDeNaissance: context.params[:code_pays_lieu_de_naissance],
-      sexe: context.params[:gender],
-      recipient: context.params[:recipient]
-    }.compact
+    mocking_params = if context.params[:france_connect]
+                       france_connect_mocking_params
+                     else
+                       civility_mocking_params
+                     end
+
+    mocking_params.merge!(common_mocking_params).compact
   end
-  # rubocop:enable Metrics/AbcSize
 
   def extra_headers(request)
     request['Content-Type'] = 'application/json'
@@ -26,6 +19,40 @@ class CNAF::MakeRequest < MakeRequest::Get
   end
 
   private
+
+  def common_mocking_params
+    {
+      recipient: context.params[:recipient]
+    }
+  end
+
+  # rubocop:disable Metrics/AbcSize
+  def civility_mocking_params
+    {
+      nomUsage: context.params[:nom_usage],
+      nomNaissance: context.params[:nom_naissance],
+      prenoms: context.params[:prenoms],
+      anneeDateDeNaissance: context.params[:annee_date_de_naissance],
+      moisDateDeNaissance: context.params[:mois_date_de_naissance],
+      jourDateDeNaissance: context.params[:jour_date_de_naissance],
+      codeInseeLieuDeNaissance: context.params[:code_insee_lieu_de_naissance],
+      codePaysLieuDeNaissance: context.params[:code_pays_lieu_de_naissance],
+      sexe: context.params[:gender]
+    }
+  end
+
+  def france_connect_mocking_params
+    {
+      nomUsage: context.params[:nom_usage],
+      nomNaissance: context.params[:nom_naissance],
+      prenoms: context.params[:prenoms],
+      dateNaissance: date_naissance,
+      codeInseeLieuDeNaissance: context.params[:code_insee_lieu_de_naissance],
+      codePaysLieuDeNaissance: context.params[:code_pays_lieu_de_naissance],
+      sexe: context.params[:gender]
+    }
+  end
+  # rubocop:enable Metrics/AbcSize
 
   def token
     context.token
