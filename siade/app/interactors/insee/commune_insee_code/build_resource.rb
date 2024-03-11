ecacc31bf1b9ea903addc6d6df1e2302f48a7a94
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-11 20:10:34 +0100

    Introduce INSEE::CommuneINSEECode::BuildResource

---
+++ siade/app/interactors/insee/commune_insee_code/build_resource.rb
@@ -0,0 +1,46 @@
+class INSEE::CommuneINSEECode::BuildResource < BuildResource
+  before do
+    context.errors ||= []
+  end
+
+  protected
+
+  def resource_attributes
+    {
+      code_insee:
+    }
+  end
+
+  private
+
+  def code_insee
+    if communes.count == 1
+      communes.first['code']
+    elsif communes.count > 1
+      find_commune_with_departement_code!
+    else
+      resource_not_found!
+    end
+  end
+
+  def communes
+    json_body
+  end
+
+  def find_commune_with_departement_code!
+    valid_commune = communes.detect { |commune| commune['code'].start_with?(code_insee_departement_de_naissance) }
+
+    resource_not_found! if valid_commune.blank?
+
+    valid_commune['code']
+  end
+
+  def resource_not_found!
+    context.errors << NotFoundError.new(context.provider_name, 'Aucun code commune INSEE ne correspond à ces critères')
+    context.fail!
+  end
+
+  def code_insee_departement_de_naissance
+    context.params[:code_insee_departement_de_naissance]
+  end
+end
---
+++ siade/spec/interactors/insee/commune_insee_code/build_resource_spec.rb
@@ -0,0 +1,61 @@
+RSpec.describe INSEE::CommuneINSEECode::BuildResource, type: :build_resource do
+  subject(:organizer) { described_class.call(response:, params:) }
+
+  describe 'with real http calls' do
+    let(:response) { INSEE::Metadonnees::MakeRequest.call(params:, token:).response }
+    let(:token) { INSEE::Authenticate.call.token }
+
+    let(:params) do
+      {
+        nom_commune_naissance:,
+        annee_date_de_naissance:,
+        code_insee_departement_de_naissance:
+      }
+    end
+
+    let(:annee_date_de_naissance) { '2000' }
+    let(:code_insee_departement_de_naissance) { '75' }
+
+    context 'with a response which has 1 result', vcr: { cassette_name: 'insee/metadonnees/one_result' } do
+      let(:nom_commune_naissance) { 'Gennevilliers' }
+
+      it { is_expected.to be_a_success }
+
+      describe 'resource' do
+        subject { organizer.bundled_data.data }
+
+        it { is_expected.to be_a(Resource) }
+
+        its(:code_insee) { is_expected.to eq('92036') }
+      end
+    end
+
+    context 'with a response which has more than 2 results', vcr: { cassette_name: 'insee/metadonnees/multiple_results' } do
+      let(:nom_commune_naissance) { 'La Rochette' }
+
+      context 'with 05 as code_insee_departement_de_naissance' do
+        let(:code_insee_departement_de_naissance) { '05' }
+
+        describe 'resource' do
+          subject { organizer.bundled_data.data }
+
+          it { is_expected.to be_a(Resource) }
+
+          its(:code_insee) { is_expected.to eq('05124') }
+        end
+      end
+
+      context 'with 04 as code_insee_departement_de_naissance' do
+        let(:code_insee_departement_de_naissance) { '04' }
+
+        describe 'resource' do
+          subject { organizer.bundled_data.data }
+
+          it { is_expected.to be_a(Resource) }
+
+          its(:code_insee) { is_expected.to eq('04170') }
+        end
+      end
+    end
+  end
+end
