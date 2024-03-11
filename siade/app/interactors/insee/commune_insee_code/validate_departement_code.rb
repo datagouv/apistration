553e92c4c288e92a5438bfa3a22fcd2669531aad
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-11 16:13:48 +0100

    Introduce INSEE::CommuneINSEECode::ValidateDepartementCode

---
+++ siade/app/interactors/insee/commune_insee_code/validate_departement_code.rb
@@ -0,0 +1,31 @@
+class INSEE::CommuneINSEECode::ValidateDepartementCode < ValidateParamInteractor
+  def call
+    return if valid?
+
+    invalid_param!(:code_insee_departement_de_naissance)
+  end
+
+  private
+
+  def valid?
+    param(:code_insee_departement_de_naissance).present? &&
+      (
+        corse? ||
+          dom_tom? ||
+          metropole?
+      )
+  end
+
+  def corse?
+    param(:code_insee_departement_de_naissance).in?(%w[2A 2B])
+  end
+
+  def dom_tom?
+    param(:code_insee_departement_de_naissance).in?(%w[971 972 973 974 976])
+  end
+
+  def metropole?
+    param(:code_insee_departement_de_naissance).match?(/\A\d{2}\z/) &&
+      (1..95).cover?(param(:code_insee_departement_de_naissance).to_i)
+  end
+end
---
+++ siade/spec/interactors/insee/commune_insee_code/validate_departement_code_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe INSEE::CommuneINSEECode::ValidateDepartementCode do
+  subject { described_class.call(params: { code_insee_departement_de_naissance: }) }
+
+  context 'with corse departement code' do
+    let(:code_insee_departement_de_naissance) { '2A' }
+
+    it { is_expected.to be_success }
+  end
+
+  context 'with dom tom departement code' do
+    let(:code_insee_departement_de_naissance) { '971' }
+
+    it { is_expected.to be_success }
+  end
+
+  context 'with metropole departement code' do
+    let(:code_insee_departement_de_naissance) { '01' }
+
+    it { is_expected.to be_success }
+
+    context 'when not formatted well' do
+      let(:code_insee_departement_de_naissance) { '1' }
+
+      it { is_expected.to be_a_failure }
+    end
+  end
+
+  context 'with invalid departement code' do
+    let(:code_insee_departement_de_naissance) { '999' }
+
+    it { is_expected.to be_a_failure }
+  end
+end
