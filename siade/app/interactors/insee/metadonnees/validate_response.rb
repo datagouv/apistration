ca8cdf6d43e9b83c16f5f97d6387d100d3e7685c
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-11 18:34:58 +0100

    Introduce INSEE::Metadonnees::ValidateResponse

---
+++ siade/app/interactors/insee/metadonnees/validate_response.rb
@@ -0,0 +1,16 @@
+class INSEE::Metadonnees::ValidateResponse < ValidateResponse
+  def call
+    return if http_ok? && valid_json?
+
+    resource_not_found! if http_not_found?
+
+    unknown_provider_response!
+  end
+
+  private
+
+  def valid_json?
+    json_body.count.positive? &&
+      json_body[0]['code'].present?
+  end
+end
---
+++ siade/spec/interactors/insee/metadonnees/validate_response_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe INSEE::Metadonnees::ValidateResponse, type: :validate_response do
+  subject { described_class.call(response:, provider_name: 'INSEE') }
+
+  describe 'with real http calls' do
+    let(:response) { INSEE::Metadonnees::MakeRequest.call(params:, token:).response }
+    let(:token) { INSEE::Authenticate.call.token }
+
+    let(:params) do
+      {
+        nom_commune_naissance:,
+        annee_date_de_naissance:
+      }
+    end
+
+    let(:annee_date_de_naissance) { '2000' }
+
+    context 'with a response which has 1 result', vcr: { cassette_name: 'insee/metadonnees/one_result' } do
+      let(:nom_commune_naissance) { 'Gennevilliers' }
+
+      it { is_expected.to be_a_success }
+
+      its(:errors) { is_expected.to be_empty }
+    end
+
+    context 'with a response which has 2 results', vcr: { cassette_name: 'insee/metadonnees/multiple_results' } do
+      let(:nom_commune_naissance) { 'La Rochette' }
+
+      it { is_expected.to be_a_success }
+
+      its(:errors) { is_expected.to be_empty }
+    end
+  end
+
+  context 'with a not found response' do
+    let(:response) { instance_double(Net::HTTPNotFound, code: '404') }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(NotFoundError)) }
+  end
+
+  context 'with an unknown error' do
+    let(:response) { instance_double(Net::HTTPResponse, code: '418') }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(ProviderUnknownError)) }
+  end
+end
