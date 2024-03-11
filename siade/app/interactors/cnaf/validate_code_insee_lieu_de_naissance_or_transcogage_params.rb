373d249e68279723fe607285a9b2a673e2c03295
Loïc Delmaire <loic.delmaire@gmail.com>
2024-03-11 21:13:46 +0100

    Introduce CNAF::ValidateCodeINSEELieuDeNaissanceOrTranscogageParams
    Prepare to use INSEE::CommuneINSEECode

---
+++ siade/app/interactors/cnaf/validate_code_insee_lieu_de_naissance_or_transcogage_params.rb
@@ -0,0 +1,30 @@
+class CNAF::ValidateCodeINSEELieuDeNaissanceOrTranscogageParams < ValidateParamInteractor
+  def call
+    validator = call_validator
+
+    return if validator.success?
+
+    context.errors = validator.errors
+    context.fail!
+  end
+
+  private
+
+  def call_validator
+    if code_insee_lieu_de_naissance?
+      CNAF::ValidateCodeINSEELieuDeNaissance.call(params: context.params)
+    elsif transcogage_params?
+      INSEE::CommuneINSEECode::ValidateParams.call(params: context.params)
+    else
+      invalid_param!(:birth_place)
+    end
+  end
+
+  def code_insee_lieu_de_naissance?
+    params[:code_insee_lieu_de_naissance].present?
+  end
+
+  def transcogage_params?
+    %i[nom_commune_naissance annee_date_de_naissance code_insee_departement_de_naissance].all? { |key| context.params[key].present? }
+  end
+end
