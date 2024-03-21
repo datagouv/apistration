b7737b461dfc52107efcb280111a71952ad4629d
Samuelfaure <samuel.faure.dev@gmail.com>
2024-03-21 13:02:57 +0400

    Boyscout: sugar methods

--- siade/app/interactors/cnaf/build_resource.rb
+++ siade/app/interactors/cnaf/build_resource.rb
@@ -29,9 +29,21 @@ class CNAF::BuildResource < BuildResource
 
   def latest_open_prestation
     @latest_open_prestation ||= prestations
-      .select { |prestation| prestation['etat']['cd'] == 10 }
-      .select { |prestation| matching_prestations.include?(prestation['cd']) }
-      .min { |prestation1, prestation2| Date.parse(prestation2['dtOuvertureDroit']) <=> Date.parse(prestation1['dtOuvertureDroit']) }
+      .select { |prestation| open?(prestation) }
+      .select { |prestation| matches_current_prestation?(prestation) }
+      .min { |prestation1, prestation2| sort_by_ouverture_droit(prestation1, prestation2) }
+  end
+
+  def open?(prestation)
+    prestation['etat']['cd'] == 10
+  end
+
+  def matches_current_prestation?(prestation)
+    matching_prestations.include?(prestation['cd'])
+  end
+
+  def sort_by_ouverture_droit(prestation1, prestation2)
+    Date.parse(prestation1['dtOuvertureDroit']) <=> Date.parse(prestation2['dtOuvertureDroit'])
   end
 
   def prestations
