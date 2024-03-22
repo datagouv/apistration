4fbde324b8bceeb5d912624e8cc7ea5c2b55364a
Samuelfaure <samuel.faure.dev@gmail.com>
2024-03-22 14:26:57 +0400

    Boyscout: clean dead code
    This method is not used anywhere + is wrong
    'legacy_token_migrated' is set to 't' not 'true'

--- siade/app/models/token.rb
+++ siade/app/models/token.rb
@@ -13,8 +13,4 @@ class Token < ApplicationRecord
   def legacy_token?
     extra_info.present? && extra_info['legacy_token_id'].present?
   end
-
-  def legacy_token_migrated?
-    legacy_token? && (extra_info['legacy_token_migrated'].try(:to_s) == 'true')
-  end
 end
