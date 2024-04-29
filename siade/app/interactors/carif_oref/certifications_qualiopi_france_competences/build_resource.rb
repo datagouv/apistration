2ce311748b50e58a937fc310721d07ac756fda95
Thomas COMES <thomas.comes.tech@gmail.com>
2024-04-29 14:53:19 +0200

    Updating qualiopi response to new format
    Updated cassetes and payload for tests
    Updated boolean matching behavior

--- siade/app/interactors/carif_oref/certifications_qualiopi_france_competences/build_resource.rb
+++ siade/app/interactors/carif_oref/certifications_qualiopi_france_competences/build_resource.rb
@@ -5,7 +5,7 @@ class CarifOref::CertificationsQualiopiFranceCompetences::BuildResource < BuildR
     {
       siret: json_body['siret_actif'],
       code_uai: etablissement['uai'],
-      unite_legale_avec_plusieurs_nda: string_to_bool(unite_legale['multinda']),
+      unite_legale_avec_plusieurs_nda: unite_legale['multinda'],
       declarations_activites_etablissement:,
       habilitations_france_competence:
     }
@@ -13,19 +13,18 @@ class CarifOref::CertificationsQualiopiFranceCompetences::BuildResource < BuildR
 
   private
 
-  # rubocop:disable Metrics/AbcSize
   def declarations_activites_etablissement
     json_body['nda'].map do |nda_payload|
       {
         numero_de_declaration: nda_payload['nda'],
-        actif: string_to_bool(nda_payload['actif']),
+        actif: nda_payload['actif'],
         date_derniere_declaration: nda_payload['date_derniere_declaration'],
         date_fin_exercice: nda_payload['fin_exercice'],
         certification_qualiopi: {
-          action_formation: string_to_bool(nda_payload['qualiopi_action_formation']),
-          bilan_competences: string_to_bool(nda_payload['qualiopi_bilan_comptence']),
-          validation_acquis_experience: string_to_bool(nda_payload['qualiopi_vae']),
-          apprentissage: string_to_bool(nda_payload['qualiopi_apprentissage']),
+          action_formation: nda_payload['qualiopi_action_formation'],
+          bilan_competences: nda_payload['qualiopi_bilan_comptence'],
+          validation_acquis_experience: nda_payload['qualiopi_vae'],
+          apprentissage: nda_payload['qualiopi_apprentissage'],
           obtention_via_unite_legale: obtention_via_unite_legale(nda_payload['obtention_qualiopi'])
         },
         specialites: {
@@ -36,24 +35,21 @@ class CarifOref::CertificationsQualiopiFranceCompetences::BuildResource < BuildR
       }
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
-  # rubocop:disable Metrics/AbcSize
   def habilitations_france_competence
     valid_habilitations.map do |habilitation_payload|
       {
         code: habilitation_payload['code_fc'],
-        actif: string_to_bool(habilitation_payload['actif']),
+        actif: habilitation_payload['actif'],
         date_actif: format_date(habilitation_payload['date_habilitation']),
         date_fin_enregistrement: format_date(habilitation_payload['date_fin_enregistrement']),
         date_decision: format_date(habilitation_payload['date_decision']),
-        habilitation_pour_former: string_to_bool(habilitation_payload['a_former']),
-        habilitation_pour_organiser_l_evaluation: string_to_bool(habilitation_payload['a_organiser']),
+        habilitation_pour_former: habilitation_payload['a_former'],
+        habilitation_pour_organiser_l_evaluation: habilitation_payload['a_organiser'],
         sirets_organismes_certificateurs: habilitation_payload['certificateur'].pluck('siret')
       }
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
   def valid_habilitations
     json_body['habilitations'].reject do |habilitation_payload|
@@ -84,13 +80,6 @@ class CarifOref::CertificationsQualiopiFranceCompetences::BuildResource < BuildR
 
   def unite_legale
     json_body['unite_legale']
-  end
-
-  def string_to_bool(string)
-    case string
-    when 'True', 'true' then true
-    when 'False', 'false' then false
-    end
   end
 
   def format_date(date)
