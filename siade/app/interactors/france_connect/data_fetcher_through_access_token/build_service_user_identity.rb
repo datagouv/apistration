8309a8d6e542ef36f1ba5a5061b0dbc6d2b6d418
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-05-02 11:22:43 +0200

    Fix error on empty FranceConnect preferred_username
    FranceConnect does not return a "preferred_username" key when the data
    is empty.
    This commit fixes an issue occurring in this case as we considered the
    key would always be there: we are using the response body to instanciate
    a Resource object which gives access to the data via "method_missing".
    When a key is not present it raises as expected.
    
    The workaround in more details: we normalize the response body by
    setting the "preferred_username" key to nil when it's not present.
    Effectively making the Resource instance work as expected.

--- siade/app/interactors/france_connect/data_fetcher_through_access_token/build_service_user_identity.rb
+++ siade/app/interactors/france_connect/data_fetcher_through_access_token/build_service_user_identity.rb
@@ -1,5 +1,19 @@
 class FranceConnect::DataFetcherThroughAccessToken::BuildServiceUserIdentity < FranceConnect::BuildDataFromAccessTokenInteractor
   def call
-    context.service_user_identity = Resource.new(json_body['identity'])
+    context.service_user_identity = Resource.new(normalized_identity)
+  end
+
+  private
+
+  def origin_identity
+    json_body['identity']
+  end
+
+  def normalized_identity
+    origin_identity.tap do |identity|
+      next if identity.key?('preferred_username')
+
+      identity['preferred_username'] = nil
+    end
   end
 end
--- siade/spec/interactors/france_connect/data_fetcher_through_access_token/build_service_user_identity_spec.rb
+++ siade/spec/interactors/france_connect/data_fetcher_through_access_token/build_service_user_identity_spec.rb
@@ -11,8 +11,24 @@ RSpec.describe FranceConnect::DataFetcherThroughAccessToken::BuildServiceUserIde
 
     its(:service_user_identity) { is_expected.to be_a(Resource) }
 
-    it 'associates identite attributes to resource' do
-      expect(call.service_user_identity.to_h).to eq(payload[:identity])
+    context 'when france connect returns a preferred username' do
+      it 'associates identite attributes to resource' do
+        expect(call.service_user_identity.to_h).to eq(payload[:identity])
+      end
+    end
+
+    context 'when france connect does not return a preferred username' do
+      let(:payload) do
+        {
+          scope: nil,
+          identity: default_france_connect_identity_attributes.except(:preferred_username),
+          client: france_connect_client_attributes
+        }
+      end
+
+      it 'sets preferred_username to nil' do
+        expect(call.service_user_identity.preferred_username).to be_nil
+      end
     end
   end
 end
