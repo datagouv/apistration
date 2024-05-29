054f22941d10150921f3ba0bf9d0a468fe50a8cd
Samuelfaure <samuel.faure.dev@gmail.com>
2024-05-29 14:02:54 +0900

    corrections scopes APIs Prestation

--- siade/app/serializers/api_particulier/cnav/revenu_solidarite_active/v2.rb
+++ siade/app/serializers/api_particulier/cnav/revenu_solidarite_active/v2.rb
@@ -1,10 +1,11 @@
 class APIParticulier::CNAV::RevenuSolidariteActive::V2 < APIParticulier::V2BaseSerializer
   %i[
     status
-    majoration
     dateDebut
     dateFin
   ].each do |resource_attribute|
     attribute resource_attribute, if: -> { scope?(:revenu_solidarite_active) }
   end
+
+  attribute :majoration, if: -> { scope?(:revenu_solidarite_active_majoration) }
 end
