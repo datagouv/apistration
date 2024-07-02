32c8fb205713b2fcc1de1c81186b07745fa30288
Samuelfaure <samuel.faure.dev@gmail.com>
2024-07-02 17:12:42 +0900

    Acceptance test for credential files

--- siade/spec/acceptances/credentials_spec.rb
+++ siade/spec/acceptances/credentials_spec.rb
@@ -1,21 +1,33 @@
-RSpec.describe Siade, '.credentials' do
-  subject(:credential) { described_class.credentials[key] }
+RSpec.describe Siade, 'credentials' do
+  describe 'credential files' do
+    let(:credential_files) { [Dir['config/credentials/*.yml'], 'config/credentials.yml.enc'].flatten }
 
-  context 'when key exists' do
-    let(:key) { :jwt_hash_secret }
-
-    it 'renders value' do
-      expect(credential).to eq('this_is_a_valid_jwt_hash_for_spec')
+    it 'has exactly one line' do
+      credential_files.each do |file|
+        expect(File.readlines(file).size).to eq(1)
+      end
     end
   end
 
-  context 'when key does not exist' do
-    let(:key) { :lolilol }
+  describe '.credentials' do
+    subject(:credential) { described_class.credentials[key] }
 
-    it 'raises KeyError error' do
-      expect {
-        credential
-      }.to raise_error(KeyError)
+    context 'when key exists' do
+      let(:key) { :jwt_hash_secret }
+
+      it 'renders value' do
+        expect(credential).to eq('this_is_a_valid_jwt_hash_for_spec')
+      end
+    end
+
+    context 'when key does not exist' do
+      let(:key) { :lolilol }
+
+      it 'raises KeyError error' do
+        expect {
+          credential
+        }.to raise_error(KeyError)
+      end
     end
   end
 end
