a17663047a971ea0b7f694c0fc566ff633efee47
Samuelfaure <samuel.faure.dev@gmail.com>
2024-07-05 15:00:13 +0900

    Actes & Bilans Download: Serializers

---
+++ siade/app/serializers/api_entreprise/inpi/rne/actes_download_serializer/v3.rb
@@ -0,0 +1,2 @@
+class APIEntreprise::INPI::RNE::ActesDownloadSerializer::V3 < APIEntreprise::V3AndMore::DocumentSerializer
+end
---
+++ siade/app/serializers/api_entreprise/inpi/rne/bilans_download_serializer/v3.rb
@@ -0,0 +1,2 @@
+class APIEntreprise::INPI::RNE::BilansDownloadSerializer::V3 < APIEntreprise::V3AndMore::DocumentSerializer
+end
