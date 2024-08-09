8f25fe68e72f21326ad88922984d0043b77d866f
Samuelfaure <samuel.faure.dev@gmail.com>
2024-08-09 13:02:00 +0700

    Fix flaky test
    Same object was used in both tests, leading to fails.
    
    Succeeded with seed 55174
    Failed with seed 41330

--- siade/spec/organizers/upload_document_organizer_spec.rb
+++ siade/spec/organizers/upload_document_organizer_spec.rb
@@ -1,34 +1,47 @@
 RSpec.describe UploadDocumentOrganizer, type: :organizer do
-  before(:all) do
-    class DummyUploadDocumentOrganizer < UploadDocumentOrganizer
-      def source_file_content
-        'dummy_source_file'
-      end
-
-      def file_type
-        'dummy_file_type'
-      end
-
-      def filename
-        'dummy_filename'
-      end
-    end
-  end
-
   describe '.call' do
-    subject { DummyUploadDocumentOrganizer.call }
-
-    its(:content) { is_expected.to eq 'dummy_source_file' }
-    its(:file_type) { is_expected.to eq 'dummy_file_type' }
-    its(:filename) { is_expected.to eq 'dummy_filename' }
-
     context 'when not specifying expiration time' do
+      subject { DummyUploadDocumentOrganizer1.call }
+
+      before do
+        class DummyUploadDocumentOrganizer1 < UploadDocumentOrganizer
+          def source_file_content
+            'dummy_source_file'
+          end
+
+          def file_type
+            'dummy_file_type'
+          end
+
+          def filename
+            'dummy_filename'
+          end
+        end
+      end
+
+      its(:content) { is_expected.to eq 'dummy_source_file' }
+      its(:file_type) { is_expected.to eq 'dummy_file_type' }
+      its(:filename) { is_expected.to eq 'dummy_filename' }
       its(:expires_in) { is_expected.to eq 1.day.to_i }
     end
 
     context 'when specifying a different expiration time' do
+      subject { DummyUploadDocumentOrganizer2.call }
+
       before do
-        DummyUploadDocumentOrganizer.class_eval do
+        class DummyUploadDocumentOrganizer2 < UploadDocumentOrganizer
+          def source_file_content
+            'dummy_source_file'
+          end
+
+          def file_type
+            'dummy_file_type'
+          end
+
+          def filename
+            'dummy_filename'
+          end
+
           def expires_in
             1.hour.to_i
           end
