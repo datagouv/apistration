50cb5ba629a478b42fa0038ba1ef76d943e31a72
Samuelfaure <samuel.faure.dev@gmail.com>
2024-08-14 13:36:47 +0700

    actes / bilans return proxied file url

--- siade/app/serializers/api_entreprise/inpi/rne/bilans_download_serializer/v3.rb
+++ siade/app/serializers/api_entreprise/inpi/rne/bilans_download_serializer/v3.rb
@@ -1,2 +1,5 @@
 class APIEntreprise::INPI::RNE::BilansDownloadSerializer::V3 < APIEntreprise::V3AndMore::DocumentSerializer
+  attribute :document_url do |object|
+    url_for_proxied_file(object.document_url)
+  end
 end
