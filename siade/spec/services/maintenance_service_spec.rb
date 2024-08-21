27594b5030f86bcba7d26476a48e7c44d700e2b1
Samuelfaure <samuel.faure.dev@gmail.com>
2024-08-21 19:24:10 +0700

    Fix flaky tests
    In local, seed 4805 made CNAV and GIPMDS tests fails.
    Was missing some Timecop.return which was messing with
    time.

--- siade/spec/services/maintenance_service_spec.rb
+++ siade/spec/services/maintenance_service_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe MaintenanceService, type: :service do
   let(:instance) { described_class.new(provider) }
   let(:beginning_of_day) { Time.zone.now.beginning_of_day }
 
+  after do
+    Timecop.return
+  end
+
   describe 'on?' do
     subject { instance.on? }
 
@@ -13,10 +17,6 @@ RSpec.describe MaintenanceService, type: :service do
 
     context 'with valid provider which has a maintenance window within the same day' do
       let(:provider) { 'provider_which_has_a_window_within_same_day' }
-
-      after do
-        Timecop.return
-      end
 
       context 'when it is not a maintenance window' do
         before do
@@ -39,10 +39,6 @@ RSpec.describe MaintenanceService, type: :service do
       let(:provider) { 'provider_which_has_a_window_across_two_day' }
 
       let(:beginning_of_day) { Time.zone.now.beginning_of_day }
-
-      after do
-        Timecop.return
-      end
 
       context 'when it is not a maintenance window' do
         before do
@@ -132,10 +128,6 @@ RSpec.describe MaintenanceService, type: :service do
     context 'with valid provider which has a maintenance window within the same day (between 1 am and 2 am)' do
       let(:provider) { 'provider_which_has_a_window_within_same_day' }
 
-      after do
-        Timecop.return
-      end
-
       context 'when it is not a maintenance window' do
         before do
           Timecop.freeze(beginning_of_day + 12.hours)
@@ -157,10 +149,6 @@ RSpec.describe MaintenanceService, type: :service do
       let(:provider) { 'provider_which_has_a_window_across_two_day' }
 
       let(:beginning_of_day) { Time.zone.now.beginning_of_day }
-
-      after do
-        Timecop.return
-      end
 
       context 'when it is not a maintenance window' do
         before do
