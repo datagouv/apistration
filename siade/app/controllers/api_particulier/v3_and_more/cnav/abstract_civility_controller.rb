d489cda54d3454fdadaa70e2b0dc4498e8c32234
Thomas COMES <thomas.comes.tech@gmail.com>
2024-08-29 09:53:43 +0200

    Introduce APIParticulier::V3AndMore::CNAV::AbstractCivilityController
    Also refactor prior existing cnav controller in v3

---
+++ siade/app/controllers/api_particulier/v3_and_more/cnav/abstract_civility_controller.rb
@@ -0,0 +1,36 @@
+class APIParticulier::V3AndMore::CNAV::AbstractCivilityController < APIParticulier::V3AndMore::BaseController
+  include APIParticulier::CivilityParameters
+
+  def show
+    organizer = retrieve_payload_data(organizer_class)
+
+    if organizer.success?
+      render json: serialize_data(organizer),
+        status: extract_http_code(organizer)
+    else
+      render_errors(organizer)
+    end
+  end
+
+  protected
+
+  def transcogage?
+    true
+  end
+
+  private
+
+  def organizer_params
+    civility_parameters(requireds: %i[
+      nomNaissance
+      codePaysLieuDeNaissance
+      sexeEtatCivil
+      codeCogInseeCommuneDeNaissance
+    ])
+      .merge({ recipient: current_user.siret, request_id: request.request_id })
+  end
+
+  def organizer_class
+    raise NotImplementedError
+  end
+end
--- siade/app/controllers/api_particulier/v3_and_more/cnav/revenu_solidarite_active_with_civility_controller.rb
+++ siade/app/controllers/api_particulier/v3_and_more/cnav/revenu_solidarite_active_with_civility_controller.rb
@@ -1,33 +1,6 @@
-class APIParticulier::V3AndMore::CNAV::RevenuSolidariteActiveWithCivilityController < APIParticulier::V3AndMore::BaseController
-  include APIParticulier::CivilityParameters
-
-  def show
-    organizer = retrieve_payload_data(CNAV::RevenuSolidariteActive)
-
-    if organizer.success?
-      render json: serialize_data(organizer),
-        status: extract_http_code(organizer)
-    else
-      render_errors(organizer)
-    end
-  end
-
-  protected
-
-  def transcogage?
-    true
-  end
-
-  private
-
-  def organizer_params
-    civility_parameters(requireds: %i[
-      nomNaissance
-      codePaysLieuDeNaissance
-      sexeEtatCivil
-      codeCogInseeCommuneDeNaissance
-    ])
-      .merge({ recipient: current_user.siret, request_id: request.request_id })
+class APIParticulier::V3AndMore::CNAV::RevenuSolidariteActiveWithCivilityController < APIParticulier::V3AndMore::CNAV::AbstractCivilityController
+  def organizer_class
+    CNAV::RevenuSolidariteActive
   end
 
   def serializer_module
