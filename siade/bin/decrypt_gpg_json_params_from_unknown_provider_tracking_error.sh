fb5fee302ee3ca8b4db1e11eb2d787fcc4b30777
Loïc Delmaire <loic.delmaire@gmail.com>
2024-10-22 11:51:25 +0100

    Enhance bin/decrypt_gpg_json_params_from_unknown_provider_tracking_error.sh

--- siade/bin/decrypt_gpg_json_params_from_unknown_provider_tracking_error.sh
+++ siade/bin/decrypt_gpg_json_params_from_unknown_provider_tracking_error.sh
@@ -3,13 +3,14 @@
 gpg --list-secret-keys | grep equipe@entreprise.api.gouv.fr &> /dev/null
 
 if [ $? -ne 0 ]; then
-  echo -n "You need to import the GPG secret key first. Check ansible repository for file private.asc (encrypted)\nRun the following command from ansible repo:\n> ansible-vault view roles/siade/files/private.asc | gpg --import\n"
+  echo -ne "You need to import the GPG secret key first. Check ansible repository for file private.asc (encrypted)\nRun the following command from ansible repo:\n> ansible-vault view roles/siade/files/private.asc | gpg --import\n"
   exit 1
 fi
 
 file=$1
 
 if [ -f "$file" ] ; then
+  echo -ne "Notes:\n1. File should begins with '-----BEGIN PGP MESSAGE-----'\n2. You need to get the passphrase from the ansible repository\n   > ansible-vault view secrets/siade_gpg_infos.yml\n\n--------------------\n"
   decrypted_string=$(gpg --decrypt $file 2> /dev/null)
 
   echo $decrypted_string | python -m json.tool
