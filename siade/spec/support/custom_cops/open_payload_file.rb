45606e399643ef422ddd777d882a47f946f6bb7c
Samuelfaure <samuel.faure.dev@gmail.com>
2024-10-24 12:31:17 +0100

    migrated to new rubocop format

--- siade/spec/support/custom_cops/open_payload_file.rb
+++ siade/spec/support/custom_cops/open_payload_file.rb
@@ -2,7 +2,7 @@ require 'rubocop'
 
 # rubocop:disable Style/ClassAndModuleChildren
 module RuboCop::Cop::CustomCops
-  class OpenPayloadFile < RuboCop::Cop::Cop
+  class OpenPayloadFile < RuboCop::Cop::Base
     MSG = 'Avoid using File.open, File.read, or Rails.root.join with "payloads" in the parameter. Use "read_payload_file" or "open_payload_file" instead'.freeze
 
     def on_send(node)
@@ -11,7 +11,7 @@ module RuboCop::Cop::CustomCops
       arg = node.arguments.first
       return unless arg&.str_type? && arg.str_content.include?('/payloads/')
 
-      add_offense(node, location: :expression, message: MSG)
+      add_offense(node.loc.selector, message: MSG)
     end
   end
 end
