f1822ea4db83de4f0f06b752c727f9240a1c146b
Thomas COMES <thomas.comes.tech@gmail.com>
2024-10-28 15:12:12 +0100

    Reworked Cnous Serializer spec
    This is to prevent further regressions

--- siade/spec/serializers/api_particulier/cnous/etudiant_boursier/v3_spec.rb
+++ siade/spec/serializers/api_particulier/cnous/etudiant_boursier/v3_spec.rb
@@ -14,20 +14,178 @@ RSpec.describe APIParticulier::CNOUS::EtudiantBoursier::V3, type: :serializer do
   context 'with all cnous scopes' do
     let(:scopes) { all_cnous_scopes }
 
-    it 'has key email' do
+    it 'has all keys' do
+      expect(subject[:data]).to have_key(:nom)
+      expect(subject[:data]).to have_key(:prenom)
+      expect(subject[:data]).to have_key(:prenom2)
+      expect(subject[:data]).to have_key(:date_naissance)
+      expect(subject[:data]).to have_key(:lieu_naissance)
+      expect(subject[:data]).to have_key(:sexe)
       expect(subject[:data]).to have_key(:email)
+      expect(subject[:data]).to have_key(:boursier)
+      expect(subject[:data]).to have_key(:echelon_bourse)
+      expect(subject[:data]).to have_key(:statut)
+      expect(subject[:data]).to have_key(:statut_libelle)
+      expect(subject[:data]).to have_key(:date_de_rentree)
+      expect(subject[:data]).to have_key(:duree_versement)
+      expect(subject[:data]).to have_key(:ville_etudes)
+      expect(subject[:data]).to have_key(:etablissement)
     end
   end
 
-  context 'with just email scope' do
-    let(:scopes) { 'cnous_email' }
+  context 'with partial cnous scopes' do
+    context 'with cnous_identite scope' do
+      let(:scopes) { %w[cnous_identite] }
 
-    it 'has key email' do
-      expect(subject[:data]).to have_key(:email)
+      it 'has the cnous_identite keys' do
+        expect(subject[:data]).to have_key(:nom)
+        expect(subject[:data]).to have_key(:prenom)
+        expect(subject[:data]).to have_key(:prenom2)
+        expect(subject[:data]).to have_key(:date_naissance)
+        expect(subject[:data]).to have_key(:lieu_naissance)
+        expect(subject[:data]).to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
     end
 
-    it 'doesnt have key last_name' do
-      expect(subject[:data]).not_to have_key(:last_name)
+    context 'with cnous_email scope' do
+      let(:scopes) { %w[cnous_email] }
+
+      it 'has the cnous_email keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_statut_boursier scope' do
+      let(:scopes) { %w[cnous_statut_boursier] }
+
+      it 'has the cnous_statut_boursier keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_echelon_bourse scope' do
+      let(:scopes) { %w[cnous_echelon_bourse] }
+
+      it 'has the cnous_echelon_bourse keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_statut_bourse scope' do
+      let(:scopes) { %w[cnous_statut_bourse] }
+
+      it 'has the cnous_statut_bourse keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).to have_key(:statut)
+        expect(subject[:data]).to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_periode_versement scope' do
+      let(:scopes) { %w[cnous_periode_versement] }
+
+      it 'has the cnous_periode_versement keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).to have_key(:date_de_rentree)
+        expect(subject[:data]).to have_key(:duree_versement)
+        expect(subject[:data]).not_to have_key(:ville_etudes)
+        expect(subject[:data]).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_ville_etudes scope' do
+      let(:scopes) { %w[cnous_ville_etudes] }
+
+      it 'has the cnous_ville_etudes keys' do
+        expect(subject[:data]).not_to have_key(:nom)
+        expect(subject[:data]).not_to have_key(:prenom)
+        expect(subject[:data]).not_to have_key(:prenom2)
+        expect(subject[:data]).not_to have_key(:date_naissance)
+        expect(subject[:data]).not_to have_key(:lieu_naissance)
+        expect(subject[:data]).not_to have_key(:sexe)
+        expect(subject[:data]).not_to have_key(:email)
+        expect(subject[:data]).not_to have_key(:boursier)
+        expect(subject[:data]).not_to have_key(:echelon_bourse)
+        expect(subject[:data]).not_to have_key(:statut)
+        expect(subject[:data]).not_to have_key(:statut_libelle)
+        expect(subject[:data]).not_to have_key(:date_de_rentree)
+        expect(subject[:data]).not_to have_key(:duree_versement)
+        expect(subject[:data]).to have_key(:ville_etudes)
+        expect(subject[:data]).to have_key(:etablissement)
+      end
     end
   end
 end
--- siade/spec/serializers/api_particulier/cnous/student_scholarship/v2_spec.rb
+++ siade/spec/serializers/api_particulier/cnous/student_scholarship/v2_spec.rb
@@ -12,20 +12,178 @@ RSpec.describe APIParticulier::CNOUS::StudentScholarship::V2, type: :serializer 
   context 'with all cnous scopes' do
     let(:scopes) { all_cnous_scopes }
 
-    it 'has key email' do
+    it 'has all keys' do
+      expect(serialized_resource).to have_key(:nom)
+      expect(serialized_resource).to have_key(:prenom)
+      expect(serialized_resource).to have_key(:prenom2)
+      expect(serialized_resource).to have_key(:dateNaissance)
+      expect(serialized_resource).to have_key(:lieuNaissance)
+      expect(serialized_resource).to have_key(:sexe)
       expect(serialized_resource).to have_key(:email)
+      expect(serialized_resource).to have_key(:boursier)
+      expect(serialized_resource).to have_key(:echelonBourse)
+      expect(serialized_resource).to have_key(:statut)
+      expect(serialized_resource).to have_key(:statutLibelle)
+      expect(serialized_resource).to have_key(:dateDeRentree)
+      expect(serialized_resource).to have_key(:dureeVersement)
+      expect(serialized_resource).to have_key(:villeEtudes)
+      expect(serialized_resource).to have_key(:etablissement)
     end
   end
 
-  context 'with just email scope' do
-    let(:scopes) { 'cnous_email' }
+  context 'with partial cnous scopes' do
+    context 'with cnous_identite scope' do
+      let(:scopes) { %w[cnous_identite] }
 
-    it 'has key email' do
-      expect(serialized_resource).to have_key(:email)
+      it 'has the cnous_identite keys' do
+        expect(serialized_resource).to have_key(:nom)
+        expect(serialized_resource).to have_key(:prenom)
+        expect(serialized_resource).to have_key(:prenom2)
+        expect(serialized_resource).to have_key(:dateNaissance)
+        expect(serialized_resource).to have_key(:lieuNaissance)
+        expect(serialized_resource).to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
     end
 
-    it 'doesnt have key lastName' do
-      expect(serialized_resource).not_to have_key(:lastName)
+    context 'with cnous_email scope' do
+      let(:scopes) { %w[cnous_email] }
+
+      it 'has the cnous_email keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_statut_boursier scope' do
+      let(:scopes) { %w[cnous_statut_boursier] }
+
+      it 'has the cnous_statut_boursier keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_echelon_bourse scope' do
+      let(:scopes) { %w[cnous_echelon_bourse] }
+
+      it 'has the cnous_echelon_bourse keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_statut_bourse scope' do
+      let(:scopes) { %w[cnous_statut_bourse] }
+
+      it 'has the cnous_statut_bourse keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).to have_key(:statut)
+        expect(serialized_resource).to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_periode_versement scope' do
+      let(:scopes) { %w[cnous_periode_versement] }
+
+      it 'has the cnous_periode_versement keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).to have_key(:dateDeRentree)
+        expect(serialized_resource).to have_key(:dureeVersement)
+        expect(serialized_resource).not_to have_key(:villeEtudes)
+        expect(serialized_resource).not_to have_key(:etablissement)
+      end
+    end
+
+    context 'with cnous_ville_etudes scope' do
+      let(:scopes) { %w[cnous_ville_etudes] }
+
+      it 'has the cnous_ville_etudes keys' do
+        expect(serialized_resource).not_to have_key(:nom)
+        expect(serialized_resource).not_to have_key(:prenom)
+        expect(serialized_resource).not_to have_key(:prenom2)
+        expect(serialized_resource).not_to have_key(:dateNaissance)
+        expect(serialized_resource).not_to have_key(:lieuNaissance)
+        expect(serialized_resource).not_to have_key(:sexe)
+        expect(serialized_resource).not_to have_key(:email)
+        expect(serialized_resource).not_to have_key(:boursier)
+        expect(serialized_resource).not_to have_key(:echelonBourse)
+        expect(serialized_resource).not_to have_key(:statut)
+        expect(serialized_resource).not_to have_key(:statutLibelle)
+        expect(serialized_resource).not_to have_key(:dateDeRentree)
+        expect(serialized_resource).not_to have_key(:dureeVersement)
+        expect(serialized_resource).to have_key(:villeEtudes)
+        expect(serialized_resource).to have_key(:etablissement)
+      end
     end
   end
 end
