7fba6fc55181b8d6ca382ca78756338edba8fc04
Loïc Delmaire <loic.delmaire@gmail.com>
2024-11-14 10:39:49 +0100

    DJEPVA::DocumentUrlHelper#build_valid_document_url handles nil urls
    Alternative to https://github.com/etalab/siade/pull/1483

--- siade/app/interactors/concerns/djepva/document_url_helper.rb
+++ siade/app/interactors/concerns/djepva/document_url_helper.rb
@@ -1,5 +1,7 @@
 module DJEPVA::DocumentUrlHelper
   def build_valid_document_url(url)
+    return if url.blank?
+
     if url.start_with?('http://localhost:8181/services')
       url.gsub('http://localhost:8181/services', "#{Siade.credentials[:mi_domain]}/apim/api-asso")
     else
