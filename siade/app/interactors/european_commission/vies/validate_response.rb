3b51c0a79790d94ac05f08d13218731d9d7be27f
Loïc Delmaire <loic.delmaire@gmail.com>
2025-01-10 12:06:07 +0100

    Harden EuropeanCommission::VIES::ValidateResponse
    Handles IP banned error

--- siade/app/interactors/european_commission/vies/validate_response.rb
+++ siade/app/interactors/european_commission/vies/validate_response.rb
@@ -1,7 +1,16 @@
 class EuropeanCommission::VIES::ValidateResponse < ValidateResponse
   def call
     unknown_provider_response! unless http_ok?
+
     unknown_provider_response! if invalid_json? || valid_tva_number_boolean.nil?
+
+    handle_valid_json
+  end
+
+  private
+
+  def handle_valid_json
+    provider_rate_limiting_error! if ip_banned?
     provider_unavailable! if country_provider_error == 'MS_UNAVAILABLE'
 
     case valid_tva_number_boolean
@@ -15,7 +24,13 @@ class EuropeanCommission::VIES::ValidateResponse < ValidateResponse
     end
   end
 
-  private
+  def provider_rate_limiting_error!
+    fail_with_error!(build_error(ProviderRateLimitingError))
+  end
+
+  def ip_banned?
+    json_body['userError'] == 'IP_BLOCKED'
+  end
 
   def country_provider_error
     json_body['userError']
