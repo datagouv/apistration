61a61974452b6d29652b00bb6a35efcbb2726483
Delmaire Loïc <loic.delmaire@gmail.com>
2025-02-10 12:23:52 +0100

    Harden CanLogRequestsInfoForDebugging
    1. Handles staging
    2. Handles if there is not provider response (can happen for example on
       503)
    
    Closes https://errors.data.gouv.fr/organizations/sentry/issues/249006/

--- siade/app/controllers/concerns/can_log_requests_info_for_debugging.rb
+++ siade/app/controllers/concerns/can_log_requests_info_for_debugging.rb
@@ -5,23 +5,27 @@ module CanLogRequestsInfoForDebugging
     after_action :log_request_info_for_debugging, if: :requests_debugging_enabled?
   end
 
-  # rubocop:disable Metrics/AbcSize
   def log_request_info_for_debugging
     RequestsDebuggerLogger.instance.log(
       controller_name:,
       path: request.path,
       request_params: request.params.except('token', 'controller', 'action'),
       provided_organizer_params:,
-      provider: {
-        header: organizer.response.headers,
-        body: Base64.strict_encode64(organizer.response.body),
-        status: organizer.response.status
-      },
+      provider: provider_payload,
       response_body: JSON.parse(response.body),
       response_status: response.status
     )
   end
-  # rubocop:enable Metrics/AbcSize
+
+  def provider_payload
+    return {} unless organizer.response
+
+    {
+      header: organizer.response.headers,
+      body: Base64.strict_encode64(organizer.response.body),
+      status: organizer.response.status
+    }
+  end
 
   def requests_debugging_enabled?
     RequestsDebuggingService.new(operation_id, response.status).enable?
