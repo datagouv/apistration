724dba8b7036d03a8e8d95b8c2efe08e11760841
Thomas COMES <thomas.comes.tech@gmail.com>
2025-02-10 11:25:05 +0100

    CNAV - Enhanced prenoms validation
    This is to prevent error 400 being sent by our provider

--- siade/app/interactors/cnav/validate_prenoms.rb
+++ siade/app/interactors/cnav/validate_prenoms.rb
@@ -7,6 +7,10 @@ class CNAV::ValidatePrenoms < ValidateParamInteractor
 
   def valid_prenoms?
     param(:prenoms).nil? ||
-      (param(:prenoms).is_a?(Array) && !param(:prenoms).empty?)
+      (param(:prenoms).is_a?(Array) && !param(:prenoms).empty? && valid_string?)
+  end
+
+  def valid_string?
+    param(:prenoms).all? { |p| /\A[a-zA-ZÀ-ÖØ-öø-ÿ' -]+\z/.match?(p) }
   end
 end
--- siade/spec/interactors/cnav/validate_prenoms_spec.rb
+++ siade/spec/interactors/cnav/validate_prenoms_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CNAV::ValidatePrenoms, type: :validate_param_interactor do
   end
 
   context 'when prenoms is an array' do
-    let(:prenoms) { %w[Jean Paul] }
+    let(:prenoms) { %w[Jean Paul Jean-marc Jean-Noël Derc'hen] }
 
     it { is_expected.to be_a_success }
 
@@ -31,6 +31,30 @@ RSpec.describe CNAV::ValidatePrenoms, type: :validate_param_interactor do
 
   context 'when prenoms is not an array' do
     let(:prenoms) { 'jean' }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+  end
+
+  context 'when prenoms is a date' do
+    let(:prenoms) { %w[01/02/2025] }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+  end
+
+  context 'when prenoms is a number' do
+    let(:prenoms) { %w[123] }
+
+    it { is_expected.to be_a_failure }
+
+    its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+  end
+
+  context 'when prenoms contains non alpha characters' do
+    let(:prenoms) { %w[Jean.Paul/!@#$%^&*()[]<>"':;{}] }
 
     it { is_expected.to be_a_failure }
 
