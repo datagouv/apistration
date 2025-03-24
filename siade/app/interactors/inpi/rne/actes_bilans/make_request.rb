b82b2bae2ddb21a1b806a2f4d6742fb3efc8cca4
Thomas COMES <thomas.comes.tech@gmail.com>
2025-03-24 14:43:37 +0100

    Fixing Acte & Bilan RNE mocking params

--- siade/app/interactors/inpi/rne/actes_bilans/make_request.rb
+++ siade/app/interactors/inpi/rne/actes_bilans/make_request.rb
@@ -1,4 +1,10 @@
 class INPI::RNE::ActesBilans::MakeRequest < INPI::RNE::MakeRequest
+  def mocking_params
+    {
+      siren: context.params[:siren]
+    }
+  end
+
   def request_uri
     URI("#{Siade.credentials[:inpi_rne_url]}/api/companies/#{siren}/attachments")
   end
