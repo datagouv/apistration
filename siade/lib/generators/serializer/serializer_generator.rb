ca7d67f13257482e1120bf4cf7e169f5999c769b
Samuelfaure <samuel.faure.dev@gmail.com>
2025-03-28 11:15:30 +0100

    fix: generators for API Particulier have right namespace

--- siade/lib/generators/serializer/serializer_generator.rb
+++ siade/lib/generators/serializer/serializer_generator.rb
@@ -5,10 +5,15 @@ class SerializerGenerator < BaseGenerator
     type: :boolean,
     default: false,
     desc: 'Add support of documents uploader'
+    
+  class_option :api_kind,
+    type: :string,
+    default: 'entreprise',
+    desc: 'Which product ?'
 
   desc 'See `bin/rails generate scaffold_resource --help` instead'
   def create_serializer
-    template 'serializer.rb.erb', File.join('app/serializers/api_entreprise/', provider_namespace.underscore, "#{resource_class.underscore}_serializer", 'v3.rb')
+    template 'serializer.rb.erb', File.join("app/serializers/api_#{api}/", provider_namespace.underscore, "#{resource_class.underscore}_serializer", 'v3.rb')
   end
 
   private
