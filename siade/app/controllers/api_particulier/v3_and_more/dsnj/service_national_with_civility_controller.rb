f898af9e61a840cd770933bd0a377f05f4183d65
Samuelfaure <samuel.faure.dev@gmail.com>
2025-04-18 15:37:02 +0200

    feat: DSNJ::ServiceNational conditional birthplace
    do not send birthplace unless birthcountry is france

--- siade/app/controllers/api_particulier/v3_and_more/dsnj/service_national_with_civility_controller.rb
+++ siade/app/controllers/api_particulier/v3_and_more/dsnj/service_national_with_civility_controller.rb
@@ -19,7 +19,8 @@ class APIParticulier::V3AndMore::DSNJ::ServiceNationalWithCivilityController < A
   def organizer_params
     civility_parameters.merge(
       prenoms: civility_parameters[:prenoms]&.map(&:capitalize),
-      nom_naissance: civility_parameters[:nom_naissance]&.upcase
+      nom_naissance: civility_parameters[:nom_naissance]&.upcase,
+      code_cog_insee_commune_naissance:
     )
   end
 
@@ -29,5 +30,11 @@ class APIParticulier::V3AndMore::DSNJ::ServiceNationalWithCivilityController < A
 
   def organizer
     @organizer ||= retrieve_payload_data(::DSNJ::ServiceNational)
+  end
+
+  def code_cog_insee_commune_naissance
+    return nil if params[:code_cog_insee_pays_naissance].to_s != '99100'
+
+    params[:code_cog_insee_commune_naissance]
   end
 end
