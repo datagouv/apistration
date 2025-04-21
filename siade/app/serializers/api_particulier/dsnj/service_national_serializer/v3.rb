3d188f2f83ce16e8f3f156a31d7126fb7ee479ad
Samuelfaure <samuel.faure.dev@gmail.com>
2025-04-21 15:10:44 +0200

    Feat: DSNJ::ServiceNational - Serializer

--- siade/app/serializers/api_particulier/dsnj/service_national_serializer/v3.rb
+++ siade/app/serializers/api_particulier/dsnj/service_national_serializer/v3.rb
@@ -1,4 +1,4 @@
 class APIParticulier::DSNJ::ServiceNationalSerializer::V3 < APIParticulier::V3AndMore::BaseSerializer
-  attribute :en_regle, if: -> { scope?(:dsnj_service_national) }
-  attribute :commentaires, if: -> { scope?(:dsnj_service_national) }
+  attribute :statut_service_national, if: -> { scope?(:dsnj_statut_service_national) }
+  attribute :commentaires, if: -> { scope?(:dsnj_statut_service_national) }
 end
