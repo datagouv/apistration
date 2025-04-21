27969959edf7750444a878bcefc7f19bd048b641
Samuelfaure <samuel.faure.dev@gmail.com>
2025-04-21 16:05:13 +0200

    Feat: DSNJ::ServiceNational - Organizer
    Feat: deporting code cog logic from DSNJ controller
    
    to organizer

--- siade/app/organizers/dsnj/service_national.rb
+++ siade/app/organizers/dsnj/service_national.rb
@@ -1,8 +1,19 @@
 class DSNJ::ServiceNational < RetrieverOrganizer
   organize DSNJ::ServiceNational::ValidateParams,
-    MockedInteractor
+    DSNJ::ServiceNational::MakeRequest,
+    DSNJ::ServiceNational::ValidateResponse,
+    DSNJ::ServiceNational::BuildResource
+
+  def call
+    remove_commune_naissance_if_foreign_country
+    super
+  end
 
   def provider_name
     'DSNJ'
+  end
+
+  def remove_commune_naissance_if_foreign_country
+    context.params[:code_cog_insee_commune_naissance] = nil if context.params[:code_cog_insee_pays_naissance].to_s != '99100'
   end
 end
--- siade/spec/organizers/dsnj/service_national_spec.rb
+++ siade/spec/organizers/dsnj/service_national_spec.rb
@@ -16,14 +16,40 @@ RSpec.describe DSNJ::ServiceNational, type: :retriever_organizer do
 
   let(:nom_naissance) { 'Sekiro' }
   let(:prenoms) { %w[Shinobi Wolf] }
-  let(:annee_date_naissance) { 1988 }
+  let(:annee_date_naissance) { 2008 }
   let(:mois_date_naissance) { 3 }
   let(:jour_date_naissance) { 22 }
   let(:code_cog_insee_commune_naissance) { '92036' }
   let(:code_cog_insee_pays_naissance) { '99100' }
   let(:sexe_etat_civil) { 'M' }
 
-  describe 'valid params' do
-    pending 'Implement endpoint'
+  before do
+    stub_dsnj_service_national_valid
+  end
+
+  describe 'happy path' do
+    it { is_expected.to be_a_success }
+
+    it 'retrieves the resource' do
+      resource = subject.bundled_data.data
+
+      expect(resource).to be_present
+    end
+  end
+
+  describe 'when code_cog_insee_pays_naissance is not France' do
+    let(:code_cog_insee_pays_naissance) { '11111' }
+
+    it 'removes code_cog_insee_commune_naissance' do
+      expect(subject.params[:code_cog_insee_commune_naissance]).to be_nil
+    end
+  end
+
+  describe 'when code_cog_insee_pays_naissance is France' do
+    let(:code_cog_insee_pays_naissance) { '99100' }
+
+    it 'does not remove code_cog_insee_commune_naissance' do
+      expect(subject.params[:code_cog_insee_commune_naissance]).to eq(code_cog_insee_commune_naissance)
+    end
   end
 end
