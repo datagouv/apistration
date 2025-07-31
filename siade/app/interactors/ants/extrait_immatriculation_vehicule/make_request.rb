056c9f238586043288f781a9c1b7c7fe87609cb0
Samuelfaure <samuel.faure.dev@gmail.com>
2025-07-31 15:09:17 +0200

    Fix: mocking params for ANTS FC

--- siade/app/interactors/ants/extrait_immatriculation_vehicule/make_request.rb
+++ siade/app/interactors/ants/extrait_immatriculation_vehicule/make_request.rb
@@ -1,19 +1,10 @@
 class ANTS::ExtraitImmatriculationVehicule::MakeRequest < MockedInteractor
   protected
 
-  def mocking_params # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  def mocking_params
     {
-      immatriculation: context.params[:immatriculation].downcase,
-      nomNaissance: context.params[:nom_naissance]&.downcase,
-      nomUsage: context.params[:nom_usage]&.downcase,
-      prenoms: context.params[:prenoms]&.map(&:downcase),
-      anneeDateNaissance: context.params[:annee_date_naissance]&.to_i,
-      moisDateNaissance: context.params[:mois_date_naissance]&.to_i,
-      jourDateNaissance: context.params[:jour_date_naissance]&.to_i,
-      sexeEtatCivil: context.params[:sexe_etat_civil]&.downcase,
-      codeCogInseePaysNaissance: context.params[:code_cog_insee_pays_naissance],
-      codeCogInseeCommuneNaissance: context.params[:code_cog_insee_commune_naissance]
-    }.compact
+      immatriculation: context.params[:immatriculation]
+    }
   end
 
   def request_uri
