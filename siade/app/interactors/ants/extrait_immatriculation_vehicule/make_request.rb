71dfaa7a489d2b0ee9796f73862de4cb2db07c3c
Samuelfaure <samuel.faure.dev@gmail.com>
2025-08-06 14:53:41 +0200

    WIP: MakeRequest

--- siade/app/interactors/ants/extrait_immatriculation_vehicule/make_request.rb
+++ siade/app/interactors/ants/extrait_immatriculation_vehicule/make_request.rb
@@ -1,4 +1,6 @@
-class ANTS::ExtraitImmatriculationVehicule::MakeRequest < MockedInteractor
+class ANTS::ExtraitImmatriculationVehicule::MakeRequest < MakeRequest::Post
+  include UseWildcardSSLCertificate
+
   protected
 
   def mocking_params
@@ -8,11 +10,20 @@ class ANTS::ExtraitImmatriculationVehicule::MakeRequest < MockedInteractor
   end
 
   def request_uri
-    fail NotImplementedError
+    URI(Siade.credentials[:ants_siv_url])
   end
 
   def request_params
-    fail NotImplementedError
+    {}
+  end
+
+  def build_request_body
+    ANTSDossierImmatriculationSoapBuilder.new(
+      immatriculation: context.params[:immatriculation].upcase,
+      request_id: context.params[:request_id],
+      certificate: cert,
+      private_key: key
+    ).render
   end
 
   private
--- siade/spec/interactors/ants/extrait_immatriculation_vehicule/make_request_spec.rb
+++ siade/spec/interactors/ants/extrait_immatriculation_vehicule/make_request_spec.rb
@@ -2,32 +2,40 @@ RSpec.describe ANTS::ExtraitImmatriculationVehicule::MakeRequest, type: :make_re
   describe '.call' do
     subject { described_class.call(params:) }
 
-    let(:params) { {} }
+    let(:params) { { immatriculation:, request_id: } }
+    let(:immatriculation) { 'AA-123-AA' }
+    let(:request_id) { SecureRandom.uuid }
+
     let!(:stubbed_request) do
-      stub_request(post, Siade.credentials[:provider_url]).with(body:)
+      allow(certificate).to receive(:to_der).and_return('dummy_cert_der')
+      allow(private_key).to receive(:sign).and_return('dummy_key_signature')
+      stub_request(:post, Siade.credentials[:ants_siv_url]).with(body:)
     end
 
     let(:body) do
-      request_params.to_json
+      ANTSDossierImmatriculationSoapBuilder.new(
+        immatriculation:,
+        request_id:,
+        certificate:,
+        private_key:
+      ).render
     end
 
-    let(:request_params) do
-      {
-        immatriculation: 'AA-123-AA',
-        given_name: 'Jean',
-        family_name: 'DUPONT',
-        birthdate: '2000-10-01',
-        gender: 'male',
-        birthplace: nil,
-        birthcountry: '12345'
-      }
+    let(:private_key) { instance_double(OpenSSL::PKey::RSA) }
+    let(:certificate) { instance_double(OpenSSL::X509::Certificate) }
+
+    before do
+      allow(File).to receive(:read).and_call_original
+      allow(File).to receive(:read).with(Siade.credentials[:ssl_wildcard_certif_key_path]).and_return('dummy_key')
+      allow(File).to receive(:read).with(Siade.credentials[:ssl_wildcard_certif_crt_path]).and_return('dummy_cert')
+
+      allow(OpenSSL::PKey::RSA).to receive(:new).with('dummy_key').and_return(private_key)
+      allow(OpenSSL::X509::Certificate).to receive(:new).with('dummy_cert').and_return(certificate)
     end
 
-    describe 'Implements endpoint', pending: 'Implement endpoint' do
-      it { is_expected.to be_a_success }
-    end
+    it { is_expected.to be_a_success }
 
-    it 'calls url with correct params', pending: 'Implement endpoint' do
+    it 'calls url with correct params' do
       subject
 
       expect(stubbed_request).to have_been_requested
