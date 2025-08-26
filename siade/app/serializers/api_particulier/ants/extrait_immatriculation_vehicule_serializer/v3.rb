c799c99ab439e324bd9a6844a4ad0da51d29d013
Samuelfaure <samuel.faure.dev@gmail.com>
2025-08-26 10:54:00 +0200

    Fix: ANTS Serializer always sends back identity
    Scope from FranceConnect v2 removes identity scope
    We can mitigate the issue by not conditioning if the scope
    is there; we always want to send back identity so
    in this case it works

--- siade/app/serializers/api_particulier/ants/extrait_immatriculation_vehicule_serializer/v3.rb
+++ siade/app/serializers/api_particulier/ants/extrait_immatriculation_vehicule_serializer/v3.rb
@@ -1,5 +1,5 @@
 class APIParticulier::ANTS::ExtraitImmatriculationVehiculeSerializer::V3 < APIParticulier::V3AndMore::BaseSerializer
-  attribute :identite_particulier, if: -> { scope?(:ants_extrait_immatriculation_vehicule_identite_particulier) }
+  attribute :identite_particulier
   attribute :adresse_particulier, if: -> { scope?(:ants_extrait_immatriculation_vehicule_adresse_particulier) }
   attribute :statut_rattachement, if: -> { scope?(:ants_extrait_immatriculation_vehicule_statut_rattachement) }
   attribute :donnees_immatriculation_vehicule, if: -> { scope?(:ants_extrait_immatriculation_vehicule_donnees_immatriculation_vehicule) }
