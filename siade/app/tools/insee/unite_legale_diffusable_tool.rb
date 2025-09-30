be013860b6fd68b95b7485878e51b20752a3fa0f
Delmaire Loïc <loic.delmaire@gmail.com>
2025-09-30 10:45:49 +0200

    Introduce INSEE::UniteLegaleDiffusableTool

---
+++ siade/app/tools/insee/unite_legale_diffusable_tool.rb
@@ -0,0 +1,20 @@
+class INSEE::UniteLegaleDiffusableTool < ApplicationTool
+  title 'INSEE - Unités légales diffusables'
+  tool_name 'insee/unite_legale_diffusable'
+  description "Permet d'obtenir les informations diffusables issue du répertoire SIRENE de l'INSEE d'une unité légale à partir de son SIREN. Les données diffusables sont celles qui peuvent être librement partagées et utilisées, conformément aux règles de confidentialité et de protection des données."
+
+  input_schema(
+    properties: {
+      siren: { type: 'string', pattern: '^\d{9}$' }
+    },
+    required: %w[siren]
+  )
+
+  def self.organizer_class
+    INSEE::UniteLegaleDiffusable
+  end
+
+  def self.protected_data?
+    false
+  end
+end
