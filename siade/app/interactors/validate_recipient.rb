ef5cb3db0e13cc7bb92952abaff6b956c89fec7d
Delmaire Loïc <loic.delmaire@gmail.com>
2025-10-08 09:23:15 +0200

    API Particulier empty siret investigation part 4

--- siade/app/interactors/validate_recipient.rb
+++ siade/app/interactors/validate_recipient.rb
@@ -13,7 +13,18 @@ class ValidateRecipient < ValidateParamInteractor
 
   def invalid_recipient!
     context.errors << InvalidRecipientError.new
+    track_invalid_recipient
 
     mark_as_error!
+  end
+
+  def track_invalid_recipient
+    MonitoringService.instance.track_with_added_context(
+      'info',
+      'Invalid recipient',
+      {
+        recipient: context.recipient || 'empty recipient'
+      }
+    )
   end
 end
