89efadc65b90fc751de362e92cae0ea3f4c5635d
Samuelfaure <samuel.faure.dev@gmail.com>
2025-10-10 17:17:13 +0200

    Fix: ANTS staging not working

--- siade/app/organizers/ants/extrait_immatriculation_vehicule/validate_identity_matching.rb
+++ siade/app/organizers/ants/extrait_immatriculation_vehicule/validate_identity_matching.rb
@@ -1,4 +1,8 @@
 class ANTS::ExtraitImmatriculationVehicule::ValidateIdentityMatching < ApplicationOrganizer
+  around do |interactor|
+    interactor.call unless clogged_env?
+  end
+
   organize ANTS::ExtraitImmatriculationVehicule::ExtractIdentities,
     ANTS::ExtraitImmatriculationVehicule::ValidateOneIdentityIsMatching
 end
