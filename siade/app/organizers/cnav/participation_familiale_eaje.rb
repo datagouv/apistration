1712e5060f3742bb735c3ab7385d1bdfdd5aff8b
Delmaire Loïc <loic.delmaire@gmail.com>
2025-11-05 16:01:38 +0100

    FIX CNAV::ParticipationFamilialeEAJE mocked interactor

--- siade/app/organizers/cnav/participation_familiale_eaje.rb
+++ siade/app/organizers/cnav/participation_familiale_eaje.rb
@@ -1,6 +1,18 @@
 class CNAV::ParticipationFamilialeEAJE < CNAV::RetrieverOrganizer
+  class MockedInteractor < ::MockedInteractor
+    def mocking_params
+      mocking_params = super.compact
+      mocking_params.delete(:request_id)
+      mocking_params.deep_transform_keys! do |key|
+        key.to_s.camelize(:lower).gsub('INSEE', 'Insee')
+      end
+
+      mocking_params
+    end
+  end
+
   organize CNAV::ValidateParams,
-    MockedInteractor
+    CNAV::ParticipationFamilialeEAJE::MockedInteractor
 
   def provider_name
     'Sécurité sociale'
