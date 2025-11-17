1408f2ada00ca6e5ace68a3500f02e92aa35d998
Samuelfaure <samuel.faure.dev@gmail.com>
2025-11-17 16:03:09 +0100

    Feat: CNAV::EAJE Organizer

--- siade/app/organizers/cnav/participation_familiale_eaje.rb
+++ siade/app/organizers/cnav/participation_familiale_eaje.rb
@@ -1,18 +1,9 @@
 class CNAV::ParticipationFamilialeEAJE < CNAV::RetrieverOrganizer
-  class MockedInteractor < ::MockedInteractor
-    def mocking_params
-      mocking_params = super.compact
-      mocking_params.delete(:request_id)
-      mocking_params.deep_transform_keys! do |key|
-        key.to_s.camelize(:lower).gsub('INSEE', 'Insee')
-      end
-
-      mocking_params
-    end
-  end
-
   organize CNAV::ValidateParams,
-    CNAV::ParticipationFamilialeEAJE::MockedInteractor
+    CNAV::Authenticate,
+    CNAV::MakeRequest,
+    CNAV::ValidateResponse,
+    CNAV::ParticipationFamilialeEAJE::BuildResource
 
   def provider_name
     'Sécurité sociale'
--- siade/spec/organizers/cnav/participation_familiale_eaje_spec.rb
+++ siade/spec/organizers/cnav/participation_familiale_eaje_spec.rb
@@ -1,10 +1,17 @@
-RSpec.describe CNAV::ParticipationFamilialeEAJE, type: :retriever_organizer do
-  subject { described_class.call(params:, operation_id: 'api_particulier_v3_cnav_participation_familiale_eaje_with_civility', recipient:) }
+require 'rails_helper'
 
-  let(:request_id) { SecureRandom.uuid }
-  let(:recipient) { valid_siret }
+RSpec.describe CNAV::ParticipationFamilialeEAJE do
+  subject(:instance) { described_class.call(**params) }
 
   let(:params) do
+    {
+      params: call_params,
+      recipient:
+    }
+  end
+  let(:recipient) { valid_siret }
+
+  let(:call_params) do
     {
       nom_naissance: 'CHAMPION',
       prenoms: ['JEAN-PASCAL'],
@@ -12,31 +19,45 @@ RSpec.describe CNAV::ParticipationFamilialeEAJE, type: :retriever_organizer do
       mois_date_naissance: 6,
       jour_date_naissance: 12,
       sexe_etat_civil: 'M',
-      code_cog_insee_commune_naissance: '75101',
       code_cog_insee_pays_naissance: '99100',
-      request_id:
+      code_cog_insee_commune_naissance: '17300',
+      request_id: SecureRandom.uuid
     }
   end
 
-  describe 'valid params' do
-    before do
-      allow(Rails.env).to receive(:staging?).and_return(true)
-      allow(MockDataBackend).to receive(:get_response_for).and_return(
-        {
-          status: '200',
-          payload: {
-            test: 'lol'
-          }
-        }
+  before do
+    stub_cnav_authenticate('participation_familiale_eaje')
+    stub_cnav_valid('participation_familiale_eaje')
+  end
+
+  it { is_expected.to be_a_success }
+
+  describe 'bundled_data' do
+    subject { instance.bundled_data.data.to_h }
+
+    it 'returns properly structured resource' do
+      expect(subject).to include(
+        allocataires: array_including(
+          hash_including(nom_naissance: 'DUPOND')
+        ),
+        enfants: array_including(
+          hash_including(nom_naissance: 'DUPOND')
+        ),
+        adresse: hash_including(destinataire: 'M. DUPOND Jean-Michel'),
+        parametres_calcul_participation_familiale: hash_including(annee: 2024)
       )
     end
+  end
 
-    it { is_expected.to be_a_success }
+  describe 'provider_name' do
+    it 'returns correct provider name' do
+      expect(instance.provider_name).to eq('Sécurité sociale')
+    end
+  end
 
-    it 'retrieves the resource' do
-      resource = subject.mocked_data[:payload]
-
-      expect(resource).to be_present
+  describe 'dss_prestation_name' do
+    it 'returns correct prestation name' do
+      expect(instance.dss_prestation_name).to eq('participation_familiale_eaje')
     end
   end
 end
