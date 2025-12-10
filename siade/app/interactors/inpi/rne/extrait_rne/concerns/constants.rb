258457ad37fffce8f1ba591f82c18636c7567d1b
Thomas COMES <thomas.comes.tech@gmail.com>
2025-12-10 11:12:54 +0100

    Extrait RNE: cleaning constants

--- siade/app/interactors/inpi/rne/extrait_rne/concerns/constants.rb
+++ siade/app/interactors/inpi/rne/extrait_rne/concerns/constants.rb
@@ -1,31 +1,4 @@
 module INPI::RNE::ExtraitRNE::Concerns::Constants
-  ROLE_MAPPING = {
-    '40' => 'PRESIDENT',
-    '41' => 'DIRECTEUR GENERAL',
-    '53' => 'DIRECTEUR GENERAL',
-    '73' => 'PRESIDENT',
-    '6000' => 'Président',
-    '6001' => 'Président du conseil d\'administration',
-    '6002' => 'Président du conseil de surveillance',
-    '6100' => 'Directeur Général',
-    '6101' => 'Directeur Général Délégué',
-    '6200' => 'Gérant',
-    '6300' => 'Administrateur',
-    '6400' => 'Membre du conseil de surveillance',
-    '30' => 'Commissaire aux comptes titulaire',
-    '31' => 'Commissaire aux comptes suppléant',
-    '67' => 'Associé'
-  }.freeze
-
-  FORME_JURIDIQUE_MAPPING = {
-    '5498' => 'SARL',
-    '5510' => 'Société anonyme à conseil d\'administration',
-    '5710' => 'SAS, société par actions simplifiée',
-    '5720' => 'SASU, société par actions simplifiée unipersonnelle',
-    '6540' => 'EURL',
-    '1000' => 'Entreprise individuelle'
-  }.freeze
-
   STATUT_ETABLISSEMENT = {
     '5' => 'actif',
     '6' => 'fermé'
