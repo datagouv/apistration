94e2b3e36152359e8af2b5de24b15dce6c1979af
Delmaire Loïc <loic.delmaire@gmail.com>
2026-02-04 09:29:18 +0100

    Rate limit Sentry events à 5k/h par fingerprint
    Incident du 03/02: DNS resolution failure sur api.insee.fr a généré
    ~29k events en 2h (17 issues Socket::ResolutionError), saturant
    notre instance Sentry.
    
    Analyse des issues actuelles:
    - 14 issues dépassent 10k events/24h
    - En cas de pic concentré, certaines atteignent 40k+/h
    
    Limite à 5k/h car:
    - Suffisant pour diagnostiquer un incident (contexte conservé)
    - Protège contre les cascades (provider down = spam massif)
    - Chaque fingerprint a son propre compteur (pas d'impact croisé)

--- siade/config/initializers/sentry.rb
+++ siade/config/initializers/sentry.rb
@@ -7,6 +7,12 @@ Sentry.init do |config|
   config.traces_sample_rate = 1.0
 
   config.before_send = lambda do |event, _hint|
+    cache_key = "sentry:#{event.fingerprint&.join(':') || event.exception&.values&.first&.type}"
+
+    if Rails.cache.increment(cache_key, 1, expires_in: 1.hour) > 5_000
+      next nil
+    end
+
     begin
       event.request.headers.delete('X-Api-Key')
     rescue NoMethodError
