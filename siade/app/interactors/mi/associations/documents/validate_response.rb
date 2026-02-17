686d307fbf59ca7943ceb12b40c68d5db6eb1e92
Delmaire Loïc <loic.delmaire@gmail.com>
2026-02-17 14:05:55 +0100

    Fix Sentry#275934: traiter les réponses proxy MI comme indisponibilité
    Le proxy MI renvoie parfois une 404 avec un body HTML
    "No service was found." qui n'est pas une vraie 404 mais une erreur
    temporaire de proxy. Le code crashait (NoMethodError) car
    xml_body_as_hash[:asso] est nil sur ces réponses.
    
    - Utilise dig(:asso, :documents) pour éviter le NoMethodError
    - Lève provider_unavailable! (502) quand le payload n'a pas la
      structure attendue (pas de clé :asso), au lieu de laisser passer
      vers resource_not_found! (404)

--- siade/app/interactors/mi/associations/documents/validate_response.rb
+++ siade/app/interactors/mi/associations/documents/validate_response.rb
@@ -4,6 +4,8 @@ class MI::Associations::Documents::ValidateResponse < MI::Associations::Validate
 
     return if http_ok? && payload_valid? && payload_has_documents?
 
+    provider_unavailable! unless payload_present?
+
     resource_not_found!(:siret_or_rna) if not_found_in_body? || !payload_has_documents?
 
     unknown_provider_response!
@@ -12,7 +14,7 @@ class MI::Associations::Documents::ValidateResponse < MI::Associations::Validate
   private
 
   def payload_has_documents?
-    return false unless xml_body_as_hash[:asso][:documents]
+    return false unless xml_body_as_hash.dig(:asso, :documents)
 
     xml_body_as_hash[:asso][:documents][:nbDocRna].to_i.positive?
   end
--- siade/spec/interactors/mi/associations/documents/validate_response_spec.rb
+++ siade/spec/interactors/mi/associations/documents/validate_response_spec.rb
@@ -96,6 +96,15 @@ RSpec.describe MI::Associations::Documents::ValidateResponse, type: :validate_re
       its(:errors) { is_expected.to include(instance_of(NotFoundError)) }
     end
 
+    context 'with a 404 proxy error (No service was found)' do
+      let(:code) { '404' }
+      let(:body) { '<html><body>No service was found.</body></html>' }
+
+      it { is_expected.to be_a_failure }
+
+      its(:errors) { is_expected.to include(instance_of(ProviderUnavailable)) }
+    end
+
     context 'with a valid code and a body containing nonsense' do
       let(:code) { '200' }
       let(:body) { 'Nonsense' }
