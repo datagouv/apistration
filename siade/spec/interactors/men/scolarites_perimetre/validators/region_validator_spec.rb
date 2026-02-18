112b8145d648799fe566643a911cc6ddcaca452e
Delmaire Loïc <loic.delmaire@gmail.com>
2026-02-18 09:59:15 +0100

    Use BCN academic region codes and clean department allowlists
    Regions: replace INSEE administrative codes with BCN
    N_REGION_ACADEMIQUE codes (01-18), as confirmed by FD.
    The ANGES API expects academic region codes, not INSEE ones.
    
    Departments: remove obsolete/uninhabited codes rejected by the
    ANGES API (020 old Corse, 984 TAAF, 985 old Mayotte, 989 Clipperton).

---
+++ siade/spec/interactors/men/scolarites_perimetre/validators/region_validator_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe MEN::ScolaritesPerimetre::Validators::RegionValidator do
+  describe '.valid?' do
+    subject { described_class.valid?(values) }
+
+    context 'with valid codes' do
+      %w[01 10 18].each do |code|
+        context "when value is #{code}" do
+          let(:values) { [code] }
+
+          it { is_expected.to be true }
+        end
+      end
+    end
+
+    context 'with invalid codes' do
+      {
+        '99' => 'not in BCN list',
+        '1' => 'too short (1 char)',
+        'ABC' => 'letters only',
+        '' => 'empty string'
+      }.each do |code, reason|
+        context "when value is #{code} (#{reason})" do
+          let(:values) { [code] }
+
+          it { is_expected.to be false }
+        end
+      end
+    end
+
+    context 'with mix of valid and invalid' do
+      let(:values) { %w[11 99] }
+
+      it { is_expected.to be false }
+    end
+  end
+end
