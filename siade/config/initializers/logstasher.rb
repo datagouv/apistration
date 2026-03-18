3adbb65de93c7b2b6f3b981696885b2b75701577
Samuelfaure <samuel.faure.dev@gmail.com>
2026-03-18 12:02:57 -0300

    SIADE open sourcing: reintroducing conflict files
    Those files got us rebase conflicts for some reason
    Reintroducing the correct version here

--- siade/config/initializers/logstasher.rb
+++ siade/config/initializers/logstasher.rb
@@ -6,19 +6,7 @@ if LogStasher.enabled?
   end
 
   LogStasher.add_custom_fields_to_request_context do |fields|
-    controller_name = request.params['controller']
-
-    is_api_request = (controller_name =~ /\Aapi\//)
-
-    if is_api_request
-      fields[:api_version] = controller_name.split('/')[1]
-    end
-
-    if retriever_cached?
-      fields[:retriever_cached] = 't'
-    else
-      fields[:retriever_cached] = 'f'
-    end
+    LogStasherFieldsBuilder.new(self, fields).perform
   end
 
   LogStasher.watch('user_access') do |name, start, finish, id, payload, store|
