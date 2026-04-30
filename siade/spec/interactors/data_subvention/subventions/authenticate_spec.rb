4ca9edb30a9abfac2c88fa9e58b6fe59ebf5bca7
Delmaire Loïc <loic.delmaire@gmail.com>
2026-04-30 13:25:06 +0200

    Fix NoMethodError when DataSubvention auth returns JSON without user/jwt
    Fixes:
    - https://errors.data.gouv.fr/organizations/sentry/issues/288903/
    - https://errors.data.gouv.fr/organizations/sentry/issues/288905/
    - https://errors.data.gouv.fr/organizations/sentry/issues/288908/
    - https://errors.data.gouv.fr/organizations/sentry/issues/288909/

--- siade/spec/interactors/data_subvention/subventions/authenticate_spec.rb
+++ siade/spec/interactors/data_subvention/subventions/authenticate_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe DataSubvention::Subventions::Authenticate, type: :interactor do
       expect(subject.token).to be_present
     end
   end
+
+  context 'when provider is down and replies with valid JSON missing the user/jwt structure' do
+    before do
+      stub_request(:post, "#{Siade.credentials[:data_subvention_url]}/auth/login")
+        .to_return(status: 500, body: '{"error":"Internal Server Error"}')
+    end
+
+    it { is_expected.to be_a_failure }
+
+    it 'fails with a ProviderUnknownError' do
+      expect(subject.errors.first).to be_a(ProviderUnknownError)
+    end
+  end
 end
