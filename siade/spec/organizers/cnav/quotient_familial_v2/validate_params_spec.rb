4b309baa9ac3b84cdfff06a80824dd919bc360db
Delmaire Loïc <loic.delmaire@gmail.com>
2026-05-12 17:28:40 +0200

    Make CNAV birth place commune params optional, keep country code mandatory
    The CNAV provider only requires the country COG code as mandatory birth
    place parameter. Relax the validation so commune-related params (COG code
    or transcogage triplet) are strongly recommended but no longer mandatory
    for French-born persons.
    
    Update all 8 endpoint fiches (parameters section, footnotes, FAQ),
    OpenAPI descriptions, tests and changelog.

--- siade/spec/organizers/cnav/quotient_familial_v2/validate_params_spec.rb
+++ siade/spec/organizers/cnav/quotient_familial_v2/validate_params_spec.rb
@@ -101,9 +101,9 @@ RSpec.describe CNAV::QuotientFamilialV2::ValidateParams, type: :validate_params 
       let(:code_cog_insee_commune_naissance) { nil }
       let(:code_cog_insee_pays_naissance) { '99100' }
 
-      it { is_expected.to be_a_failure }
+      it { is_expected.to be_a_success }
 
-      its(:errors) { is_expected.to include(instance_of(UnprocessableEntityError)) }
+      its(:errors) { is_expected.to be_empty }
     end
 
     describe 'with empty params for lieu de naissance and not in france' do
