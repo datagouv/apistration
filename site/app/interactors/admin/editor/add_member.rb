0346782d8e6be5793f86509f28271414dd884731
Delmaire Loïc <loic.delmaire@gmail.com>
2026-05-20 17:51:40 +0200

    Add editor members management (add/remove) via interactors

---
+++ site/app/interactors/admin/editor/add_member.rb
@@ -0,0 +1,20 @@
+class Admin::Editor::AddMember < ApplicationInteractor
+  def call
+    context.user = find_user
+    validate_not_already_member
+    context.user.update!(editor: context.editor)
+  end
+
+  private
+
+  def find_user
+    User.find_by(email: context.email) ||
+      context.fail!(message: "Aucun utilisateur trouvé avec l'email #{context.email}")
+  end
+
+  def validate_not_already_member
+    return unless context.user.editor_id == context.editor.id
+
+    context.fail!(message: "#{context.user.email} est déjà membre de cet éditeur")
+  end
+end
