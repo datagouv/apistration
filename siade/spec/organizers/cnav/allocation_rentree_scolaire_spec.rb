5dfb06c2b55c4fdf531079fb986a61ab963f3523
Samuel-Zacharie FAURE <samuel.faure.dev@gmail.com>
2026-06-01 15:13:50 -0300

    Feat: CNAV::ARS organizer

---
+++ siade/spec/organizers/cnav/allocation_rentree_scolaire_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe CNAV::AllocationRentreeScolaire, type: :retriever_organizer do
+  subject { described_class.call(params:, recipient:) }
+
+  let(:request_id) { SecureRandom.uuid }
+  let(:recipient) { valid_siret }
+
+  describe '.call with civility params' do
+    let(:common_params) do
+      {
+        nom_naissance: 'CHAMPION',
+        prenoms: ['JEAN-PASCAL'],
+        annee_date_naissance: 1980,
+        mois_date_naissance: 6,
+        jour_date_naissance: 12,
+        sexe_etat_civil:,
+        code_cog_insee_pays_naissance: '99100',
+        request_id:,
+        user_id: valid_siret
+      }
+    end
+
+    let(:sexe_etat_civil) { 'M' }
+
+    context 'when it is with code insee lieu de naissance' do
+      let(:params) do
+        common_params.merge(
+          code_cog_insee_commune_naissance: '17300'
+        )
+      end
+
+      describe 'happy path' do
+        before do
+          stub_cnav_authenticate('allocation_rentree_scolaire')
+          stub_cnav_valid('allocation_rentree_scolaire')
+        end
+
+        it { is_expected.to be_a_success }
+
+        it 'retrieves the resource' do
+          resource = subject.bundled_data.data
+
+          expect(resource.status).to eq('allocataire')
+          expect(resource.date_debut_droit).to eq('2024-08-09')
+        end
+      end
+    end
+  end
+end
