ed6f74714324e6cb8194912b46f81691da9609a0
Dorine Lambinet <dorine.lambinet@gmail.com>
2026-06-17 15:31:23 +0200

    Fix le design des cartes cas d'usages simplifions, utilisation du DSFR titre en bleu et icone adaptée

--- site/app/helpers/tag_color_helper.rb
+++ site/app/helpers/tag_color_helper.rb
@@ -1,60 +1,40 @@
 module TagColorHelper
   ADMINISTRATION_COLORS = {
-    # --- Tous ---
-    "Tous les acteurs publics"                                                         => "beige-gris-galet",
-
-    # --- Collectivités territoriales ---
-    "Communes et groupements de communes"                                              => "green-tilleul-verveine",
-    "Départements"                                                                     => "green-tilleul-verveine",
-    "Régions"                                                                          => "green-tilleul-verveine",
-    "Collectivités et territoires d'Outre-mer"                                         => "green-tilleul-verveine",
-    "Syndicats mixtes ouverts"                                                         => "green-tilleul-verveine",
-    "Autres établissements publics rattachés aux collectivités territoriales"           => "green-tilleul-verveine",
-
-    # --- État central ---
-    "Ministères, services centraux et déconcentrés"                                    => "blue-cumulus",
-    "Agences, centres, caisses et offices publics nationaux"                           => "blue-cumulus",
-    "Agences régionale de santé (ARS)"                                                 => "blue-cumulus",
-    "Agences de l'eau"                                                                 => "blue-cumulus",
-    "Parc nationaux"                                                                   => "blue-cumulus",
-    "Autorités administratives ou publiques indépendantes"                             => "blue-cumulus",
-    "Parlement et CESE"                                                                => "blue-cumulus",
-    "Autres établissements publics rattachés à l'État"                                 => "blue-cumulus",
-
-
-    # --- Éducation ---
-    "Établissements scolaires"                                                         => "pink-macaron",
-    "Établissements d'enseignement supérieur"                                          => "pink-macaron",
-    "Caisses des écoles"                                                               => "pink-macaron",
-    "CROUS"                                                                            => "pink-macaron",
-    "Crèches et établissements d'accueil du jeune enfant"                              => "pink-macaron",
-
-    # --- Santé ---
-    "Établissements publics de santé"                                                  => "green-menthe",
-    "Fondations et associations de santé"                                              => "green-menthe",
-    "Établissements privés de santé"                                                   => "green-menthe",
-    "Centres d'action sociales (CCAS ou CIAS)"                                         => "green-menthe",
-
-    # --- Culture ---
-    "Musées, théatre et autres établissements culturels"                               => "purple-glycine",
-
-    # --- Transport & infrastructure ---
-    "Ports et aéroports"                                                               => "blue-ecume",
-    "Organismes de transports publics (AOM, régies et opérateurs de transport)"        => "green-bourgeon",
-
-    # --- Logement ---
-    "Organismes d'habitat à loyer modéré (OPH, ESH, etc)"                             => "orange-terre-brulee",
-
-    # --- Économie & consulaires ---
-    "Chambres de commerce et d'industrie, des métiers et de l'artisanat, d'agriculture" => "green-archipel",
-    "éditeurs de logiciels"                                                            => "green-archipel",
-
-    # --- Protection sociale ---
-    "Sécurité sociale, prévoyance et mutuelles"                                        => "green-menthe",
-    "Autres organismes de droit privé investis d'une mission d'intérêt public"         => "brown-cafe-creme",
+    'Tous les acteurs publics'                                                           => 'beige-gris-galet',
+    'Communes et groupements de communes'                                                => 'green-tilleul-verveine',
+    'Départements'                                                                       => 'green-tilleul-verveine',
+    'Régions'                                                                            => 'green-tilleul-verveine',
+    "Collectivités et territoires d'Outre-mer"                                           => 'green-tilleul-verveine',
+    'Syndicats mixtes ouverts'                                                           => 'green-tilleul-verveine',
+    'Autres établissements publics rattachés aux collectivités territoriales'             => 'green-tilleul-verveine',
+    'Ministères, services centraux et déconcentrés'                                      => 'blue-cumulus',
+    'Agences, centres, caisses et offices publics nationaux'                             => 'blue-cumulus',
+    'Agences régionales de santé (ARS)'                                                  => 'blue-cumulus',
+    "Agences de l'eau"                                                                   => 'blue-cumulus',
+    'Parcs nationaux'                                                                    => 'blue-cumulus',
+    'Autorités administratives ou publiques indépendantes'                               => 'blue-cumulus',
+    'Parlement et CESE'                                                                  => 'blue-cumulus',
+    "Autres établissements publics rattachés à l'État"                                   => 'blue-cumulus',
+    'Établissements scolaires'                                                           => 'pink-macaron',
+    "Établissements d'enseignement supérieur"                                            => 'pink-macaron',
+    'Caisses des écoles'                                                                 => 'pink-macaron',
+    'CROUS'                                                                              => 'pink-macaron',
+    "Crèches et établissements d'accueil du jeune enfant"                                => 'pink-macaron',
+    'Établissements publics de santé'                                                    => 'green-menthe',
+    'Fondations et associations de santé'                                                => 'green-menthe',
+    'Établissements privés de santé'                                                     => 'green-menthe',
+    "Centres d'action sociales (CCAS ou CIAS)"                                          => 'green-menthe',
+    'Musées, théâtres et autres établissements culturels'                                => 'purple-glycine',
+    'Ports et aéroports'                                                                 => 'blue-ecume',
+    'Organismes de transports publics (AOM, régies et opérateurs de transport)'          => 'green-bourgeon',
+    "Organismes d'habitat à loyer modéré (OPH, ESH, etc)"                               => 'orange-terre-brulee',
+    "Chambres de commerce et d'industrie, des métiers et de l'artisanat, d'agriculture" => 'green-archipel',
+    'Éditeurs de logiciels'                                                              => 'green-archipel',
+    'Sécurité sociale, prévoyance et mutuelles'                                          => 'green-menthe',
+    "Autres organismes de droit privé investis d'une mission d'intérêt public"           => 'brown-cafe-creme',
   }.freeze
 
   def administration_badge_color(label)
-    ADMINISTRATION_COLORS.fetch(label, "pink-tuile")
+    ADMINISTRATION_COLORS.fetch(label, 'pink-tuile')
   end
 end
