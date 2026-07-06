80d7539c736ba9f47234563cdee76d7f016f3c4a
Delmaire Loïc <loic.delmaire@gmail.com>
2026-07-06 11:11:01 +0200

    Linting

--- site/spec/support/shared_examples/features/logout.rb
+++ site/spec/support/shared_examples/features/logout.rb
@@ -8,9 +8,7 @@ RSpec.shared_examples 'a logout feature' do |controller_class, profile_path_meth
 
   before do
     login_as(create(:user))
-    # rubocop:disable RSpec/AnyInstance
     allow_any_instance_of(controller_class).to receive(:after_logout_path).and_return(after_logout_path)
-    # rubocop:enable RSpec/AnyInstance
   end
 
   it 'clears the session' do
