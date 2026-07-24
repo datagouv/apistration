31e8c9343e63ecf4c92ea55286095088462f3866
Samuel-Zacharie FAURE <samuel.faure.dev@gmail.com>
2026-07-24 11:43:22 +0200

    feat: add DatagouvAPIClient for the data.gouv.fr dataservices API

---
+++ site/app/clients/datagouv_api_client.rb
@@ -0,0 +1,53 @@
+require 'faraday'
+
+class DatagouvAPIClient
+  def list_dataservices(organization:)
+    dataservices = []
+    page = 1
+
+    loop do
+      response = http_connection.get("#{host}/api/1/dataservices/", { organization: organization, page: page, page_size: 100 }).body
+      dataservices.concat(response['data'])
+      break if response['next_page'].blank?
+
+      page += 1
+    end
+
+    dataservices
+  end
+
+  def create_dataservice(payload)
+    http_connection.post("#{host}/api/1/dataservices/", payload.to_json).body
+  end
+
+  def update_dataservice(id, payload)
+    http_connection.patch("#{host}/api/1/dataservices/#{id}/", payload.to_json).body
+  end
+
+  def delete_dataservice(id)
+    http_connection.delete("#{host}/api/1/dataservices/#{id}/")
+    nil
+  end
+
+  private
+
+  def host
+    ENV.fetch('DATAGOUV_HOST')
+  end
+
+  def token
+    ENV.fetch('DATAGOUV_API_TOKEN')
+  end
+
+  def http_connection(&block)
+    Faraday.new do |conn|
+      conn.headers['X-API-KEY'] = token
+      conn.headers['Content-Type'] = 'application/json'
+      conn.request :retry, max: 2
+      conn.response :raise_error
+      conn.response :json
+      conn.options.timeout = 5
+      yield(conn) if block
+    end
+  end
+end
