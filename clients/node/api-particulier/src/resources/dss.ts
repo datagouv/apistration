5594a47557a99ed214991668cdee09888a868e37
Samuel-Zacharie FAURE <samuel.faure.dev@gmail.com>
2026-08-28 12:58:37 +0200

    Regenerate Ruby and Node SDK scaffolding for CNAV Foyer RSA
    Via clients/ruby/bin/scaffold_resources --api particulier and
    clients/node/bin/scaffold-resources.ts --api particulier, from the
    regenerated openapi-particulier.yaml. Adds Dss#foyer_rsa (FranceConnect)
    and Dss#foyer_rsa_identite (civility) to both clients.

--- clients/node/api-particulier/src/resources/dss.ts
+++ clients/node/api-particulier/src/resources/dss.ts
@@ -150,6 +150,34 @@ export class Dss {
     return this.client.get(path, { params: { 'recipient': options.recipient, 'delegation_id': options.delegation_id, 'nomNaissance': options.nom_naissance, 'nomUsage': options.nom_usage, 'prenoms': options.prenoms, 'anneeDateNaissance': options.annee_date_naissance, 'moisDateNaissance': options.mois_date_naissance, 'jourDateNaissance': options.jour_date_naissance, 'sexeEtatCivil': options.sexe_etat_civil, 'codeCogInseePaysNaissance': options.code_cog_insee_pays_naissance, 'codeCogInseeCommuneNaissance': options.code_cog_insee_commune_naissance, 'nomCommuneNaissance': options.nom_commune_naissance, 'codeCogInseeDepartementNaissance': options.code_cog_insee_departement_naissance } });
   }
 
+  /** [FranceConnect] Composition du foyer RSA */
+  async foyer_rsa(options: { version?: number; recipient?: string; delegation_id?: string } = {}) {
+    const resolvedVersion = options.version ?? 3;
+    const path = (() => {
+      switch (resolvedVersion) {
+      case 3:
+        return `/v3/dss/foyer_rsa/france_connect`;
+        default:
+          throw new Error(`version ${resolvedVersion} not available for /dss/foyer_rsa/france_connect; supported: [3]`);
+      }
+    })();
+    return this.client.get(path, { params: { 'recipient': options.recipient, 'delegation_id': options.delegation_id } });
+  }
+
+  /** [Identité] Composition du foyer RSA */
+  async foyer_rsa_identite(options: { version?: number; recipient?: string; delegation_id?: string; nom_naissance: string; prenoms: string[]; annee_date_naissance: string; mois_date_naissance: string; jour_date_naissance: string; sexe_etat_civil: string; code_cog_insee_commune_naissance?: string; code_cog_insee_pays_naissance: string }) {
+    const resolvedVersion = options.version ?? 3;
+    const path = (() => {
+      switch (resolvedVersion) {
+      case 3:
+        return `/v3/dss/foyer_rsa/identite`;
+        default:
+          throw new Error(`version ${resolvedVersion} not available for /dss/foyer_rsa/identite; supported: [3]`);
+      }
+    })();
+    return this.client.get(path, { params: { 'recipient': options.recipient, 'delegation_id': options.delegation_id, 'nomNaissance': options.nom_naissance, 'prenoms': options.prenoms, 'anneeDateNaissance': options.annee_date_naissance, 'moisDateNaissance': options.mois_date_naissance, 'jourDateNaissance': options.jour_date_naissance, 'sexeEtatCivil': options.sexe_etat_civil, 'codeCogInseeCommuneNaissance': options.code_cog_insee_commune_naissance, 'codeCogInseePaysNaissance': options.code_cog_insee_pays_naissance } });
+  }
+
   /** [FranceConnect] Participation familiale EAJE */
   async participation_familiale_eaje(options: { version?: number; recipient?: string; delegation_id?: string } = {}) {
     const resolvedVersion = options.version ?? 3;
